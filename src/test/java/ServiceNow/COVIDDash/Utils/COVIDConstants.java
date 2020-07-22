1d37b09e6dacefc0b925aa420b7b4d50b1f42a23
sohil-nih <sohil.sohil@nih.gov>
2020-07-22 14:41:12 -0400

    valid study submission updated

--- src/test/java/ServiceNow/COVIDDash/Utils/COVIDConstants.java
+++ src/test/java/ServiceNow/COVIDDash/Utils/COVIDConstants.java
@@ -4,11 +4,11 @@ package ServiceNow.COVIDDash.Utils;
 
 public class COVIDConstants {
 
-	public static final String DOC_PATH = System.getProperty("user.dir")+"/src/test/resources/test-data/Study-Documentation.docx";
+	public static final String DOC_PATH = System.getProperty("user.dir")+"/src/test/java/ServiceNow/COVIDDash/Resources/Study-Documentation.docx";
 
 	public static final String TEST_URL = "teams.microsoft.com/l/file";
 	
-	public static final String GOOGLE_URL = "www.google.com/";
+	public static final String GOOGLE_URL = "www.google.com";
 	
 
 
