b01d72f1ac2cfc2af4cfa22554695ae13f4ed1bd
sohil-nih <sohil.sohil@nih.gov>
2020-07-22 13:41:26 -0400

    config file updates

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -89,8 +89,8 @@ formatter.step({
 formatter.match({
   "location": "COVIDHomePageSteps.a_User_has_logged_in_to_the_NIH_COVID_Biorepository_Dashboard_Application(Integer)"
 });
-formatter.write("2020-07-22 11:19:55: INFO: Environment: Test (https://service-test.nci.nih.gov/nih-covid-19-dashboard?xpage\u003dlanding_pub)");
-formatter.write("2020-07-22 11:19:55: Screenshot: ");
+formatter.write("2020-07-22 13:35:47: INFO: Environment: Test (https://service-test.nci.nih.gov/nih-covid-19-dashboard?xpage\u003dlanding_pub)");
+formatter.write("2020-07-22 13:35:47: Screenshot: ");
 formatter.embedding("image/png", "embedded0.png");
 formatter.result({
   "status": "passed"
@@ -122,7 +122,7 @@ formatter.step({
 formatter.match({
   "location": "COVIDHomePageSteps.the_Institute_Division_Email_Address_and_Phone_Number_fields_are_auto_populated(String,String,String,String)"
 });
-formatter.write("2020-07-22 11:20:08: Screenshot: ");
+formatter.write("2020-07-22 13:36:00: Screenshot: ");
 formatter.embedding("image/png", "embedded1.png");
 formatter.result({
   "status": "passed"
@@ -144,6 +144,73 @@ formatter.step({
 formatter.match({
   "location": "COVIDHomePageSteps.the_user_logs_out()"
 });
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/java/ServiceNow/COVIDDash/Features/Login.feature");
+formatter.feature({
+  "name": "Login",
+  "description": "  Reviewer and Regular User/PI Login\n  Description: This feature tests User Story COVIDDASH-133, 141",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Covid dash Login",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User navigates to the COVID-19 Biorepository login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "COVIDHomePageSteps.user_navigates_to_the_COVID_Biorepository_login_page(Integer)"
+});
+formatter.write("2020-07-22 13:36:11: INFO: Environment: Test (https://service-test.nci.nih.gov/nih-covid-19-dashboard?xpage\u003dlanding_pub)");
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the User clicks on Login to access button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "COVIDHomePageSteps.the_User_clicls_on_Login_to_access_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "logs in by entering iTrust credentials",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ITrustLoginPageSteps.logs_in_by_entering_iTrust_credentials()"
+});
+formatter.write("2020-07-22 13:36:15: Screenshot: ");
+formatter.embedding("image/png", "embedded2.png");
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the User is directed to the home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "COVIDHomePageSteps.the_User_is_directed_to_the_Submissions_home_page()"
+});
+formatter.write("2020-07-22 13:36:17: Screenshot: ");
+formatter.embedding("image/png", "embedded3.png");
 formatter.result({
   "status": "passed"
 });
