fed538b6203b1e91840492c07140a2c9541f3c4b
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-19 13:58:43 -0400

    added follow up form page implementation

--- src/test/java/ServiceNow/COVIDCode/Steps/FollowUpFormSteps.java
+++ src/test/java/ServiceNow/COVIDCode/Steps/FollowUpFormSteps.java
@@ -15,17 +15,13 @@ import cucumber.api.java.en.When;
 public class FollowUpFormSteps extends PageInitializer {
 
 	@When("a COVIDCode user accesses a Follow Up Form to update an existing enrollment")
-	public void a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment() throws TestingException {
-		WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("COVIDCode"));
-		CucumberLogUtils.logScreenShot();
-		covidCodeLoginPage.accessAndCompleteFollowUpQuestionnaireButton.click();
-		CucumberLogUtils.logScreenShot();
-		loginImpl.loginToITrust();
+	public void a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment() throws TestingException {		
+		followUpFormPageImpl.accessingFollowUpForm();
 	}
 
 	@Then("the question {string} should display in the demographics section")
 	public void the_question_should_display_in_the_demographics_section(String questionText) {
-		JavascriptUtils.scrollDown(5000);
+		JavascriptUtils.scrollDown(10000);
 		String txt = followUpFormPage.whatIsHighestEducationLevelCompleted.getText();
 		Assert.assertTrue(txt.contentEquals(questionText));
 		CucumberLogUtils.logScreenShot();
