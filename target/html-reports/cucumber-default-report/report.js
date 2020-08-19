0938253afddc1cf54fa3a0afb40daf5ed1f18409
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-19 15:14:52 -0400

    tagging Sprint 7 runners

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -1,52 +1,83 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/ServicePortalEQSubmission.feature");
 formatter.feature({
-  "name": "COVIDCode Follow Up Form Scenarios",
-  "description": "Description: This feature file contains scenarios related to the Follow Up Form",
+  "name": "Service Portal Enrollment Questionnaire Submissions",
+  "description": "",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "Follow Up Form - Verifying added education question - Service Portal",
+  "name": "COVIDcode Study Provider Enrollment Questionnaire Submission Group 1 User",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
       "name": "@Progression"
-    },
-    {
-      "name": "@Sprint7"
     }
   ]
 });
 formatter.before({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
-  "name": "a COVIDCode user accesses a Follow Up Form to update an existing enrollment",
+  "name": "a COVIDcode Study Provider user has logged in to the COVIDcode Study application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.a_COVIDcode_Study_Provider_user_has_logged_in_to_the_COVIDcode_Study_application()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the COVIDcode Study Provider fills out an Enrollment Questionnaire for a \"Group 1\" who says \"Yes\" to join the study",
   "keyword": "When "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment()"
+  "location": "ServicePortalEQSubmissionsSteps.the_COVIDcode_Study_Provider_fills_out_an_Enrollment_Questionnaire_for_a_who_says_to_join_the_study(String,String)"
 });
-formatter.write("2020-08-19 13:23:03: Screenshot: ");
-formatter.embedding("image/png", "embedded0.png");
-formatter.write("2020-08-19 13:23:04: Screenshot: ");
-formatter.embedding("image/png", "embedded1.png");
-formatter.write("2020-08-19 13:23:10: Screenshot: ");
-formatter.embedding("image/png", "embedded2.png");
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
-  "name": "the question \"What is the highest level of school that you/the patient completed?\" should display in the demographics section",
+  "name": "enters users full name, required information, and demographics information",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.enters_users_full_name_required_information_and_demographics_information()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "does not enter Disease Course and Exposures and Risk Factors information",
+  "keyword": "But "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.does_not_enter_Disease_Course_and_Exposures_and_Risk_Factors_information()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Submits the enrollment questionnaire",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.submits_the_enrollment_questionnaire()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the enrollment questionnaire should be successfully submitted",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.the_question_should_display_in_the_demographics_section(String)"
+  "location": "ServicePortalEQSubmissionsSteps.the_enrollment_questionnaire_should_be_successfully_submitted()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "status": "passed"
+  "status": "skipped"
 });
 });
