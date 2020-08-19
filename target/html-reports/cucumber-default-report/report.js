fba6e715309a37d8a7c43cb82fbd939d54c566f3
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-19 15:49:54 -0400

    modified runners

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -18,7 +18,7 @@ formatter.scenario({
   ]
 });
 formatter.before({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "a COVIDCode user accesses a Follow Up Form to update an existing enrollment",
@@ -27,8 +27,12 @@ formatter.step({
 formatter.match({
   "location": "FollowUpFormSteps.a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment()"
 });
+formatter.write("2020-08-19 15:48:35: Screenshot: ");
+formatter.embedding("image/png", "embedded0.png");
+formatter.write("2020-08-19 15:48:36: Screenshot: ");
+formatter.embedding("image/png", "embedded1.png");
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "the question \"What is the highest level of school that you/the patient completed?\" should display in the demographics section",
@@ -37,11 +41,13 @@ formatter.step({
 formatter.match({
   "location": "FollowUpFormSteps.the_question_should_display_in_the_demographics_section(String)"
 });
+formatter.write("2020-08-19 15:48:47: Screenshot: ");
+formatter.embedding("image/png", "embedded2.png");
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.after({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.scenario({
   "name": "RunPending scenario",
@@ -54,33 +60,40 @@ formatter.scenario({
   ]
 });
 formatter.before({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "step one",
   "keyword": "Given "
 });
-formatter.match({});
+formatter.match({
+  "location": "FollowUpFormSteps.step_one()"
+});
 formatter.result({
-  "status": "undefined"
+  "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat ServiceNow.COVIDCode.Steps.FollowUpFormSteps.step_one(FollowUpFormSteps.java:33)\n\tat ✽.step one(file:src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature:12)\n",
+  "status": "pending"
 });
 formatter.step({
   "name": "step two",
   "keyword": "When "
 });
-formatter.match({});
+formatter.match({
+  "location": "FollowUpFormSteps.step_two()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "skipped"
 });
 formatter.step({
   "name": "step three",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "FollowUpFormSteps.step_three()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "skipped"
 });
 formatter.after({
-  "status": "skipped"
+  "status": "passed"
 });
 });
