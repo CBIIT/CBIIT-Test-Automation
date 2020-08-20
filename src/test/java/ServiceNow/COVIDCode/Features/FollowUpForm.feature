105447e3e7c206580d38b4a12cc16eba6f2ee00e
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-20 13:47:41 -0400

    Adding COVIDCode Scenario - Follow Up Form search function

--- src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature
+++ src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature
@@ -3,6 +3,11 @@ Feature: COVIDCode Follow Up Form Scenarios
 Description: This feature file contains scenarios related to the Follow Up Form
 
 @Sprint7 @COVID-95 @Smoke
-Scenario: RunDefined Follow Up Form - Verifying added education question - Service Portal
+Scenario: Follow Up Form - Verifying added education question - Service Portal
 When a COVIDCode user accesses a Follow Up Form to update an existing enrollment
 Then the question "What is the highest level of school that you/the patient completed?" should display in the demographics section
+
+@Sprint7 @COVID-209 @Smoke
+Scenario: Follow Up Form - Search Function - Service Portal
+Given a COVIDCode user is on the Follow Up Form to update an existing enrollment
+Then the user is able to search an existing enrollment by patient ID, last name, first name, or NIH Medical Record Number
--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -1,16 +1,22 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/ServicePortalEQSubmission.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature");
 formatter.feature({
-  "name": "Service Portal Enrollment Questionnaire Submissions",
-  "description": "",
+  "name": "COVIDCode Follow Up Form Scenarios",
+  "description": "Description: This feature file contains scenarios related to the Follow Up Form",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "COVIDcode Study Provider Enrollment Questionnaire Submission Group 1 User",
+  "name": "Follow Up Form - Verifying added education question - Service Portal",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@Progression"
+      "name": "@Sprint7"
+    },
+    {
+      "name": "@COVID-95"
+    },
+    {
+      "name": "@Smoke"
     }
   ]
 });
@@ -18,66 +24,84 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "a COVIDcode Study Provider user has logged in to the COVIDcode Study application",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "ServicePortalEQSubmissionsSteps.a_COVIDcode_Study_Provider_user_has_logged_in_to_the_COVIDcode_Study_application()"
-});
-formatter.write("2020-08-20 09:03:09: Screenshot: ");
-formatter.embedding("image/png", "embedded0.png");
-formatter.write("2020-08-20 09:03:09: Screenshot: ");
-formatter.embedding("image/png", "embedded1.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the COVIDcode Study Provider fills out an Enrollment Questionnaire for a \"Group 1\" who says \"Yes\" to join the study",
+  "name": "a COVIDCode user accesses a Follow Up Form to update an existing enrollment",
   "keyword": "When "
 });
 formatter.match({
-  "location": "ServicePortalEQSubmissionsSteps.the_COVIDcode_Study_Provider_fills_out_an_Enrollment_Questionnaire_for_a_who_says_to_join_the_study(String,String)"
+  "location": "FollowUpFormSteps.a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment()"
 });
+formatter.write("2020-08-20 13:40:07: Screenshot: ");
+formatter.embedding("image/png", "embedded0.png");
+formatter.write("2020-08-20 13:40:08: Screenshot: ");
+formatter.embedding("image/png", "embedded1.png");
+formatter.write("2020-08-20 13:40:15: Screenshot: ");
+formatter.embedding("image/png", "embedded2.png");
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "enters users full name, required information, and demographics information",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalEQSubmissionsSteps.enters_users_full_name_required_information_and_demographics_information()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "does not enter Disease Course and Exposures and Risk Factors information",
-  "keyword": "But "
-});
-formatter.match({
-  "location": "ServicePortalEQSubmissionsSteps.does_not_enter_Disease_Course_and_Exposures_and_Risk_Factors_information()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Submits the enrollment questionnaire",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalEQSubmissionsSteps.submits_the_enrollment_questionnaire()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the enrollment questionnaire should be successfully submitted",
+  "name": "the question \"What is the highest level of school that you/the patient completed?\" should display in the demographics section",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "ServicePortalEQSubmissionsSteps.the_enrollment_questionnaire_should_be_successfully_submitted()"
+  "location": "FollowUpFormSteps.the_question_should_display_in_the_demographics_section(String)"
 });
+formatter.write("2020-08-20 13:40:16: Screenshot: ");
+formatter.embedding("image/png", "embedded3.png");
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Follow Up Form - Search Function - Service Portal",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Sprint7"
+    },
+    {
+      "name": "@COVID-209"
+    },
+    {
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "a COVIDCode user is on the Follow Up Form to update an existing enrollment",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "FollowUpFormSteps.a_COVIDCode_user_is_on_the_Follow_Up_Form_to_update_an_existing_enrollment()"
+});
+formatter.write("2020-08-20 13:40:29: Screenshot: ");
+formatter.embedding("image/png", "embedded4.png");
+formatter.write("2020-08-20 13:40:29: Screenshot: ");
+formatter.embedding("image/png", "embedded5.png");
+formatter.write("2020-08-20 13:40:37: Screenshot: ");
+formatter.embedding("image/png", "embedded6.png");
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user is able to search an existing enrollment by patient ID, last name, first name, or NIH Medical Record Number",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "FollowUpFormSteps.the_user_is_able_to_search_an_existing_enrollment_by_patient_ID_last_name_first_name_or_NIH_Medical_Record_Number()"
+});
+formatter.write("2020-08-20 13:40:42: Screenshot: ");
+formatter.embedding("image/png", "embedded7.png");
+formatter.write("2020-08-20 13:40:45: Screenshot: ");
+formatter.embedding("image/png", "embedded8.png");
+formatter.write("2020-08-20 13:40:49: Screenshot: ");
+formatter.embedding("image/png", "embedded9.png");
 formatter.result({
   "status": "passed"
 });
