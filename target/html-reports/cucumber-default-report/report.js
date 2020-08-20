20b13626325ce2d8b39723294ec7a7c958db0e24
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-20 09:06:14 -0400

    Added new date format to COVIDCode Enrollment

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -1,19 +1,16 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/ServicePortalEQSubmission.feature");
 formatter.feature({
-  "name": "COVIDCode Follow Up Form Scenarios",
-  "description": "Description: This feature file contains scenarios related to the Follow Up Form",
+  "name": "Service Portal Enrollment Questionnaire Submissions",
+  "description": "",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "RunDefined Follow Up Form - Verifying added education question - Service Portal",
+  "name": "COVIDcode Study Provider Enrollment Questionnaire Submission Group 1 User",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@Sprint7"
-    },
-    {
-      "name": "@COVID-95"
+      "name": "@Progression"
     }
   ]
 });
@@ -21,77 +18,68 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "a COVIDCode user accesses a Follow Up Form to update an existing enrollment",
-  "keyword": "When "
+  "name": "a COVIDcode Study Provider user has logged in to the COVIDcode Study application",
+  "keyword": "Given "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment()"
+  "location": "ServicePortalEQSubmissionsSteps.a_COVIDcode_Study_Provider_user_has_logged_in_to_the_COVIDcode_Study_application()"
 });
-formatter.write("2020-08-19 15:48:35: Screenshot: ");
+formatter.write("2020-08-20 09:03:09: Screenshot: ");
 formatter.embedding("image/png", "embedded0.png");
-formatter.write("2020-08-19 15:48:36: Screenshot: ");
+formatter.write("2020-08-20 09:03:09: Screenshot: ");
 formatter.embedding("image/png", "embedded1.png");
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the question \"What is the highest level of school that you/the patient completed?\" should display in the demographics section",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "FollowUpFormSteps.the_question_should_display_in_the_demographics_section(String)"
-});
-formatter.write("2020-08-19 15:48:47: Screenshot: ");
-formatter.embedding("image/png", "embedded2.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "RunPending scenario",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@Sprint7"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "step one",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "FollowUpFormSteps.step_one()"
-});
-formatter.result({
-  "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat ServiceNow.COVIDCode.Steps.FollowUpFormSteps.step_one(FollowUpFormSteps.java:33)\n\tat ✽.step one(file:src/test/java/ServiceNow/COVIDCode/Features/FollowUpForm.feature:12)\n",
-  "status": "pending"
-});
-formatter.step({
-  "name": "step two",
+  "name": "the COVIDcode Study Provider fills out an Enrollment Questionnaire for a \"Group 1\" who says \"Yes\" to join the study",
   "keyword": "When "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.step_two()"
+  "location": "ServicePortalEQSubmissionsSteps.the_COVIDcode_Study_Provider_fills_out_an_Enrollment_Questionnaire_for_a_who_says_to_join_the_study(String,String)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
-  "name": "step three",
+  "name": "enters users full name, required information, and demographics information",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.enters_users_full_name_required_information_and_demographics_information()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "does not enter Disease Course and Exposures and Risk Factors information",
+  "keyword": "But "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.does_not_enter_Disease_Course_and_Exposures_and_Risk_Factors_information()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Submits the enrollment questionnaire",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ServicePortalEQSubmissionsSteps.submits_the_enrollment_questionnaire()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the enrollment questionnaire should be successfully submitted",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.step_three()"
+  "location": "ServicePortalEQSubmissionsSteps.the_enrollment_questionnaire_should_be_successfully_submitted()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.after({
   "status": "passed"
