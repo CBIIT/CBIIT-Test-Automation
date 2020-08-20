341c92c9d82a13e4e956493f0c44d4f3001ffe83
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-20 13:47:41 -0400

    Adding COVIDCode Scenario - Follow Up Form search function

--- src/test/java/ServiceNow/COVIDCode/Steps/FollowUpFormSteps.java
+++ src/test/java/ServiceNow/COVIDCode/Steps/FollowUpFormSteps.java
@@ -1,9 +1,16 @@
 package ServiceNow.COVIDCode.Steps;
 
+import java.util.List;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 import com.nci.automation.utils.CucumberLogUtils;
+import com.nci.automation.utils.MiscUtils;
 import com.nci.automation.web.JavascriptUtils;
+import com.nci.automation.web.WebDriverUtils;
 import com.nci.automation.xceptions.TestingException;
 
 import appsCommon.PageInitializer;
@@ -23,10 +30,35 @@ public class FollowUpFormSteps extends PageInitializer {
 		String txt = followUpFormPage.whatIsHighestEducationLevelCompleted.getText();
 		Assert.assertTrue(txt.contentEquals(questionText));
 		JavascriptUtils.scrollDown(2000);
-		CucumberLogUtils.logScreenShot();
-		
+		CucumberLogUtils.logScreenShot();		
 	}
 	
+	@Given("a COVIDCode user is on the Follow Up Form to update an existing enrollment")
+	public void a_COVIDCode_user_is_on_the_Follow_Up_Form_to_update_an_existing_enrollment() throws TestingException {	
+		followUpFormPageImpl.accessingFollowUpForm();	
+	}
+
+	@Then("the user is able to search an existing enrollment by patient ID, last name, first name, or NIH Medical Record Number")
+	public void the_user_is_able_to_search_an_existing_enrollment_by_patient_ID_last_name_first_name_or_NIH_Medical_Record_Number() {
+		followUpFormPage.enrollmentSearchDropDown.click();
+		followUpFormPage.enrollmentSearchTxtBox.sendKeys("AutomatedFN");	
+		MiscUtils.sleep(3000);
+		followUpFormPage.enrollmentSearchTxtBox.sendKeys(Keys.ENTER);
+		CucumberLogUtils.logScreenShot();
+		followUpFormPage.enrollmentSearchXbutton.click();
+		followUpFormPage.enrollmentSearchDropDown.click();
+		followUpFormPage.enrollmentSearchTxtBox.sendKeys("NVAutomatedLN");
+		MiscUtils.sleep(3000);
+		followUpFormPage.enrollmentSearchTxtBox.sendKeys(Keys.ENTER);
+		CucumberLogUtils.logScreenShot();
+		followUpFormPage.enrollmentSearchXbutton.click();
+		followUpFormPage.enrollmentSearchDropDown.click();
+		followUpFormPage.enrollmentSearchTxtBox.sendKeys("HCC");
+		MiscUtils.sleep(3000);
+		followUpFormPage.enrollmentSearchTxtBox.sendKeys(Keys.ENTER);
+		CucumberLogUtils.logScreenShot();
+		followUpFormPage.enrollmentSearchXbutton.click();	
+	}
 
 
 }
