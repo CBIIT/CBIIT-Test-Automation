397b6495595463dfc4e559be309e448df5bf4733
iamdez99 <diegojuarezbusiness@gmail.com>
2020-08-21 14:08:25 -0400

    Added COVIDCode Scenario - verifying Group User ID and Host Hospital Code fields are locked

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -5,18 +5,12 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "Follow Up Form - Verifying added education question - Service Portal",
+  "name": "Follow Up Form - Verifying Group ID and Hospital Code fields are locked - Service Portal",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@Sprint7"
-    },
-    {
-      "name": "@COVID-95"
-    },
-    {
-      "name": "@Smoke"
+      "name": "@Progression"
     }
   ]
 });
@@ -24,84 +18,40 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "a COVIDCode user accesses a Follow Up Form to update an existing enrollment",
-  "keyword": "When "
+  "name": "a COVIDCode Provider is on the Follow Up Form to update an existing enrollment",
+  "keyword": "Given "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.a_COVIDCode_user_accesses_a_Follow_Up_Form_to_update_an_existing_enrollment()"
+  "location": "FollowUpFormSteps.a_COVIDCode_Provider_is_on_the_Follow_Up_Form_to_update_an_existing_enrollment()"
 });
-formatter.write("2020-08-20 13:40:07: Screenshot: ");
+formatter.write("2020-08-21 14:00:48: Screenshot: ");
 formatter.embedding("image/png", "embedded0.png");
-formatter.write("2020-08-20 13:40:08: Screenshot: ");
+formatter.write("2020-08-21 14:00:49: Screenshot: ");
 formatter.embedding("image/png", "embedded1.png");
-formatter.write("2020-08-20 13:40:15: Screenshot: ");
+formatter.write("2020-08-21 14:00:57: Screenshot: ");
 formatter.embedding("image/png", "embedded2.png");
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the question \"What is the highest level of school that you/the patient completed?\" should display in the demographics section",
-  "keyword": "Then "
+  "name": "an enrollment is selected",
+  "keyword": "When "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.the_question_should_display_in_the_demographics_section(String)"
+  "location": "FollowUpFormSteps.an_enrollment_is_selected()"
 });
-formatter.write("2020-08-20 13:40:16: Screenshot: ");
+formatter.write("2020-08-21 14:01:02: Screenshot: ");
 formatter.embedding("image/png", "embedded3.png");
 formatter.result({
   "status": "passed"
 });
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Follow Up Form - Search Function - Service Portal",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@Sprint7"
-    },
-    {
-      "name": "@COVID-209"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
 formatter.step({
-  "name": "a COVIDCode user is on the Follow Up Form to update an existing enrollment",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "FollowUpFormSteps.a_COVIDCode_user_is_on_the_Follow_Up_Form_to_update_an_existing_enrollment()"
-});
-formatter.write("2020-08-20 13:40:29: Screenshot: ");
-formatter.embedding("image/png", "embedded4.png");
-formatter.write("2020-08-20 13:40:29: Screenshot: ");
-formatter.embedding("image/png", "embedded5.png");
-formatter.write("2020-08-20 13:40:37: Screenshot: ");
-formatter.embedding("image/png", "embedded6.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user is able to search an existing enrollment by patient ID, last name, first name, or NIH Medical Record Number",
+  "name": "the Group ID and Hospital Code fields should be locked",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "FollowUpFormSteps.the_user_is_able_to_search_an_existing_enrollment_by_patient_ID_last_name_first_name_or_NIH_Medical_Record_Number()"
+  "location": "FollowUpFormSteps.the_Group_ID_and_Hospital_Code_fields_should_be_locked()"
 });
-formatter.write("2020-08-20 13:40:42: Screenshot: ");
-formatter.embedding("image/png", "embedded7.png");
-formatter.write("2020-08-20 13:40:45: Screenshot: ");
-formatter.embedding("image/png", "embedded8.png");
-formatter.write("2020-08-20 13:40:49: Screenshot: ");
-formatter.embedding("image/png", "embedded9.png");
 formatter.result({
   "status": "passed"
 });
