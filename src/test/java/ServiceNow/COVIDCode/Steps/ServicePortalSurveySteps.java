cd0cbb009ca794ab55dc05ebf990ccd6dd86cdb6
iamdez99 <diegojuarezbusiness@gmail.com>
2020-09-11 23:31:00 -0400

    adding waits to failed scenarios

--- src/test/java/ServiceNow/COVIDCode/Steps/ServicePortalSurveySteps.java
+++ src/test/java/ServiceNow/COVIDCode/Steps/ServicePortalSurveySteps.java
@@ -30,7 +30,7 @@ public class ServicePortalSurveySteps extends PageInitializer {
 
 	@Given("selects the option to Volunteer")
 	public void selects_the_option_to_Volunteer() {
-		MiscUtils.sleep(2000);
+		MiscUtils.sleep(3000);
 		covidCodeLoginPage.volunteerHereButton.click();
 	}
 
@@ -225,7 +225,7 @@ public class ServicePortalSurveySteps extends PageInitializer {
 
 	@When("searching for {string} to view follow ups")
 	public void searching_for_to_view_follow_ups(String covidCodeVolunteerSurveysTxt) {
-		MiscUtils.sleep(2000);
+		MiscUtils.sleep(3000);
 		CommonUtils.waitForVisibility(WebDriverUtils.webDriver.findElement(By.id("filter")));
 		MiscUtils.sleep(4000);
 		nativeViewEnrollementsPage.filterNavigator.sendKeys(covidCodeVolunteerSurveysTxt);
