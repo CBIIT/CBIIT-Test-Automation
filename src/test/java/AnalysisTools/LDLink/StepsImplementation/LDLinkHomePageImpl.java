1374556df0d8eb718b48178acf0a0288f0059d13
uddins2 <uddins2@nci-02134309-ml.fios-router.home>
2020-09-22 00:03:02 -0400

    LDLink homepage feature and steps

--- src/test/java/AnalysisTools/LDLink/StepsImplementation/LDLinkHomePageImpl.java
+++ src/test/java/AnalysisTools/LDLink/StepsImplementation/LDLinkHomePageImpl.java
@@ -1,9 +1,25 @@
 package AnalysisTools.LDLink.StepsImplementation;
 
+import org.junit.Test;
+
+import com.nci.automation.web.EnvUtils;
+import com.nci.automation.web.WebDriverUtils;
+import com.nci.automation.xceptions.TestingException;
+
 import appsCommon.PageInitializer;
 
 
 public class LDLinkHomePageImpl extends PageInitializer {
+	
+	@Test
+	public void openUp() {
+		try {
+			WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("COVIDCode"));
+		} catch (TestingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
 
 
 
