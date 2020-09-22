ff3e786a20705dce72dc56c01a2f69067bd8163b
uddins2 <uddins2@nci-02134309-ml.fios-router.home>
2020-09-22 00:03:02 -0400

    LDLink homepage feature and steps

--- src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomePageSteps.java
+++ src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomePageSteps.java
@@ -1,8 +1,32 @@
 package AnalysisTools.LDLink.Steps;
 
+import com.nci.automation.web.EnvUtils;
+import com.nci.automation.web.WebDriverUtils;
+import com.nci.automation.xceptions.TestingException;
 
+import appsCommon.PageInitializer;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
-public class LDLinkHomePageSteps {
+public class LDLinkHomePageSteps extends PageInitializer {
+	
+	@When("a LDLink user opens landing page")
+	public void a_LDLink_user_opens_landing_page() throws TestingException {
+		WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("LDLink"));
+	}
+
+	@Then("user verify LDLink present on the screen")
+	public void user_verify_LDLink_present_on_the_screen() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("user verify the description {string}")
+	public void user_verify_the_description(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
 
 
 
