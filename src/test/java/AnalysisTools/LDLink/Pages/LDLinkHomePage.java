7ab437775ac5073a8294de9ab695b9fad84cf81f
uddins2 <uddins2@nci-02134309-ml.fios-router.home>
2020-09-28 10:05:58 -0400

    LDexpress feature and page

--- src/test/java/AnalysisTools/LDLink/Pages/LDLinkHomePage.java
+++ src/test/java/AnalysisTools/LDLink/Pages/LDLinkHomePage.java
@@ -1,7 +1,10 @@
 package AnalysisTools.LDLink.Pages;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -20,9 +23,37 @@ public class LDLinkHomePage extends CommonUtils {
 	public List<WebElement> crdLDLinkApplications;
 	
 	@FindBy(xpath = "//div[@id='home-tab']/div/h2")
-	public List<WebElement> lblWelcomeHeading;
+	public WebElement lblWelcomeHeading;
 	
 	@FindBy(xpath = "//div[@id='home-tab']/div[2]/p")
-	public List<WebElement> lblLDLinkDescription;
+	public WebElement lblLDLinkWelcomeNote;
+	
+	@FindBy(xpath = "//a[@dest='ldexpress']/div[1]")
+	public WebElement lnkLdExpress;
+	
+	
+	public boolean isLDLinkWelcomeHeaderDisplayed() {
+		return CommonUtils.isElementDisplayed(lblWelcomeHeading); 
+	}
+	
+	public String getLDLinkHomePageWelcomeNoteText() {
+		return CommonUtils.getText(lblLDLinkWelcomeNote).trim(); 
+	}
+	
+	public Map<String,String> getApplicationsNameAndDescription(){
+		Map<String, String> actApplication = new HashMap<>(); 
+		for(WebElement card:crdLDLinkApplications) {
+			String appName = card.findElement(By.xpath("./a/div/h2")).getText().trim();
+			String appDesc = card.findElement(By.xpath(".a/div/p")).getText().trim();
+			actApplication.put(appName, appDesc); 
+		}
+		return actApplication; 
+	}
+	
+	public void clickLDExpressCardOnHomePage() {
+		CommonUtils.click(lnkLdExpress);
+	}
+	
+	
 
 }
