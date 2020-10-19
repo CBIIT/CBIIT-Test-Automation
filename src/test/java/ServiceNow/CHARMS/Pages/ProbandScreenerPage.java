4b650d36e5424450a9ee1755283dd72ac09c4825
iamdez99 <diegojuarezbusiness@gmail.com>
2020-10-19 14:51:40 -0400

    ..

--- src/test/java/ServiceNow/CHARMS/Pages/ProbandScreenerPage.java
+++ src/test/java/ServiceNow/CHARMS/Pages/ProbandScreenerPage.java
@@ -1,5 +1,34 @@
 package ServiceNow.CHARMS.Pages;
 
-public class ProbandScreenerPage {
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.nci.automation.web.CommonUtils;
+import com.nci.automation.web.WebDriverUtils;
+
+public class ProbandScreenerPage extends CommonUtils {
+	
+	/** Are you filling this questionnaire out for someone else? drop down */
+	@FindBy (xpath = "//span[@id='select2-chosen-1']")
+	public WebElement areYouFillingOutThisQuestionnaireForSomeoneElseDropDown;
+	
+	/** Are you filling this questionnaire out for someone else? drop down */
+	@FindBy (xpath = "/html/body/div[3]/ul/li")
+	public List<WebElement> areYouFillingOutThisQuestionnaireForSomeoneElseDropDownValues;
+	
+	/** SAVE and NEXT button */
+	@FindBy (xpath = "//button[contains(text(),'SAVE & NEXT')]")
+	public WebElement saveAndNextBtn;
+	
+	
+	
+	
+	
+	public ProbandScreenerPage() {
+		PageFactory.initElements(WebDriverUtils.webDriver, this);
+	}
 
 }
