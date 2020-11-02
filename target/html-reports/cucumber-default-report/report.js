750cb98b867129e7bf31f304dfb880df7c929602
Mata Rodriguez <matarodriguezko@nci-02133367-ml.nih.gov>
2020-11-02 16:57:54 -0500

    fixed the previously automated sign out verification for CovidCode

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -10,6 +10,12 @@ formatter.scenario({
   "keyword": "Scenario",
   "tags": [
     {
+      "name": "@Test_Case_COVID-245"
+    },
+    {
+      "name": "@User_Story_COVID-261"
+    },
+    {
       "name": "@Progression"
     }
   ]
@@ -24,6 +30,10 @@ formatter.step({
 formatter.match({
   "location": "SignOutVerificationSteps.an_internal_user_is_logged_into_the_COVIDcode_application()"
 });
+formatter.write("2020-11-02 16:54:53: Screenshot: ");
+formatter.embedding("image/png", "embedded0.png");
+formatter.write("2020-11-02 16:54:56: Screenshot: ");
+formatter.embedding("image/png", "embedded1.png");
 formatter.result({
   "status": "passed"
 });
@@ -34,20 +44,22 @@ formatter.step({
 formatter.match({
   "location": "SignOutVerificationSteps.is_on_the_home_page()"
 });
-formatter.write("2020-10-30 23:26:32: Screenshot: ");
-formatter.embedding("image/png", "embedded0.png");
+formatter.write("2020-11-02 16:54:59: Screenshot: ");
+formatter.embedding("image/png", "embedded2.png");
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the internal user is able to sign out of the application by clicking on the \"Sign Out\" button",
+  "name": "the internal user is able to sign out of the application by clicking on the Sign Out button",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "SignOutVerificationSteps.the_internal_user_is_able_to_sign_out_of_the_application_by_clicking_on_the_button(String)"
+  "location": "SignOutVerificationSteps.the_internal_user_is_able_to_sign_out_of_the_application_by_clicking_on_the_button()"
 });
-formatter.write("2020-10-30 23:26:37: Screenshot: ");
-formatter.embedding("image/png", "embedded1.png");
+formatter.write("2020-11-02 16:55:01: Screenshot: ");
+formatter.embedding("image/png", "embedded3.png");
+formatter.write("2020-11-02 16:55:05: Screenshot: ");
+formatter.embedding("image/png", "embedded4.png");
 formatter.result({
   "status": "passed"
 });
