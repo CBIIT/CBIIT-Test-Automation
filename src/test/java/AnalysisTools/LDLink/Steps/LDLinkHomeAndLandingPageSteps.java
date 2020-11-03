41a4c3dd1449feb7bceab5aea2df34cd7871ed2d
Shomir Uddin <shomir.uddin@nih.gov>
2020-11-03 14:29:01 -0500

    LDLink - removed old code doesnt follow guidelines

--- src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomeAndLandingPageSteps.java
+++ src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomeAndLandingPageSteps.java
@@ -17,53 +17,7 @@ import org.junit.Assert;
 public class LDLinkHomeAndLandingPageSteps extends PageInitializer {
 	
 	
-	@When("a LDlink user opens Analysis Tools page")
-	public void a_LDlink_user_opens_Analysis_Tools_page() throws TestingException {
-		WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("Analysistools"));
-	}
 
-	
-	
-
-	@When("a LDlink user opens home page")
-	public void a_LDlink_user_opens_home_page() throws TestingException {
-		WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("LDLink"));
-	}
-
-	@Then("user verify the description {string}")
-	public void user_verify_the_description(String expectedText) {
-	    String actualText = ldLinkLandingPage.lblLDLinkDescripition.getText();
-	    Assert.assertEquals("Verifying LDLink landing page user sees LDLink description",
-				expectedText, actualText);
-	    CucumberLogUtils.logScreenShot();
-	    
-	}
-	
-	@Then("User verify title is {string}")
-	public void user_verify_title_is(String expectedTitle) {
-		String actualTitle = WebDriverUtils.webDriver.getTitle(); 
-		Assert.assertEquals("Verifying COVIDcode homepage title", expectedTitle, actualTitle); 
-	}
-	
-	@When("user clicks LDLink on landing page")
-	public void user_clicks_LDLink_on_landing_page() {
-		ldLinkLandingPage.lnkLDLink.click();
-	   
-	}
-
-
-	@Then("user verify the following applications displayed with given description")
-	public void user_verify_the_following_applications_displayed_with_given_description(Map<String,String> expApps) {
-		CucumberLogUtils.logScreenShot();
-		Map<String,String> actApps = ldLinkHomePage.getApplicationsNameAndDescription(); 
-		Assert.assertEquals(expApps, actApps);
-		
-	}
-
-	@When("user clicks on LDExpress application")
-	public void userClicksOnLDLinkExpressApplication() {
-		ldLinkHomePage.clickLDExpressCardOnHomePage();
-	}
 
 
 
