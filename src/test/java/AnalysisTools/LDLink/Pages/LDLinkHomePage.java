4e41b165f12a358114cfab50e5d02788c1949223
Shomir Uddin <shomir.uddin@nih.gov>
2020-11-03 14:29:01 -0500

    LDLink - removed old code doesnt follow guidelines

--- src/test/java/AnalysisTools/LDLink/Pages/LDLinkHomePage.java
+++ src/test/java/AnalysisTools/LDLink/Pages/LDLinkHomePage.java
@@ -15,44 +15,6 @@ import com.nci.automation.web.WebDriverUtils;
 public class LDLinkHomePage extends CommonUtils {
 	
 	
-	public LDLinkHomePage() {
-		PageFactory.initElements(WebDriverUtils.webDriver, this);
-	}
-	
-	@FindBy(xpath = "//div[@class='card-outside']")
-	public List<WebElement> crdLDLinkApplications;
-	
-	@FindBy(xpath = "//div[@id='home-tab']/div/h2")
-	public WebElement lblWelcomeHeading;
-	
-	@FindBy(xpath = "//div[@id='home-tab']/div[2]/p")
-	public WebElement lblLDLinkWelcomeNote;
-	
-	@FindBy(xpath = "//a[@dest='ldexpress']/div[1]")
-	public WebElement lnkLdExpress;
-	
-	
-	public boolean isLDLinkWelcomeHeaderDisplayed() {
-		return CommonUtils.isElementDisplayed(lblWelcomeHeading); 
-	}
-	
-	public String getLDLinkHomePageWelcomeNoteText() {
-		return CommonUtils.getText(lblLDLinkWelcomeNote).trim(); 
-	}
-	
-	public Map<String,String> getApplicationsNameAndDescription(){
-		Map<String, String> actApplication = new HashMap<>(); 
-		for(WebElement card:crdLDLinkApplications) {
-			String appName = card.findElement(By.xpath("./a/div/h2")).getText().trim();
-			String appDesc = card.findElement(By.xpath(".a/div/p")).getText().trim();
-			actApplication.put(appName, appDesc); 
-		}
-		return actApplication; 
-	}
-	
-	public void clickLDExpressCardOnHomePage() {
-		CommonUtils.click(lnkLdExpress);
-	}
 	
 	
 
