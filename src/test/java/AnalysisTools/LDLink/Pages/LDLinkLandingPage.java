dacae70aa83f3475783447986814b665f6b1305a
Shomir Uddin <shomir.uddin@nih.gov>
2020-11-03 14:29:01 -0500

    LDLink - removed old code doesnt follow guidelines

--- src/test/java/AnalysisTools/LDLink/Pages/LDLinkLandingPage.java
+++ src/test/java/AnalysisTools/LDLink/Pages/LDLinkLandingPage.java
@@ -8,19 +8,6 @@ import com.nci.automation.web.WebDriverUtils;
 
 public class LDLinkLandingPage {
 	
-	public LDLinkLandingPage() {
-		PageFactory.initElements(WebDriverUtils.webDriver, this);
-	}
-	
-	@FindBy(linkText = "LDLink")
-	public WebElement lnkLDLink; 
-	
-	
-//	@FindBy(xpath = "//a[text() = 'LDLink']/../following-sibling::div")
-//	public WebElement lblLDLinkDescripition; 
-	
-	@FindBy(xpath = "(//div[@class='tools-section'])[8]/div")
-	public WebElement lblLDLinkDescripition; 
 	
 
 }
