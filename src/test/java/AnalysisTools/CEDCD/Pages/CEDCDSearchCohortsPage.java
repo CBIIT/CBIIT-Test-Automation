d38f0301df7cc9edd3a3350bd67635c56d0915d2
Mata Rodriguez <matarodriguezko@nci-02133367-ml.myfiosgateway.com>
2020-11-13 14:32:56 -0500

    CEDCD - Automated 3 more test cases

--- src/test/java/AnalysisTools/CEDCD/Pages/CEDCDSearchCohortsPage.java
+++ src/test/java/AnalysisTools/CEDCD/Pages/CEDCDSearchCohortsPage.java
@@ -9,29 +9,29 @@ import com.nci.automation.web.WebDriverUtils;
 
 public class CEDCDSearchCohortsPage extends CommonUtils {
 
-	/** This element is the search cohorts tab */
+	/** This element is the search Cohorts tab */
 	@FindBy(xpath = "//a[@id='searchCohortsTab']")
 	public WebElement searchCohortTab;
 
-	/** This element is the gender dropdown from the search cohorts tab */
+	/** This element is the gender drop down from the search Cohorts tab */
 	@FindBy(xpath = "//button[contains(text(),'Gender')]")
 	public WebElement searchCohortsGenderBtn;
 
 	/**
-	 * This element selects "Female" from the gender dropdown from the search
+	 * This element selects "Female" from the gender drop down from the search
 	 * cohorts tab
 	 */
 	@FindBy(xpath = "//label[contains(text(),'Female')]")
 	public WebElement searchCohortsFemaleOption;
 
 	/**
-	 * This element selects "Male" from the gender dropdown from the search cohorts
+	 * This element selects "Male" from the gender drop down from the search Cohorts
 	 * tab
 	 */
 	@FindBy(xpath = "//label[contains(text(),'Male')]")
 	public WebElement searchCohortsMaleOption;
 
-	/** This element clicks on the search cohort button */
+	/** This element clicks on the search Cohort button */
 	@FindBy(xpath = "//input[@id='filterEngage']")
 	public WebElement searchCohortResultBtn;
 
@@ -42,6 +42,22 @@ public class CEDCDSearchCohortsPage extends CommonUtils {
 	/** This element displays the number of returned results in text form */
 	@FindBy(xpath = "(//span[@id='summaryCount'])[1]")
 	public WebElement numberOfSearchResultReturned;
+
+	/** This element is the select all check box on the Search Cohorts Page */
+	@FindBy(xpath = "//input[@id='select_all']")
+	public WebElement selectAllCheckBox;
+	
+	/** This element selects the Biospecimen tab on the home page */
+	@FindBy (xpath = "//span[contains(text(),'Biospecimen Counts')]")
+	public WebElement biospecimenTab;
+	
+	/** This element selects the Specimen Ttpe drop down from the Biospecimen tab */
+	@FindBy (xpath = "//button[contains(text(), 'Specimen Type')]")
+	public WebElement specimenType;
+	
+	/** This element selects "All Types" from the Specimen Type drop down */
+	@FindBy (xpath = "//label[contains(text(), 'All Types')]")
+	public WebElement specimenTypeDropDownAllTypeValue;
 
 	public CEDCDSearchCohortsPage() {
 		PageFactory.initElements(WebDriverUtils.webDriver, this);
