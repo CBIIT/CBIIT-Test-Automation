4c7e6ad3b4e6cad32863415a552cdf85267c11b4
Mata Rodriguez <matarodriguezko@nci-02133367-ml.nih.gov>
2020-11-16 14:39:42 -0500

    CEDCD - Added Biospecimen Clear All feature and automated as well

--- src/test/java/AnalysisTools/CEDCD/Pages/CEDCDSearchCohortsPage.java
+++ src/test/java/AnalysisTools/CEDCD/Pages/CEDCDSearchCohortsPage.java
@@ -51,13 +51,35 @@ public class CEDCDSearchCohortsPage extends CommonUtils {
 	@FindBy (xpath = "//span[contains(text(),'Biospecimen Counts')]")
 	public WebElement biospecimenTab;
 	
-	/** This element selects the Specimen Ttpe drop down from the Biospecimen tab */
+	/** This element selects the Specimen Type drop down from the Biospecimen tab */
 	@FindBy (xpath = "//button[contains(text(), 'Specimen Type')]")
 	public WebElement specimenType;
 	
 	/** This element selects "All Types" from the Specimen Type drop down */
 	@FindBy (xpath = "//label[contains(text(), 'All Types')]")
 	public WebElement specimenTypeDropDownAllTypeValue;
+	
+	/** This element clicks on the Cancer Type drop down on the Biospecimen tab*/
+	@FindBy (xpath = "//button[contains(text(),'Cancer Type')]")
+	public WebElement cancerType;
+	
+	/** This element clicks on the "All Types" from the cancerType drop down */
+	@FindBy (xpath = "//label[contains(text(),'All Cancers')]")
+	public WebElement cancerTypeDropDownAllTypeValues;
+	
+	/** This element clicks on the Cohort drop down from the Biospecimen tab */
+	@FindBy (xpath = "//button[contains(text(),'Select Cohorts')]")
+	public WebElement cohortType;
+	
+	/** This element clicks on the "All Cohorts" from the Cohorts drop down */
+	@FindBy (xpath = "//label[contains(text(),'All Cohorts')]")
+	public WebElement cohortTypeDropDownAllTypeValue;
+	
+	@FindBy (xpath = "//input[@name='submitBtn']")
+	public WebElement biospecimenSubmitBtn;
+	
+	@FindBy (xpath = "//a[contains(text(),'Clear All')]")
+	public WebElement biospecimenClearAllBtn;
 
 	public CEDCDSearchCohortsPage() {
 		PageFactory.initElements(WebDriverUtils.webDriver, this);
