4605ca2d59109b942e50e621ac52589fc45b94b8
Mata Rodriguez <matarodriguezko@nci-02133367-ml.nih.gov>
2020-11-25 15:25:48 -0500

    CEDCD -    Added the Biospecimen Counts - Cancer Types Sorted Alphabetically automated test case and local configs for Jenkins

---
+++ src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDBiospecimenCountsAlphabeticalCancerTypeStepImp.java
@@ -0,0 +1,41 @@
+package AnalysisTools.CEDCD.StepsImplementation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.Assert;
+
+import com.nci.automation.utils.CucumberLogUtils;
+
+import appsCommon.PageInitializer;
+
+public class CEDCDBiospecimenCountsAlphabeticalCancerTypeStepImp extends PageInitializer {
+
+	public void sortCancerTypeAlphabetically() {
+
+		ArrayList<String> cancerTypeDropDown = new ArrayList<String>();
+
+		for (int i = 0; i < 28; i++) {
+
+			cancerTypeDropDown.add(cedcdBiospecimenCountsPage.cancerTypeValues.get(i).getText());
+
+		}
+		ArrayList<String> cancerTypeDropDownSortAlphabetically = new ArrayList<String>();
+
+		for (int i = 0; i < 28; i++) {
+
+			cancerTypeDropDownSortAlphabetically.add(cedcdBiospecimenCountsPage.cancerTypeValues.get(i).getText());
+
+		}
+
+		Collections.sort(cancerTypeDropDownSortAlphabetically);
+
+		System.out.println(cancerTypeDropDown);
+		System.out.println(cancerTypeDropDownSortAlphabetically);
+
+		Assert.assertEquals(cancerTypeDropDownSortAlphabetically, cancerTypeDropDown);
+		CucumberLogUtils.logScreenShot();
+
+	}
+
+}
