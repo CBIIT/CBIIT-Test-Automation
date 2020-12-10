affc3bea0bd2f1d1dcec1dfe006ccaee917819bb
Shomir Uddin <shomir.uddin@nih.gov>
2020-12-10 15:02:06 -0500

    Tabs into one feature

--- src/test/java/AnalysisTools/PLCO/Features/About.feature
+++
@@ -1,8 +0,0 @@
-Feature: About 
-
-@Smoke
-Scenario: User is able to go to source code from About page
-Given user on PLCO homepage
-When user clicks on About page
-And clicks on Github
-Then user goes to source code from About page
--- src/test/java/AnalysisTools/PLCO/Steps/ExploreGWASSteps.java
+++ src/test/java/AnalysisTools/PLCO/Steps/ExploreGWASSteps.java
@@ -1,11 +1,12 @@
 package AnalysisTools.PLCO.Steps;
 
-import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import com.nci.automation.utils.CucumberLogUtils;
-import com.nci.automation.utils.MiscUtils;
 import com.nci.automation.web.CommonUtils;
 import com.nci.automation.web.EnvUtils;
 import com.nci.automation.web.JavascriptUtils;
@@ -31,9 +32,13 @@ public class ExploreGWASSteps extends PageInitializer {
 	
 	@When("selects phenotype as BMI at baseline")
 	public void selects_phenotype_as_BMI_at_baseline(){
+		try {
+			Thread.sleep(3000);
+		}catch(Exception e) {}
 		exploreGWASPage.btnPlusAnthropometricMeasures.click();
 		exploreGWASPage.btnPlusBMI.click();
 		exploreGWASPage.chkBMIBaseLine.click();
+		
 	}
 	
 	@When("selects ancestry as East Asian All")
@@ -51,12 +56,70 @@ public class ExploreGWASSteps extends PageInitializer {
 		exploreGWASPage.dspManhattanPlot.isDisplayed(); 
 		int width = exploreGWASPage.dspManhattanPlot.getSize().getWidth();
 		int height = exploreGWASPage.dspManhattanPlot.getSize().getHeight();
+		System.out.println(width+":"+height);
+		try {
+			Thread.sleep(5000);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		Actions action = new Actions(WebDriverUtils.webDriver);
-		action.moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/5, height/5).click().build().perform();
+		action.moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/10, height/10).click().build().perform();
+		System.out.println("Height of element:"+exploreGWASPage.dspGenePlot.getSize().getHeight());
+		while(exploreGWASPage.dspGenePlot.getSize().getHeight()==0) {			
+			try {
+				Thread.sleep(2000);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("Height of element:"+exploreGWASPage.dspGenePlot.getSize().getHeight());
+			action.clickAndHold(exploreGWASPage.dspManhattanPlot).moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/10, height/10).release(exploreGWASPage.dspManhattanPlot).build().perform();
+			try {
+				Thread.sleep(2000);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}	
+		}
+		int hight = 280;//exploreGWASPage.dspGenePlot.getSize().getHeight();
+		 width = 500;//exploreGWASPage.dspGenePlot.getSize().getWidth();
+		System.out.println("Width Of Gene Plot:"+width);
+		System.out.println("Height Of Gene Plot:"+hight);
+		//while(!isElementDisplayed()) {
+			boolean flag = false;
+			for(int i=10;i<hight;i++) {
+				for(int j=10;j<width;j++) {
+					action.moveToElement(exploreGWASPage.dspGenePlot,j,i).build().perform();
+					try {
+						Thread.sleep(1500);
+					}catch(Exception e) {}
+					System.out.println("X-Value: "+j+", Y-Value"+i);
+					if(isElementDisplayed()) {
+						flag = true;
+						break;
+					}
+					j = j+5;
+				}
+				if(flag)
+					break;
+				i = i+5;
+			}
+			
+		//}
+		
 		JavascriptUtils.scrollIntoView(exploreGWASPage.dspManhattanPlot);
 		CucumberLogUtils.logScreenShot();
 	}
-	
+	public boolean isElementDisplayed() {
+		System.out.println(" Waiting for tool tip element");
+		boolean flag = false;
+		WebDriverUtils.webDriver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		try {
+			flag = WebDriverUtils.webDriver.findElement(By.xpath("//div[contains(@class, 'tool') and contains(@style, 'block')]/div")).isDisplayed();
+		}catch(Exception e) {
+			flag = false;
+		}
+		WebDriverUtils.webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return flag;
+	}
 	
 	@When("selects Pairwise Plots")
 	public void selects_Pairwise_Plots(){
