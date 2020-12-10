531d0500909b3d8a53dc1281d5f309e8f2daf71e
Shomir Uddin <shomir.uddin@nih.gov>
2020-12-10 15:02:06 -0500

    Tabs into one feature

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunLDLinkSmokeTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunLDLinkSmokeTest.java
@@ -21,7 +21,7 @@ import cucumber.api.junit.Cucumber;
 		, "rerun:target/failed.txt","pretty"}
 		, features="src/test/java/AnalysisTools/LDLink/Features"
 		, glue="AnalysisTools.LDLink.Steps"
-		, tags="@Smoke"
+		, tags="@Smoke1"
 		, dryRun = false
 		, monochrome=true
 		, strict = true
