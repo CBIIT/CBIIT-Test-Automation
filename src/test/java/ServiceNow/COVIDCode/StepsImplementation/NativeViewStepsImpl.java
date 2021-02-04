32d7073de9b0c3538b5aa6abc7f5d8de04416301
iamdez99 <diegojuarezbusiness@gmail.com>
2021-02-04 18:49:08 -0500

    Updated COVIDcode test scenarios and Added new SEER Data Access Request Test Scenarios

--- src/test/java/ServiceNow/COVIDCode/StepsImplementation/NativeViewStepsImpl.java
+++ src/test/java/ServiceNow/COVIDCode/StepsImplementation/NativeViewStepsImpl.java
@@ -1,21 +1,21 @@
 package ServiceNow.COVIDCode.StepsImplementation;
 
-import org.openqa.selenium.Keys;
 
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import com.nci.automation.utils.CucumberLogUtils;
 import com.nci.automation.utils.MiscUtils;
 import com.nci.automation.web.CommonUtils;
 import com.nci.automation.web.JavascriptUtils;
-import com.nci.automation.web.WebDriverUtils;
 
 import appsCommon.PageInitializer;
 
 public class NativeViewStepsImpl extends PageInitializer {
 
 	/***
-	 * The below method navigates and fills out demographics section for an enrollment
-	 * questionnaire in Native View
-	 * Values are hard coded
+	 * The below method navigates and fills out demographics section for an
+	 * enrollment questionnaire in Native View Values are hard coded
 	 */
 
 	public void nativeViewFillOutDemographicsSection() {
@@ -52,9 +52,8 @@ public class NativeViewStepsImpl extends PageInitializer {
 	}
 
 	/***
-	 * The below method navigates to and fills out the Symptomology section for an enrollment
-	 * questionnaire in native view
-	 * values are hard coded
+	 * The below method navigates to and fills out the Symptomology section for an
+	 * enrollment questionnaire in native view values are hard coded
 	 */
 
 	public void nativeViewFillOutSymptomologySection() {
@@ -71,9 +70,8 @@ public class NativeViewStepsImpl extends PageInitializer {
 	}
 
 	/***
-	 * The below navigates to and fills out the Exposures and Risk Factors section for an
-	 * enrollment questionnaire in native view
-	 * Values are hard coded
+	 * The below navigates to and fills out the Exposures and Risk Factors section
+	 * for an enrollment questionnaire in native view Values are hard coded
 	 */
 	public void nativeViewFillOutExposuresAndRiskFactorsSection() {
 		nativeViewEnrollmentViewPage.nativeViewEnrollmentViewExposuredAndRiskFactorsTab.click();
@@ -125,8 +123,8 @@ public class NativeViewStepsImpl extends PageInitializer {
 
 	/***
 	 * The below method navigates to the Participant Events Section in native view
-	 * for an enrollment questionnaire and entering a consent date
-	 * values are hard coded
+	 * for an enrollment questionnaire and entering a consent date values are hard
+	 * coded
 	 */
 	public void nativeViewConsentingParticipantEventsSection() {
 		nativeViewEnrollmentViewPage.nativeViewEnrollmentViewParticipantEventsTab.click();
@@ -135,9 +133,10 @@ public class NativeViewStepsImpl extends PageInitializer {
 				nativeViewEnrollmentViewPage.nativeViewEnrollmentViewParticipantEventsConsentedDate, "01/20/2021");
 
 	}
-	
+
 	/***
-	 * This method navigates to a new COVIDcode enrollment questionnaire form in native view
+	 * This method navigates to a new COVIDcode enrollment questionnaire form in
+	 * native view
 	 */
 	public void nativeViewNavigateToCovidCodeEnrollmentQuestionnaire() {
 		CommonUtils.waitForVisibility(nativeViewEnrollementsPage.filterNavigator);
@@ -146,17 +145,54 @@ public class NativeViewStepsImpl extends PageInitializer {
 		CucumberLogUtils.logScreenShot();
 		nativeViewEnrollementsPage.nativeViewPanelNavigatorCovidCodeEnrollmentsLink.click();
 	}
-	
+
 	/***
-	 * This method consents, chooses group ID, and fills out patient basic information on the enrollment questionnaire form in Native View
-	 * values are hard coded
+	 * This method navigates to a new COVIDcode Specimens form in native view
+	 */
+	public void nativeViewNavigateToCovidCodeSpecimens() {
+		CommonUtils.waitForVisibility(nativeViewEnrollementsPage.filterNavigator);
+		nativeViewEnrollementsPage.filterNavigator.sendKeys("CovidCode App");
+		MiscUtils.sleep(5000);
+		CucumberLogUtils.logScreenShot();
+		nativeViewEnrollementsPage.nativeViewPanelNavigatorSpecimensLink.click();
+	}
+
+	/***
+	 * This method navigates to a new COVIDcode follow up questionnaire form in
+	 * native view
+	 */
+	public void nativeViewNavigateToCovidCodeFollowUpQuestionnaire() {
+		CommonUtils.waitForVisibility(nativeViewEnrollementsPage.filterNavigator);
+		nativeViewEnrollementsPage.filterNavigator.sendKeys("CovidCode App");
+		MiscUtils.sleep(5000);
+		CucumberLogUtils.logScreenShot();
+		nativeViewEnrollementsPage.nativeViewPanelNavigatorCovidCodeFollowUpsLink.click();
+	}
+
+	/***
+	 * This method navigates to a new COVIDcode My Inquiry Tracking Records in
+	 * native view
+	 */
+	public void nativeViewNavigateToCovidCodeMyInquiryTrackingRecords() {
+		CommonUtils.waitForVisibility(nativeViewEnrollementsPage.filterNavigator);
+		nativeViewEnrollementsPage.filterNavigator.sendKeys("CovidCode App");
+		MiscUtils.sleep(5000);
+		CucumberLogUtils.logScreenShot();
+		nativeViewEnrollementsPage.nativeViewPanelNavigatorMyInquiryTrackingRecordsLink.click();
+	}
+
+	/***
+	 * This method consents, chooses group ID, and fills out patient basic
+	 * information on the enrollment questionnaire form in Native View values are
+	 * hard coded
 	 */
 	public void nativeViewConsentingWithGroupIDandFillPatientBasicInfo(String group, String consent) {
 		CommonUtils.switchToFrame(nativeViewEnrollementsPage.NativeViewFrame);
 		MiscUtils.sleep(2000);
 		nativeViewEnrollementsPage.covidCodeEnrollmentsNewButton.click();
 		CucumberLogUtils.logScreenShot();
-		CommonUtils.selectDropDownValue(group, nativeViewEnrollmentViewPage.nativeViewEnrollmentViewUserGroupIDDropDown);
+		CommonUtils.selectDropDownValue(group,
+				nativeViewEnrollmentViewPage.nativeViewEnrollmentViewUserGroupIDDropDown);
 		CommonUtils.selectDropDownValue(consent, nativeViewEnrollmentViewPage.nativeViewEnrollmentViewConsentDropDown);
 		nativeViewEnrollmentViewPage.nativeViewEnrollmentViewPatientLastNameField.sendKeys("AutomatedLN");
 		nativeViewEnrollmentViewPage.nativeViewEnrollmentViewPatientFirstName.sendKeys("AutomatedFN");
@@ -169,24 +205,82 @@ public class NativeViewStepsImpl extends PageInitializer {
 		nativeViewEnrollmentViewPage.nativeViewEnrollmentViewZipCode.sendKeys("20850");
 		CucumberLogUtils.logScreenShot();
 	}
-	
+
 	/***
 	 * This method verifies an enrollment form has been successfully submitted
-	 * Values are hard coded
-	 * This method is to follow any enrollment that has been submitted with the email address email@automatedtest.com
+	 * Values are hard coded This method is to follow any enrollment that has been
+	 * submitted with the email address email@automatedtest.com
 	 */
 	public void nativeViewVerifyingEnrollmentQuestionnaireWasSubmitted() {
 		MiscUtils.sleep(2000);
 		/*
-		 * Clicking on the 'All' breadcrumb is necessary to properly search without added filters - refer to UI for further review
+		 * Clicking on the 'All' breadcrumb is necessary to properly search without
+		 * added filters - refer to UI for further review
 		 */
 		nativeViewEnrollmentViewPage.nativeViewAllBreadCrumbLink.click();
 		MiscUtils.sleep(1000);
-		CommonUtils.selectDropDownValue("Contact Email Address", nativeViewEnrollementsPage.covidCodeEnrollmentsSearchDropDown);
+		CommonUtils.selectDropDownValue("Contact Email Address",
+				nativeViewEnrollementsPage.covidCodeEnrollmentsSearchDropDown);
 		nativeViewEnrollementsPage.covidCodeEnrollmentsSearchTextBox.sendKeys("email@automatedtest.com");
 		MiscUtils.sleep(2000);
 		nativeViewEnrollementsPage.covidCodeEnrollmentsSearchTextBox.sendKeys(Keys.RETURN);
 		CucumberLogUtils.logScreenShot();
+		MiscUtils.sleep(2000);
+		CucumberLogUtils.logScreenShot();
+	}
+
+	/***
+	 * This method search Nurses in Assigned to text field on the My Inquiry
+	 * Tracking in Native View and do assertion by selected values Values are hard
+	 * coded
+	 */
+	public void nativeViewMyInquiryTrackingSearchNursesinAssignedToTextField() {
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys("Alan Orpia");
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys("Janet Bracci");
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys("Laura Harney");
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys("Lisa Leathwood");
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys("Maureen Risch");
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys("Stephanie Steinbart");
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+				.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+		.sendKeys("Kathryn Nichols");
+        nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactUnlockAssignedToSearchTextField
+		.sendKeys(Keys.ENTER);
+		MiscUtils.sleep(2000);
+		String[] exp = { "Alan Orpia", "Janet Bracci", "Laura Harney", "Lisa Leathwood", "Maureen Risch",
+				"Stephanie Steinbart", "Kathryn Nichols"};
+		for (WebElement act : nativeViewEnrollementsPage.myInquiryTrackingRecordsStudyTeamContactListOfAssignedToDD) {
+			boolean match = false;
+			for (int i = 0; i < exp.length; i++) {
+				if (act.getText().equals(exp[i])) {
+					match = true;
+				}
+			}
+			Assert.assertTrue(match);
+		}
 		MiscUtils.sleep(2000);
 		CucumberLogUtils.logScreenShot();
 	}
