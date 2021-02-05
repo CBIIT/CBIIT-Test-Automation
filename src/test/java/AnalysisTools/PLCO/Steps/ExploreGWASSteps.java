b3d6e3e85c661c777cf54f2483df8c56c2dccf76
Shomir Uddin <shomir.uddin@nih.gov>
2021-02-05 10:43:28 -0500

    jenkins

--- src/test/java/AnalysisTools/PLCO/Steps/ExploreGWASSteps.java
+++ src/test/java/AnalysisTools/PLCO/Steps/ExploreGWASSteps.java
@@ -17,6 +17,7 @@ import appsCommon.PageInitializer;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 
 public class ExploreGWASSteps extends PageInitializer {
 	
@@ -37,7 +38,7 @@ public class ExploreGWASSteps extends PageInitializer {
 		}catch(Exception e) {}
 		exploreGWASPage.btnPlusAnthropometricMeasures.click();
 		exploreGWASPage.btnPlusBMI.click();
-		exploreGWASPage.chkBMIBaseLine.click();
+		exploreGWASPage.chkBMIBaseLineSummary.click();
 		
 	}
 	
@@ -53,57 +54,60 @@ public class ExploreGWASSteps extends PageInitializer {
 	
 	@Then("Manhattan Plot displays")
 	public void Manhattan_Plot_displays(){
-		exploreGWASPage.dspManhattanPlot.isDisplayed(); 
-		int width = exploreGWASPage.dspManhattanPlot.getSize().getWidth();
-		int height = exploreGWASPage.dspManhattanPlot.getSize().getHeight();
-		System.out.println(width+":"+height);
 		try {
-			Thread.sleep(5000);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		Actions action = new Actions(WebDriverUtils.webDriver);
-		action.moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/10, height/10).click().build().perform();
-		System.out.println("Height of element:"+exploreGWASPage.dspGenePlot.getSize().getHeight());
-		while(exploreGWASPage.dspGenePlot.getSize().getHeight()==0) {			
-			try {
-				Thread.sleep(2000);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println("Height of element:"+exploreGWASPage.dspGenePlot.getSize().getHeight());
-			action.clickAndHold(exploreGWASPage.dspManhattanPlot).moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/10, height/10).release(exploreGWASPage.dspManhattanPlot).build().perform();
-			try {
-				Thread.sleep(2000);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}	
-		}
-		int hight = 280;//exploreGWASPage.dspGenePlot.getSize().getHeight();
-		 width = 500;//exploreGWASPage.dspGenePlot.getSize().getWidth();
-		System.out.println("Width Of Gene Plot:"+width);
-		System.out.println("Height Of Gene Plot:"+hight);
-		//while(!isElementDisplayed()) {
-			boolean flag = false;
-			for(int i=10;i<hight;i++) {
-				for(int j=10;j<width;j++) {
-					action.moveToElement(exploreGWASPage.dspGenePlot,j,i).build().perform();
-					try {
-						Thread.sleep(1500);
-					}catch(Exception e) {}
-					System.out.println("X-Value: "+j+", Y-Value"+i);
-					if(isElementDisplayed()) {
-						flag = true;
-						break;
-					}
-					j = j+5;
-				}
-				if(flag)
-					break;
-				i = i+5;
-			}
-			
-		//}
+			Thread.sleep(3000);
+		}catch(Exception e) {}
+		exploreGWASPage.dspManhattanPlot.isDisplayed(); 
+//		int width = exploreGWASPage.dspManhattanPlot.getSize().getWidth();
+//		int height = exploreGWASPage.dspManhattanPlot.getSize().getHeight();
+//		System.out.println(width+":"+height);
+//		try {
+//			Thread.sleep(5000);
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//		Actions action = new Actions(WebDriverUtils.webDriver);
+//		action.moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/10, height/10).click().build().perform();
+//		System.out.println("Height of element:"+exploreGWASPage.dspGenePlot.getSize().getHeight());
+//		while(exploreGWASPage.dspGenePlot.getSize().getHeight()==0) {			
+//			try {
+//				Thread.sleep(2000);
+//			}catch(Exception e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println("Height of element:"+exploreGWASPage.dspGenePlot.getSize().getHeight());
+//			action.clickAndHold(exploreGWASPage.dspManhattanPlot).moveToElement(exploreGWASPage.dspManhattanPlot).moveByOffset(width/10, height/10).release(exploreGWASPage.dspManhattanPlot).build().perform();
+//			try {
+//				Thread.sleep(2000);
+//			}catch(Exception e) {
+//				e.printStackTrace();
+//			}	
+//		}
+//		int hight = 280;//exploreGWASPage.dspGenePlot.getSize().getHeight();
+//		 width = 500;//exploreGWASPage.dspGenePlot.getSize().getWidth();
+//		System.out.println("Width Of Gene Plot:"+width);
+//		System.out.println("Height Of Gene Plot:"+hight);
+//		//while(!isElementDisplayed()) {
+//			boolean flag = false;
+//			for(int i=10;i<hight;i++) {
+//				for(int j=10;j<width;j++) {
+//					action.moveToElement(exploreGWASPage.dspGenePlot,j,i).build().perform();
+//					try {
+//						Thread.sleep(1500);
+//					}catch(Exception e) {}
+//					System.out.println("X-Value: "+j+", Y-Value"+i);
+//					if(isElementDisplayed()) {
+//						flag = true;
+//						break;
+//					}
+//					j = j+5;
+//				}
+//				if(flag)
+//					break;
+//				i = i+5;
+//			}
+//			
+//		//}
 		
 		JavascriptUtils.scrollIntoView(exploreGWASPage.dspManhattanPlot);
 		CucumberLogUtils.logScreenShot();
@@ -153,8 +157,58 @@ public class ExploreGWASSteps extends PageInitializer {
 		CucumberLogUtils.logScreenShot();
 	}
 	
+	@When("selects variant lookup tab")
+	public void selects_variant_lookup_tab() {
+		exploreGWASPage.lnkVariantLookUp.click();
+	}
+	
+	@When("writes variant as {string}")
+	public void writes_variant_as(String variant) {
+		exploreGWASPage.actVariantTextBox.sendKeys(variant);
+	}
 	
 	
+	@When("selects phenotype as BMI at baseline for variant lookup")
+	public void selects_phenotype_as_BMI_at_baseline_for_variant_lookup() {
+		exploreGWASPage.btnPlusAnthropometricMeasures.click();
+		exploreGWASPage.btnPlusBMI.click();
+		exploreGWASPage.chkBMIBaseLineVariantLookup.click();
+	}
+	
+	@When("selects ancestry as European for variant lookup")
+	public void selects_ancestry_as_European_for_variant_lookup() {
+		CommonUtils.selectDropDownValue(exploreGWASPage.selAncestryVariantLookup, "european");
+	}
+	
+	@When("selects sex as all for variant lookup")
+	public void selects_sex_as_all_for_variant_lookup() {
+		CommonUtils.selectDropDownValue(exploreGWASPage.selSexVariantLookup, "all");
+	}
+	
+	
+	@Then("results table displays")
+	public void results_table_displays() {
+		JavascriptUtils.scrollIntoView(exploreGWASPage.dspVariantLookupResults);
+		Assert.assertTrue(exploreGWASPage.dspVariantLookupResults.isDisplayed());
+		
+	}
+	
+	@When("selects Phenotype Correlations tab")
+	public void selects_Phenotype_Correlations_tab() {
+		exploreGWASPage.lnkPhenotypeCorrelationsTab.click();
+	}
+	
+	@When("selects phenotype as BMI at baseline for phenotype correlation")
+	public void selects_phenotype_as_BMI_at_baseline_for_phenotype_correlation() {
+		exploreGWASPage.btnPlusAnthropometricMeasures.click();
+		exploreGWASPage.btnPlusBMI.click();
+		exploreGWASPage.chkBMIBaseLinePhenotypeCorrelations.click();
+	}
+	
+	@When("selects phenotype as height BQ for phenotype correlation")
+	public void selects_phenotype_as_height_BQ_for_phenotype_correlation() {
+		exploreGWASPage.chkHeightPhenotypeCorrelation.click();
+	}
 	
 	
 	
