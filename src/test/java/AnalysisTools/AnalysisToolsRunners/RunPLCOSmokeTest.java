dae09a7e9f0250b50d4ea3a9838f9847cea6f01a
Shomir Uddin <shomir.uddin@nih.gov>
2021-02-05 10:45:30 -0500

    smoke

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunPLCOSmokeTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunPLCOSmokeTest.java
@@ -19,7 +19,7 @@ import cucumber.api.junit.Cucumber;
 		, "rerun:target/failed.txt","pretty"}
 		, features={"src/test/java/AnalysisTools/PLCO/Features"}
 		, glue="AnalysisTools.PLCO.Steps"
-		, tags="@Smoke1"
+		, tags="@Smoke"
 		, dryRun = false
 		, monochrome=true
 		, strict = true
