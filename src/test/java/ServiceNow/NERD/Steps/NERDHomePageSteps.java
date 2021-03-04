18ee4b5cc59c74d40a712e26a6fea290fc94dd93
iamdez99 <diegojuarezbusiness@gmail.com>
2021-03-04 13:12:25 -0500

    ..

--- src/test/java/ServiceNow/NERD/Steps/NERDHomePageSteps.java
+++ src/test/java/ServiceNow/NERD/Steps/NERDHomePageSteps.java
@@ -1,14 +1,5 @@
 package ServiceNow.NERD.Steps;
 
-import org.openqa.selenium.By;
-
-import com.nci.automation.web.CommonUtils;
-import com.nci.automation.web.WebDriverUtils;
-
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
 public class NERDHomePageSteps {
 
 }
