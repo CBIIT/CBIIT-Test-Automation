f15a22608f9561e77e9a1e58e63bd4ea84d012f6
iamdez99 <diegojuarezbusiness@gmail.com>
2021-03-04 13:12:25 -0500

    ..

--- src/test/java/ServiceNow/COVIDCode/Pages/EnrollmentQuestionnairePage.java
+++ src/test/java/ServiceNow/COVIDCode/Pages/EnrollmentQuestionnairePage.java
@@ -1,15 +1,12 @@
 package ServiceNow.COVIDCode.Pages;
 
 import java.util.List;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.nci.automation.web.CommonUtils;
 import com.nci.automation.web.WebDriverUtils;
 
-import appsCommon.PageInitializer;
 
 public class EnrollmentQuestionnairePage extends CommonUtils {
 
