9d528082c1aeffe1c1a7c4b7df191ea8462be531
Shomir Uddin <shomir.uddin@nih.gov>
2021-03-04 15:15:34 -0500

    "Framework- Mobile Configuration Android"

--- src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomeAndLandingPageSteps.java
+++ src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomeAndLandingPageSteps.java
@@ -12,12 +12,17 @@ import cucumber.api.java.en.When;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class LDLinkHomeAndLandingPageSteps extends PageInitializer {
 	
 	
-
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "/CBIIT-Test-Automation/src/main/java/com/drivers/chromedriver"); 
+	WebDriver driver = new ChromeDriver(); 
+}
 
 
 
