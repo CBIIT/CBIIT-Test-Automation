0587c71e054febb9fd500bb9bc36653fba09b16a
Shomir Uddin <shomir.uddin@nih.gov>
2021-03-22 14:17:47 -0400

    CEDCD-Add Cohort

--- src/test/java/AnalysisTools/CEDCD/Pages/CEDCDAdminPage.java
+++ src/test/java/AnalysisTools/CEDCD/Pages/CEDCDAdminPage.java
@@ -14,17 +14,61 @@ public class CEDCDAdminPage {
 		
 	}
 	
+	/**================================================= Public Site Login Webelements =======================================================*/
+	
+	/** NIH Login Button */
+	@FindBy(linkText = "NIH Login")
+	public WebElement nihLoginBtn; 
+	
+	/** External Login Button */
+	@FindBy(linkText = "External Login")
+	public WebElement extLoginBtn;
+	
+	
 	/**================================================= Internal Login Page WebElements =======================================================*/
 	
+	
+	/** Username Textbox */
 	@FindBy(id="USER")
 	public WebElement internalUsernameTxtBox; 
 	
+	/** Password Textbox */
 	@FindBy(id="PASSWORD")
 	public WebElement internalPasswordTxtBox; 
 	
+	/** Submit Button */
 	@FindBy(xpath = "//*[@type='submit']")
-	public WebElement submit; 
+	public WebElement loginSubmitBtn; 
+	
+	/**================================================= Manage Cohort Page WebElements =======================================================*/
+	
+	/** Add New Cohort Hyperlink */
+	@FindBy(linkText = "Add New Cohort")
+	public WebElement addNewCohortlnk; 
 	
 	
-
+	/**================================================= Add New Cohort Page WebElements =======================================================*/
+	
+	/** Cohort Name Textbox */
+	@FindBy(xpath= "(//*[@class='form-control'])[1]")
+	public WebElement cohortNameTxtBox; 
+	
+	/** Cohort Acroynm Textbox */
+	@FindBy(xpath = "(//*[@class='form-control'])[2]")
+	public WebElement cohortAcroynmTxtBox; 
+	
+	/** Cohort Owner Textbox */
+	@FindBy(xpath = "//*[@class=' css-1hwfws3']")
+	public WebElement cohortOwnerTxtBox; 
+	 
+	/** Notes Textbox */
+	@FindBy(id = "cu_message")
+	public WebElement notesTxtBox; 
+	
+	/** Submit Button */
+	@FindBy(linkText = "Submit")
+	public WebElement cohortSubmitBtn; 
+	
+	
+	
 }
