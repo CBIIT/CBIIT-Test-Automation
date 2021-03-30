61e5503a93585bab1547c8c18bcbb0c81f1ad7b0
Shomir Uddin <shomir.uddin@nih.gov>
2021-03-30 12:33:38 -0400

    target

--- target/html-reports/cucumber-default-report/report.js
+++ target/html-reports/cucumber-default-report/report.js
@@ -1,26 +1,77 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/ServiceNow/COVIDCode/Features/ServicePortalSurvey.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/AnalysisTools/CEDCD/Features/CEDCDQuestionnaire.feature");
 formatter.feature({
-  "name": "Volunteer Survey",
-  "description": "  Description: Volunteer Survery scenarios including both Service Portal and Native View",
+  "name": "Questionnaire",
+  "description": "",
   "keyword": "Feature"
 });
-formatter.scenario({
-  "name": "Volunteer Survey Self Submission_1 - Service Portal",
+formatter.scenarioOutline({
+  "name": "Admin able to add cohort",
   "description": "",
-  "keyword": "Scenario",
+  "keyword": "Scenario Outline",
   "tags": [
     {
-      "name": "@Smoke"
+      "name": "@Progression"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user is on the CEDCD homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "the user is logged in as Admin",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "user selects add new Cohort",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "enters Cohort Name as \"\u003cCohort Name\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "enters Cohort Owner",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "enters \"\u003cNotes\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "clicks submit",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "Cohort is added",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "Cohort Name",
+        "Cohort Acronym",
+        "Notes"
+      ]
     },
     {
-      "name": "@Sprint9"
-    },
-    {
-      "name": "@COVID-109"
-    },
-    {
-      "name": "@juarezds"
-    },
+      "cells": [
+        "Second Automated Cohort",
+        "SAC",
+        "This Cohort Will Be Automated"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Admin able to add cohort",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
     {
       "name": "@Progression"
     }
@@ -30,274 +81,85 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "a user is on the COVIDCode home page",
+  "name": "the user is on the CEDCD homepage",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "ServicePortalSurveySteps.a_user_is_on_the_COVIDCode_home_page()"
-});
-formatter.write("2021-03-24 22:59:33: Screenshot: ");
-formatter.embedding("image/png", "embedded0.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selects the option to Volunteer",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selects_the_option_to_Volunteer()"
+  "location": "CEDCDQuestionnaireSteps.the_user_is_on_the_CEDCD_homepage()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the \"COVIDcode Volunteer Survey\" page displays with the option to start the survey",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_page_displays_with_the_option_to_start_the_survey(String)"
-});
-formatter.write("2021-03-24 22:59:36: Screenshot: ");
-formatter.embedding("image/png", "embedded1.png");
-formatter.write("2021-03-24 22:59:37: Screenshot: ");
-formatter.embedding("image/png", "embedded2.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "starting the survey",
+  "name": "the user is logged in as Admin",
   "keyword": "When "
 });
 formatter.match({
-  "location": "ServicePortalSurveySteps.starting_the_survey()"
+  "location": "CEDCDQuestionnaireSteps.the_user_is_logged_in_as_Admin()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the user selects option to fill out the survery for themselves",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_user_selects_option_to_fill_out_the_survery_for_themselves()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the question \"Have you tested positive for SARS-CoV-2 (the novel coronavirus)?\" populates",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.populates(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting No",
+  "name": "user selects add new Cohort",
   "keyword": "When "
 });
 formatter.match({
-  "location": "ServicePortalSurveySteps.selecting_No()"
+  "location": "CEDCDQuestionnaireSteps.user_selects_add_new_Cohort()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the question \"How was the sample taken?\" displays",
+  "name": "enters Cohort Name as \"Second Automated Cohort\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEDCDQuestionnaireSteps.enters_Cohort_Name_as(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "enters Cohort Owner",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEDCDQuestionnaireSteps.enters_Cohort_Owner()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "enters \"This Cohort Will Be Automated\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEDCDQuestionnaireSteps.enters(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "clicks submit",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEDCDQuestionnaireSteps.clicks_submit()"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"link text\",\"selector\":\"Submit\"}\n  (Session info: chrome\u003d89.0.4389.90)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027NCI-02134309-ML\u0027, ip: \u0027fe80:0:0:0:1427:6f46:8945:846f%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15.7\u0027, java.version: \u002714.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 89.0.4389.90, chrome: {chromedriverVersion: 88.0.4324.96 (68dba2d8a0b14..., userDataDir: /var/folders/ls/pc4m1ymd4n1...}, goog:chromeOptions: {debuggerAddress: localhost:52244}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 7b52479f9e4e8189e0875f186d224ae7\n*** Element info: {Using\u003dlink text, value\u003dSubmit}\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByLinkText(RemoteWebDriver.java:380)\n\tat org.openqa.selenium.By$ByLinkText.findElement(By.java:220)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\n\tat com.sun.proxy.$Proxy20.click(Unknown Source)\n\tat AnalysisTools.CEDCD.Steps.CEDCDQuestionnaireSteps.clicks_submit(CEDCDQuestionnaireSteps.java:76)\n\tat ✽.clicks submit(file:src/test/java/AnalysisTools/CEDCD/Features/CEDCDQuestionnaire.feature:12)\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "Cohort is added",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "ServicePortalSurveySteps.the_question_displays(String)"
+  "location": "CEDCDQuestionnaireSteps.Cohort_is_added()"
 });
 formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting \"Nasal or throat swab\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selecting(String)"
-});
-formatter.write("2021-03-24 22:59:42: Screenshot: ");
-formatter.embedding("image/png", "embedded3.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting \"Rapid testing (up to 2 hour turnaround)\" checkbox",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selecting_checkbox(String)"
-});
-formatter.write("2021-03-24 22:59:42: Screenshot: ");
-formatter.embedding("image/png", "embedded4.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the \"Are you a National Institute of Health Employee?\" question displays",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_question_display(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting No from drop down",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selecting_No_from_drop_down()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting No for being able to provide a copy of test report",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selecting_No_for_being_able_to_provide_a_copy_of_test_report()"
-});
-formatter.write("2021-03-24 22:59:46: Screenshot: ");
-formatter.embedding("image/png", "embedded5.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting No for for ever being hospitalized",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selecting_No_for_for_ever_being_hospitalized()"
-});
-formatter.write("2021-03-24 22:59:48: Screenshot: ");
-formatter.embedding("image/png", "embedded6.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "clicking next",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.clicking_next()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the \"Contact Information \" displays where the user is able to enter their contact information",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_displays_where_the_user_is_able_to_enter_their_contact_information(String)"
-});
-formatter.write("2021-03-24 22:59:48: Screenshot: ");
-formatter.embedding("image/png", "embedded7.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "entering required information",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.entering_required_information()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "clicking next to proceed",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.clicking_next_to_proceed()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the \"How did you hear about us? \" page displays with a question regarding the discovery of the survey",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_page_displays_with_a_question_regarding_the_discovery_of_the_survey(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "selecting option \"I prefer not to answer\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.selecting_option(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "clicking Submit",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.clicking_Submit()"
-});
-formatter.write("2021-03-24 22:59:58: Screenshot: ");
-formatter.embedding("image/png", "embedded8.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the \"Your survey has been submitted successfully!\" page displays",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_page_displays(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user is able to see a message \"If you have any questions, please feel free to email us at covidcode@mail.nih.gov\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_user_is_able_to_see_a_message_followed_the_by_email_address(String)"
-});
-formatter.write("2021-03-24 23:00:07: Screenshot: ");
-formatter.embedding("image/png", "embedded9.png");
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user selects OK",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_user_selects_OK()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user is prompted back to the COVIDCode Study Home Page",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ServicePortalSurveySteps.the_user_is_prompted_back_to_the_COVIDCode_Study_Home_Page()"
-});
-formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
   "status": "passed"
