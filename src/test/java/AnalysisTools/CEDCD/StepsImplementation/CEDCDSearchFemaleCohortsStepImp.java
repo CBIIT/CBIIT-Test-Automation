99c0eb09d6314deb2aee5fe16fc268d834ab31cc
Kevin Mata <matakevin1777@gmail.com>
2021-04-20 11:54:52 -0400

    CEDCD - Updated test cases

--- src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDSearchFemaleCohortsStepImp.java
+++ src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDSearchFemaleCohortsStepImp.java
@@ -1,18 +1,14 @@
 package AnalysisTools.CEDCD.StepsImplementation;
 
+import java.util.List;
 import org.junit.Assert;
-
+import org.openqa.selenium.WebElement;
 import com.nci.automation.utils.CucumberLogUtils;
 import com.nci.automation.utils.MiscUtils;
-import com.nci.automation.web.EnvUtils;
 import com.nci.automation.web.JavascriptUtils;
-import com.nci.automation.web.WebDriverUtils;
-import com.nci.automation.xceptions.TestingException;
-
 import appsCommon.PageInitializer;
 
 public class CEDCDSearchFemaleCohortsStepImp extends PageInitializer {
-
 
 	/**
 	 * This method filters the cohorts by selecting female and clicking "Search
@@ -22,7 +18,8 @@ public class CEDCDSearchFemaleCohortsStepImp extends PageInitializer {
 		cedcdSearchCohortsPage.searchCohortsGenderBtn.click();
 		cedcdSearchCohortsPage.searchCohortsFemaleOption.click();
 		cedcdSearchCohortsPage.searchCohortResultBtn.click();
-		cedcdSearchCohortsPage.showVariablesCollectedInCohortStudy.click();
+		cedcdSearchCohortsPage.searchCohortSelectAllCheckbox.click();
+		cedcdSearchCohortsPage.viewSelectedCohortDataButton.click();
 
 	}
 
@@ -30,11 +27,28 @@ public class CEDCDSearchFemaleCohortsStepImp extends PageInitializer {
 	 * This method verifies the correct number of cohorts are returned, highlights
 	 * the results in red and logs screenshot
 	 */
-	public void femaleResultsReturnedWithRedBorderScreenshot() {
-		MiscUtils.sleep(2000);
-		Assert.assertTrue(cedcdSearchCohortsPage.numberOfSearchResultReturned.getText().contains("53"));
-		JavascriptUtils.drawRedBorder(cedcdSearchCohortsPage.numberOfSearchResultReturned);
-		CucumberLogUtils.logScreenShot();
+//	public void femaleResultsReturnedWithRedBorderScreenshot() {
+//		MiscUtils.sleep(2000);
+//		Assert.assertTrue(cedcdSearchCohortsPage.numberOfSearchResultReturned.getText().contains("53"));
+//		JavascriptUtils.drawRedBorder(cedcdSearchCohortsPage.numberOfSearchResultReturned);
+//		CucumberLogUtils.logScreenShot();
+//	}
+
+	public void comparingResultsReturnedtoFemaleAndAllValues() {
+
+		List<WebElement> listOfWebElements = cedcdSearchCohortsPage.returnedResultsForFemaleFilter;
+
+		System.out.println(listOfWebElements.size());
+
+		for (WebElement webElement : listOfWebElements) {
+			String locator = webElement.getText();
+			if (locator.endsWith("All")) {
+
+			} else if (locator.endsWith("Female")) {
+
+			}
+			System.out.println("Returned result: " + locator);
+		}
 	}
 
 }
