dbd7916730910a8e8c069b2b3f97cba8ff669078
Alena Nekrashevich <alena.nekrashevich@nih.gov>
2021-05-02 22:35:24 -0400

    ...

--- src/test/java/ServiceNow/AppTracker/Utils/CommonUtil.java
+++ src/test/java/ServiceNow/AppTracker/Utils/CommonUtil.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import com.nci.automation.web.WebDriverUtils;
 
 public class CommonUtil {
-	
-		
+
+	public static void waitBrowser(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+
 	}
-
-	
-
-	
-
+}
