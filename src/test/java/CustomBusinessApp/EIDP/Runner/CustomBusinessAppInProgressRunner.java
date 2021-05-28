91303a3c252b27835683fbbd94a83edbe4a9f302
iamdez99 <diegojuarezbusiness@gmail.com>
2021-05-28 18:45:35 -0400

    Solved EID Issues

--- src/test/java/CustomBusinessApp/EIDP/Runner/CustomBusinessAppInProgressRunner.java
+++ src/test/java/CustomBusinessApp/EIDP/Runner/CustomBusinessAppInProgressRunner.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 		, "rerun:target/failed.txt","pretty"}
 		, features="src/test/java/CustomBusinessApp/EIDP/Features"
 		, glue="CustomBusinessApp.EIDP.Steps"
-    	, tags="@Regression"
+    	, tags="@InitiatesIDP"
     	, dryRun =false,
 		junit = "--step-notifications"
 		
