1b5fff5a498f5b4c34c0ac0ba2c506c9073a1489
iamdez99 <diegojuarezbusiness@gmail.com>
2021-06-28 17:09:30 -0400

    CEDCD - Updated Public Site test case - changed Biospecimen Count condition

--- src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDBiospecimenCountsAlphabeticalCancerTypeStepImp.java
+++ src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDBiospecimenCountsAlphabeticalCancerTypeStepImp.java
@@ -15,14 +15,14 @@ public class CEDCDBiospecimenCountsAlphabeticalCancerTypeStepImp extends PageIni
 
 		ArrayList<String> cancerTypeDropDown = new ArrayList<String>();
 
-		for (int i = 0; i < cedcdBiospecimenCountsPage.cancerTypeValues.size() - 1; i++) {
+		for (int i = 0; i < cedcdBiospecimenCountsPage.cancerTypeValues.size() - 3; i++) {
 
 			cancerTypeDropDown.add(cedcdBiospecimenCountsPage.cancerTypeValues.get(i).getText());
 
 		}
 		ArrayList<String> cancerTypeDropDownSortAlphabetically = new ArrayList<String>();
 
-		for (int i = 0; i < cedcdBiospecimenCountsPage.cancerTypeValues.size() - 1; i++) {
+		for (int i = 0; i < cedcdBiospecimenCountsPage.cancerTypeValues.size() - 3; i++) {
 
 			cancerTypeDropDownSortAlphabetically.add(cedcdBiospecimenCountsPage.cancerTypeValues.get(i).getText());
 
