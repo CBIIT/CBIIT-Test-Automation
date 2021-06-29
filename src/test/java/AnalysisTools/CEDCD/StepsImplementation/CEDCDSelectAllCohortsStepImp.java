157e682817c9509e2914f04affa7f615e722cc57
alenaminsk <79877036+alenaminsk@users.noreply.github.com>
2021-06-29 15:20:17 -0400

    Update CEDCDSelectAllCohortsStepImp.java

--- src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDSelectAllCohortsStepImp.java
+++ src/test/java/AnalysisTools/CEDCD/StepsImplementation/CEDCDSelectAllCohortsStepImp.java
@@ -1,22 +1,10 @@
 package AnalysisTools.CEDCD.StepsImplementation;
 
-<<<<<<< Updated upstream
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
-=======
-<<<<<<< HEAD
-import org.junit.Assert;
-=======
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Assert;
-import org.openqa.selenium.WebElement;
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
 
 import com.nci.automation.utils.CucumberLogUtils;
 import com.nci.automation.web.JavascriptUtils;
@@ -29,29 +17,6 @@ public class CEDCDSelectAllCohortsStepImp extends PageInitializer {
 		JavascriptUtils.clickByJS(cedcdSearchCohortsPage.searchCohortResultBtn);
 	}
 
-<<<<<<< Updated upstream
-	/** This method clicks on the check box that selects all cohorts and the view selected cohort data button */
-=======
-<<<<<<< HEAD
-	/** This element clicks on the check box that selects all cohorts and logs a screenshot */
->>>>>>> Stashed changes
-	public void clickOnSelectAllCheckbox() {
-		JavascriptUtils.clickByJS(cedcdSearchCohortsPage.searchCohortSelectAllCheckbox);
-//		JavascriptUtils.scrollIntoView(cedcdSearchCohortsPage.bCFRCohort);
-		CucumberLogUtils.logScreenShot();
-		cedcdSearchCohortsPage.viewSelectedCohortDataButton.click();
-	}
-
-	/** This method returns the number of cohorts, highlights the number in red and logs screenshot */
-	public void verifyNumberOfCohortsReturned() {
-<<<<<<< Updated upstream
-=======
-	Assert.assertTrue(cedcdSearchCohortsPage.numberOfSearchResultReturned.getText().contains("61"));
-		JavascriptUtils.drawRedBorder(cedcdSearchCohortsPage.numberOfSearchResultReturned);
-		CucumberLogUtils.logScreenShot();
-
-	}
-=======
 	/** This method clicks on the check box that selects all cohorts and the view selected cohort data button */
 	public void clickOnSelectAllCheckbox() {
 		JavascriptUtils.clickByJS(cedcdSearchCohortsPage.searchCohortSelectAllCheckbox);
@@ -62,7 +27,6 @@ public class CEDCDSelectAllCohortsStepImp extends PageInitializer {
 
 	/** This method returns the number of cohorts, highlights the number in red and logs screenshot */
 	public void verifyNumberOfCohortsReturned() {
->>>>>>> Stashed changes
 		
 		List<WebElement> listOfWebElements = cedcdSearchCohortsPage.returnedResultsFilter;
 		System.out.println(listOfWebElements.size());
@@ -87,9 +51,5 @@ public class CEDCDSelectAllCohortsStepImp extends PageInitializer {
 			cedcdSearchCohortsPage.numberOfSearchResultReturned.getText().contains(textResults);
 			Assert.assertEquals(cedcdSearchCohortsPage.numberOfSearchResultReturned.getText().contains(textResults), true);
 			}
-<<<<<<< Updated upstream
-=======
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
 
 }
