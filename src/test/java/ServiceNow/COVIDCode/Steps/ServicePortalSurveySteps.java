ed7f99f06a40d9dd76392ff6a941353e645f4703
alenaminsk <79877036+alenaminsk@users.noreply.github.com>
2021-06-29 16:30:13 -0400

    Update ServicePortalSurveySteps.java

--- src/test/java/ServiceNow/COVIDCode/Steps/ServicePortalSurveySteps.java
+++ src/test/java/ServiceNow/COVIDCode/Steps/ServicePortalSurveySteps.java
@@ -421,12 +421,4 @@ public class ServicePortalSurveySteps extends PageInitializer {
 
 
 
-<<<<<<< Updated upstream
 }
-=======
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
