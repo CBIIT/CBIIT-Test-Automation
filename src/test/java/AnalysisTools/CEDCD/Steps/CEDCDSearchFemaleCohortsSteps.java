7dc87bbd4fda1666495f893a641a2d2c46be0d36
alenaminsk <79877036+alenaminsk@users.noreply.github.com>
2021-06-29 15:17:55 -0400

    Update CEDCDSearchFemaleCohortsSteps.java

--- src/test/java/AnalysisTools/CEDCD/Steps/CEDCDSearchFemaleCohortsSteps.java
+++ src/test/java/AnalysisTools/CEDCD/Steps/CEDCDSearchFemaleCohortsSteps.java
@@ -6,14 +6,7 @@ import appsCommon.PageInitializer;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-<<<<<<< Updated upstream
 import junit.framework.Assert;
-=======
-<<<<<<< HEAD
-=======
-import junit.framework.Assert;
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
 
 public class CEDCDSearchFemaleCohortsSteps extends PageInitializer {
 
@@ -32,14 +25,7 @@ public class CEDCDSearchFemaleCohortsSteps extends PageInitializer {
 
 	}
 
-<<<<<<< Updated upstream
 
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
 	@When("the user filters by females")
 	public void the_user_filters_by_females() {
 
@@ -47,16 +33,6 @@ public class CEDCDSearchFemaleCohortsSteps extends PageInitializer {
 
 	}
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-	@Then("{int} results return for Female Cohort Filter Result")
-	public void results_return_for_Female_Cohort_Filter_Result(int numberOfFemalesReturned) {
-
-		cedcdSearchFemaleCohortsStepImp.femaleResultsReturnedWithRedBorderScreenshot();
-
-=======
->>>>>>> Stashed changes
 	@Then("the results returned are for Female or All Cohorts")
 	public void the_results_returned_are_for_Female_or_All_Cohorts() {
 		
@@ -64,10 +40,6 @@ public class CEDCDSearchFemaleCohortsSteps extends PageInitializer {
 		
 //		System.out.println(cedcdSearchCohortsPage.returnedResultsForFemaleFilter);
 		
-<<<<<<< Updated upstream
-=======
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
 	}
 
 }
