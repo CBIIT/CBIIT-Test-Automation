29d84d2e7412fbcad01c844d1553b84c17d3e9dc
alenaminsk <79877036+alenaminsk@users.noreply.github.com>
2021-06-29 14:08:48 -0400

    Update PasswordEncrypter.java

--- src/main/java/com/nci/automation/dao/PasswordEncrypter.java
+++ src/main/java/com/nci/automation/dao/PasswordEncrypter.java
@@ -10,25 +10,9 @@ import com.nci.automation.utils.EncryptionUtils;
 public class PasswordEncrypter {
 
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-		String pwd = "";
-		System.out.println(EncryptionUtils.encrypt(pwd));
->>>>>>> Stashed changes
 
 		String pwd = "TRPTRPTRP1029e$$";
 		System.out.println(EncryptionUtils.encrypt(pwd));
 	}
-<<<<<<< Updated upstream
 
-=======
-=======
-
-		String pwd = "";
-		System.out.println(EncryptionUtils.encrypt(pwd));
-	}
-
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
->>>>>>> Stashed changes
 }
