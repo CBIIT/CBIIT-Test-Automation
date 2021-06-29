8028a5f612e935c10a1f523d811897ca70849ea9
alenaminsk <79877036+alenaminsk@users.noreply.github.com>
2021-06-29 15:52:02 -0400

    Update GeneralInformationSteps.java

--- src/test/java/CustomBusinessApp/EIDP/Steps/GeneralInformationSteps.java
+++ src/test/java/CustomBusinessApp/EIDP/Steps/GeneralInformationSteps.java
@@ -3,18 +3,15 @@ package CustomBusinessApp.EIDP.Steps;
 import java.util.Map;
 
 import com.nci.automation.utils.CucumberLogUtils;
-<<<<<<< Updated upstream
 import com.nci.automation.web.CommonUtils;
 import com.nci.automation.web.JavascriptUtils;
-=======
->>>>>>> Stashed changes
 
 import CustomBusinessApp.EIDP.Util.CommonUtil;
 import appsCommon.PageInitializer;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.cucumber.datatable.DataTable;
 
-<<<<<<< Updated upstream
 public class GeneralInformationSteps extends PageInitializer {
 
 	@When("User fills mandatory fields in general information")
@@ -26,34 +23,15 @@ public class GeneralInformationSteps extends PageInitializer {
 	@When("User fills mandatory fields in general information section")
 	public void user_fills_mandatory_fields_in_general_information_section(io.cucumber.datatable.DataTable data)
 			throws Exception {
-=======
-public class GeneralInformationSteps extends PageInitializer{
-
-	@When("User fills mandatory fields in general information")
-	public void fillGeneralInformation() throws Exception{
-		generalInformationStepImpl.fillGeneralInformation();
-		CucumberLogUtils.logScreenShot("Generarl info completed");
-	}
-	
-	@When("User fills general information")
-	public void fillGeneralInformation(DataTable data) throws Exception {
->>>>>>> Stashed changes
 		Map<String, String> requestData = CommonUtil.getMapFromDataTable(data);
 		generalInformationStepImpl.enterPrimaryMentor(requestData.get("Primary Mentor"));
 		generalInformationStepImpl.enterLabBranchName(requestData.get("Lab Driector name"));
 		String coPrimaryMentor = requestData.get("Co Primary Mentor");
 		System.out.println("coPrimaryMentor = " + coPrimaryMentor);
-<<<<<<< Updated upstream
 		if (coPrimaryMentor != null && !"".equals(coPrimaryMentor)) {
 			generalInformationStepImpl.doYouHaveCoPrimaryMentory(true);
 			generalInformationStepImpl.selectCoPrimaryMentor(coPrimaryMentor);
 
-=======
-		if(coPrimaryMentor != null && !"".equals(coPrimaryMentor)) {
-			generalInformationStepImpl.doYouHaveCoPrimaryMentory(true);
-			generalInformationStepImpl.selectCoPrimaryMentor(coPrimaryMentor);
-			
->>>>>>> Stashed changes
 		} else {
 			generalInformationStepImpl.doYouHaveCoPrimaryMentory(false);
 		}
@@ -63,11 +41,7 @@ public class GeneralInformationSteps extends PageInitializer{
 		CucumberLogUtils.logScreenShot("General Info");
 		generalInformationStepImpl.clickOnSaveAndContinueButton();
 	}
-<<<<<<< Updated upstream
 
-=======
-	
->>>>>>> Stashed changes
 	@When("User selects primary mentor as \"([^\"]*)\"")
 	public void selectPrimaryMentor(String name) throws Exception {
 		generalInformationStepImpl.enterPrimaryMentor(name);
@@ -75,16 +49,28 @@ public class GeneralInformationSteps extends PageInitializer{
 		CommonUtil.waitBrowser(2000);
 		generalInformationStepImpl.clickOnSaveAndContinueButton();
 	}
-<<<<<<< Updated upstream
+	@Then("User selects primary mentor as exactly {string}")
+	public void user_selects_primary_mentor_as_exactly(String name) {
+		generalInformationStepImpl.enterPrimaryMentor(name);
+		CommonUtil.waitBrowser(2000);
+		generalInformationStepImpl.clickOnSaveAndContinueButton();
+		
+	}
+	@When("User will click on Search tab")
+	public void user_will_click_on_Seacrh_tab() {
+	    CommonUtils.click(generalInformationPage.searchSection);
+	}
+
+
 
 	@When("User clicks on Proceed button")
 	public void user_clicks_on_Proceed_button() {
 		CommonUtils.click(generalInformationPage.ProceedButton);
 	}
+	@Then("User will click on General Information section")
+	public void user_will_click_on_General_Information_section() {
+		CommonUtils.click(generalInformationPage.clickOnCompletedGeneralInfobutton);
+	}
+
 
 }
-=======
-	
-	
-}
->>>>>>> Stashed changes
