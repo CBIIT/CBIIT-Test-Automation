777bd0643863449c85becc2f5bf266cfc6c58d65
alenaminsk <79877036+alenaminsk@users.noreply.github.com>
2021-06-29 16:46:53 -0400

    Update NativeViewLoginImpl.java

--- src/test/java/appsCommon/NativeViewLoginImpl.java
+++ src/test/java/appsCommon/NativeViewLoginImpl.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.By;
 import com.nci.automation.utils.CucumberLogUtils;
 import com.nci.automation.utils.MiscUtils;
 import com.nci.automation.web.EnvUtils;
-<<<<<<< HEAD
-=======
 import com.nci.automation.web.JavascriptUtils;
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
 import com.nci.automation.web.WebDriverUtils;
 import com.nci.automation.xceptions.TestingException;
 
@@ -19,14 +16,10 @@ public class NativeViewLoginImpl extends PageInitializer {
 		WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("nativeview"));
 		MiscUtils.sleep(2000);
 		CucumberLogUtils.logScreenShot();
-<<<<<<< HEAD
-		WebDriverUtils.webDriver.findElement(By.linkText("Log in")).click();
-=======
 		JavascriptUtils.clickByJS(WebDriverUtils.webDriver.findElement(By.linkText("Log in")));
 		/** ----- Review Safari locators ----- */
 		//JavascriptUtils.clickByJS(WebDriverUtils.webDriver.findElement(By.xpath("//a[@href='/ncisp?id=nci_landing']")));
 		//WebDriverUtils.webDriver.findElement(By.linkText("Log in")).click();
->>>>>>> 8f4e2d8f13237294dd99ecbb9a77e6b945dae2eb
 		MiscUtils.sleep(2000);
 		CucumberLogUtils.logScreenShot();
 		loginImpl.loginToITrust();
