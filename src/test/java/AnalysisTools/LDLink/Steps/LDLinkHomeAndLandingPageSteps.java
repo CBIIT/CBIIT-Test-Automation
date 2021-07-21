24a4a265aed5995fde59ec1d56a0780ee5ab80e9
iamdez99 <diegojuarezbusiness@gmail.com>
2021-07-21 15:12:53 -0400

    Sample push

--- src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomeAndLandingPageSteps.java
+++ src/test/java/AnalysisTools/LDLink/Steps/LDLinkHomeAndLandingPageSteps.java
@@ -15,18 +15,13 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class LDLinkHomeAndLandingPageSteps extends PageInitializer {
-	
-	
-public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "/CBIIT-Test-Automation/src/main/java/com/drivers/chromedriver"); 
-	WebDriver driver = new ChromeDriver(); 
-}
 
-
-
-
-
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/CBIIT-Test-Automation/src/main/java/com/drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Printing test line");
+	}
 
 }
