20490a9a181c3a0f9af776e15dfb4746611d510d
iamdez99 <diegojuarezbusiness@gmail.com>
2021-09-14 16:59:54 -0400

    SEER ODS - Updated test scenarios

--- src/main/java/com/nci/automation/web/WebDriverUtils.java
+++ src/main/java/com/nci/automation/web/WebDriverUtils.java
@@ -90,20 +90,20 @@ public class WebDriverUtils {
 				ChromeOptions chromeOptions = new ChromeOptions();
 				if (headless.equalsIgnoreCase("true")) {
 //					chromeOptions.setHeadless(true);
-//					chromeOptions.addArguments("--headless");
+					chromeOptions.addArguments("--headless");
 					chromeOptions.addArguments("window-size=1920,1080");
 					chromeOptions.addArguments("--disable-dev-shm-usage");
 //					chromeOptions.addArguments("--disable-tmp-usage");
 					chromeOptions.addArguments("--no-sandbox");
-//					chromeOptions.addArguments("--disable-infobars");
-//					chromeOptions.addArguments("--disable-extensions");
-//					chromeOptions.addArguments("--disable-gpu");
-//					chromeOptions.addArguments("enable-automation");
-//					chromeOptions.addArguments("--disable-browser-side-navigation");
-//					chromeOptions.addArguments("--profile-directory=Default");
-//					chromeOptions.addArguments("--disable-setuid-sandbox");
-//					chromeOptions.addArguments("--user-data-dir=~/.config/google-chrome");
-//					chromeOptions.setExperimentalOption("useAutomationExtension", false);
+					chromeOptions.addArguments("--disable-infobars");
+					chromeOptions.addArguments("--disable-extensions");
+					chromeOptions.addArguments("--disable-gpu");
+					chromeOptions.addArguments("enable-automation");
+					chromeOptions.addArguments("--disable-browser-side-navigation");
+					chromeOptions.addArguments("--profile-directory=Default");
+					chromeOptions.addArguments("--disable-setuid-sandbox");
+					chromeOptions.addArguments("--user-data-dir=~/.config/google-chrome");
+					chromeOptions.setExperimentalOption("useAutomationExtension", false);
 					webDriver = new ChromeDriver(chromeOptions);
 					System.out.println(chromeOptions.getVersion());
 				} else {
