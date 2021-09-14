0bc0dc348c99d3aa9ad713570907970ad4153eb1
iamdez99 <diegojuarezbusiness@gmail.com>
2021-09-14 16:52:42 -0400

    SEER ODS - Updated test scenarios

--- src/main/java/com/nci/automation/web/WebDriverUtils.java
+++ src/main/java/com/nci/automation/web/WebDriverUtils.java
@@ -92,7 +92,7 @@ public class WebDriverUtils {
 //					chromeOptions.setHeadless(true);
 //					chromeOptions.addArguments("--headless");
 					chromeOptions.addArguments("window-size=1920,1080");
-//					chromeOptions.addArguments("--disable-dev-shm-usage");
+					chromeOptions.addArguments("--disable-dev-shm-usage");
 //					chromeOptions.addArguments("--no-sandbox");
 //					chromeOptions.addArguments("--disable-infobars");
 //					chromeOptions.addArguments("--disable-extensions");
