ce0269791b43e354707d39a48f6bb65cd1af3464
iamdez99 <diegojuarezbusiness@gmail.com>
2021-10-05 19:23:14 -0400

    CHARMS - RASopathy Study Eligibility Questionnaire

--- src/test/java/ServiceNow/CHARMS/Features/myRASScreenerSubmissions.feature
+++ src/test/java/ServiceNow/CHARMS/Features/myRASScreenerSubmissions.feature
@@ -4,24 +4,43 @@ Feature: myRAS Screener Submissions
 
   @Progression @juarezds
   Scenario: Proxy Screener Submission with data verification
-    Given a user is on the RASopathies Longitudinal Cohort Study login page
+    Given a proxy is on the RASopathies Longitudinal Cohort Study login page
     And logs in via Okta with username "charmsras4@yahoo.com" and password "RASTest2021$$"
     And clicks on Eligibility Questionnaire to begin questionnaire
     And clicks next after reviewing the STUDY INTRODCTION
     And selects I am completing this form for someone else option 
     And enters First Name "Automated", Middle Name "Middle", Last Name "Test" for What is the name of the person who may be eligible for this study?
     And enters First Name "FirstNameTest", Middle Name "MiddleNameTest", Last Name "lastNameTest" for What is your name?
-    And enters "Entering Other Relationship" for What is your relationship to automated name?
-    And selects Yes option for Are you the legal guardian of automated name?
-    And clicks next for The next set of questions will collect basic information about automated name. page 
-    And selects a Date of Birth from the calendar picker
-    And selects Male option for Sex assigned at birth
-    And selects Yes option for being adopted
-    And selects Yes for still being alive
-    And selects Yes option for living in the United States
+    And enters "Entering Other Relationship" for What is your relationship to the participant?
+    And selects Yes option for Are you the legal guardian of the participant?
+    And clicks next for The next set of questions will collect basic information about the participant. page 
+    And selects participants Date of Birth from the calendar picker
+    And selects Male option for participants Sex assigned at birth
+    And selects Yes option for participant being adopted
+    And selects Yes for participant still being alive
+    And selects Yes option for participant currently living in the United States
     And enters mailing address for where study materials can be sent, street "9609 Medical Center Dr" city "Rockville" state "Maryland" zip code "20850"
-    And enters email address "automatedTest@nci.gov"
-    And enters email address "automatedTest@nci.gov" to confirm email address
-    And enters home phone number "703-687-5816", cell phone number "703-687-5816", work phone number "703-687-5816" and selects cell phone number as Preferred Contact
-    And selects Not Hispanic/Latino option for Ethnicity of proxy
-     
+    And enters proxy email address "automatedTest@nci.gov"
+    And enters proxy email address "automatedTest@nci.gov" to confirm email address
+    And enters proxy home phone number "703-687-5816", cell phone number "703-687-5816", work phone number "703-687-5816" and selects cell phone number as Preferred Contact
+    And selects Not Hispanic/Latino option for Ethnicity of participant
+    And enters "Entering other Race" for other when selecting Race of participant
+    And selects Other option and enters "This is a test" for participant being in other research study
+    And proceeds with providing information for medical diagnosis
+    And enters "Diagnosed with other conditions" for participant being diagnosed with any of the listed conditions
+    And selects Yes option for participant being diagnosed with cancer
+    And for participants primary cancers diagnosed selects cancer types "Leukemia", "Brain tumor (ie, glioma)", "Rhabdomyosarcoma", "Skin cancer (ie, melanoma)" and selects age at diagnosis for primary cancers entered for all options "20", "21", "22", "23" and year of diagnosis "2000", "2001", "2002", "2003" and selects Yes option for currently receiving treatment 
+    And selects Yes option for participant being diagnosed with a Rasopathy
+    And selects Other and enters "Entering Other RASopathy" for selecting a specific RASopathy diagnosed of the participant
+    And enters participants age in years "20" of diagnosis 
+    And enters participants year of diagnosis "2010"
+    And proceeds with providing genetic testing information
+    And selects Yes option to family members of participants having been diagnosed with a RASopathy
+    And selects Yes option to participant having had genetic testing
+    And selects Yes option to participant having a copy of genetic test results
+    And selects Upload to this questionnaire options for providing genetic test results of participant
+    And uploads PDF file of genetic test results of participant
+    
+    
+    
+    
--- src/test/java/ServiceNow/CHARMS/Pages/RASopathyQuestionnairePage.java
+++ src/test/java/ServiceNow/CHARMS/Pages/RASopathyQuestionnairePage.java
@@ -126,101 +126,402 @@ public class RASopathyQuestionnairePage {
 	/**
 	 * --------------- END OF Sex of "name" assigned at birth PAGE ---------------
 	 */
-	
+
 	/** --------------- BEGINNING OF Is "name" adopted? PAGE --------------- */
-	
+
 	/* Yes option */
 	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID11~1']")
-	public WebElement isProxyAdoptedYesOption;
-	
-	
+	public WebElement isParticipantAdoptedYesOption;
+
 	/** --------------- END OF Is "name" adopted? PAGE --------------- */
-	
+
 	/** --------------- BEGINNING OF Is "name" still alive? PAGE --------------- */
-	
+
 	/* Yes option */
 	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID12~1']")
-	public WebElement isProxyStillAliveYesOption;
-	
-	
+	public WebElement isParticipantStillAliveYesOption;
+
 	/** --------------- END OF Is "name" still alive? PAGE --------------- */
-	
-	/** --------------- BEGINNING OF Does "name" currently live in the United States? PAGE --------------- */
-	
+
+	/**
+	 * --------------- BEGINNING OF Does "name" currently live in the United States?
+	 * PAGE ---------------
+	 */
+
 	/* Yes option */
 	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID112~1']")
-	public WebElement doesProxyLiveInUnitedStatesYesOption;
-	
-	
-	/** --------------- END OF Does "name" currently live in the United States? PAGE --------------- */
-	
-	/** --------------- BEGINNING OF Does "name" currently live in the United States? PAGE --------------- */
-	
+	public WebElement doesParticipantLiveInUnitedStatesYesOption;
+
+	/**
+	 * --------------- END OF Does "name" currently live in the United States? PAGE
+	 * ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Does "name" currently live in the United States?
+	 * PAGE ---------------
+	 */
+
 	/* Street text box */
 	@FindBy(xpath = "//input[@name='QR~QID14~4~TEXT']")
 	public WebElement addressForStudyMaterialsToBeSentStreetTextBox;
-	
+
 	/* City text box */
 	@FindBy(xpath = "//input[@name='QR~QID14~6~TEXT']")
 	public WebElement addressForStudyMaterialsToBeSentCityTextBox;
-	
+
 	/* State text box */
 	@FindBy(xpath = "//input[@name='QR~QID14~7~TEXT']")
 	public WebElement addressForStudyMaterialsToBeSentStateTextBox;
-	
+
 	/* Zip Code text box */
 	@FindBy(xpath = "//input[@name='QR~QID14~8~TEXT']")
 	public WebElement addressForStudyMaterialsToBeSentZipCodeTextBox;
-	
-	
-	/** --------------- END OF Does "name" currently live in the United States? PAGE --------------- */
-	
-	/** --------------- BEGINNING OF What is your email address? PAGE --------------- */
-	
+
+	/**
+	 * --------------- END OF Does "name" currently live in the United States? PAGE
+	 * ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF What is your email address? PAGE ---------------
+	 */
+
 	/* Email address text box */
 	@FindBy(xpath = "//input[@name='QR~QID15~TEXT']")
 	public WebElement whatIsYourEmailAddressTextBox;
-	
+
 	/** --------------- END OF What is your email address? PAGE --------------- */
-	
-	/** --------------- BEGINNING OF Please confirm your email address PAGE --------------- */
-	
+
+	/**
+	 * --------------- BEGINNING OF Please confirm your email address PAGE
+	 * ---------------
+	 */
+
 	/* Email address text box */
 	@FindBy(xpath = "//input[@name='QR~QID16~TEXT']")
 	public WebElement pleaseConfirmYourEmailAddressTextBox;
-	
-	/** --------------- END OF Please confirm your email address PAGE --------------- */
-	
-	/** --------------- BEGINNING OF Please list your phone numbers below. Please also select your preferred contact number. PAGE --------------- */
-	
-	/* Home phone number text box  */
+
+	/**
+	 * --------------- END OF Please confirm your email address PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Please list your phone numbers below. Please
+	 * also select your preferred contact number. PAGE ---------------
+	 */
+
+	/* Home phone number text box */
 	@FindBy(xpath = "//input[@name='QR~QID17~4~TEXT']")
 	public WebElement pleaseListHomePhoneNumberTextBox;
-	
-	/* Cell phone number text box  */
+
+	/* Cell phone number text box */
 	@FindBy(xpath = "//input[@name='QR~QID17~5~TEXT']")
 	public WebElement pleaseListCellPhoneNumberTextBox;
-	
-	/* Work phone number text box  */
+
+	/* Work phone number text box */
 	@FindBy(xpath = "//input[@name='QR~QID17~8~TEXT']")
 	public WebElement pleaseListWorkPhoneNumberTextBox;
-	
-	/* Preferred Contact cell phone number check box  */
+
+	/* Preferred Contact cell phone number check box */
 	@FindBy(xpath = "//tbody/tr[@class='ChoiceRow ReadableAlt ']/td[1]/label[1]")
 	public WebElement pleaseSelectCellPhonePreferredContactCheckBox;
-	
-	
-	/** --------------- END OF Please list your phone numbers below. Please also select your preferred contact number. PAGE --------------- */
-	
+
+	/**
+	 * --------------- END OF Please list your phone numbers below. Please also
+	 * select your preferred contact number. PAGE ---------------
+	 */
+
 	/** --------------- BEGINNING OF Ethnicity of "Name" PAGE --------------- */
-	
-	/* Not hispanic/latino option  */
+
+	/* Not hispanic/latino option */
 	@FindBy(xpath = "//span[contains(@class,'LabelWrapper')]//label[contains(@for,'QR~QID18~2')]")
-	public WebElement authenticityOfProxyNotHispanicLatinoOption;
-	
+	public WebElement authenticityOfParticipantNotHispanicLatinoOption;
+
 	/** --------------- END OF Ethnicity of "Name" PAGE --------------- */
+
+	/**
+	 * --------------- BEGINNING OF Race of "Name". Please select all that apply.
+	 * PAGE ---------------
+	 */
+
+	/* Other text box */
+	@FindBy(xpath = "//input[@title='Other']")
+	public WebElement raceOfParticipantOtherTextBox;
+
+	/**
+	 * --------------- END OF Race of "Name". Please select all that apply. PAGE
+	 * ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Is Automated Test a participant in any other
+	 * research study or registry group? Please specify. PAGE ---------------
+	 */
+
+	/* Other option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID115~3']")
+	public WebElement isParticipantInOtherResearchStudyOtherOption;
+
+	/* Other text box */
+	@FindBy(xpath = "//input[@title='Other']")
+	public WebElement isParticipantInOtherResearchStudyOtherTextBox;
+
+	/**
+	 * --------------- END OF Is Automated Test a participant in any other research
+	 * study or registry group? Please specify. PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Has Automated Test ever been diagnosed with the
+	 * following conditions? Select all that apply. If you do not see the exact
+	 * condition diagnosed, please select the closest answer. PAGE ---------------
+	 */
+
+	/* Other option */
+	@FindBy(xpath = "//span[contains(text(),': such as endocrine issues, bleeding disorders, se')]")
+	public WebElement hasParticipantBeenDiagnosedWithFollowingConditionsOtherOption;
+
+	/* Other text box */
+	@FindBy(xpath = "//input[contains(@title,'<strong>Other</strong>: such as endocrine issues, bleeding disorders, seizures, tethered cord')]")
+	public WebElement hasParticipantBeenDiagnosedWithFollowingConditionsOtherTextBox;
+
+	/**
+	 * --------------- END OF Has Automated Test ever been diagnosed with the
+	 * following conditions? Select all that apply. If you do not see the exact
+	 * condition diagnosed, please select the closest answer. PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Has "Name" ever been diagnosed with cancer? PAGE
+	 * ---------------
+	 */
+
+	/* Yes option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID32~1']")
+	public WebElement hasProxyBeenDiagnosedWithCancerYesOption;
+
+	/**
+	 * --------------- END OF Has "Name" ever been diagnosed with cancer? PAGE
+	 * ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Please complete the box below by selecting which
+	 * primary cancers were diagnosed and at what age and year they occurred. If
+	 * cancer spread from one place to another, please only indicate the original
+	 * cancers and not the number of sites where cancer spread. PAGE ---------------
+	 */
+
+	/* First drop down */
+	@FindBy(xpath = "//select[@name='QR~QID86#1~1']")
+	public WebElement primaryCancerFirstDropDown;
+
+	/* Second drop down */
+	@FindBy(xpath = "//select[@name='QR~QID86#1~2']")
+	public WebElement primaryCancerSecondDropDown;
+
+	/* Third drop down */
+	@FindBy(xpath = "//select[@name='QR~QID86#1~3']")
+	public WebElement primaryCancerThirdDropDown;
+
+	/* Fourth drop down */
+	@FindBy(xpath = "//select[@name='QR~QID86#1~4']")
+	public WebElement primaryCancerFourthDropDown;
+
+	/* Age at diagnosis first text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#3~1~1~TEXT']")
+	public WebElement ageAtDiagnosisFirstTextBox;
+
+	/* Age at diagnosis second text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#3~2~1~TEXT']")
+	public WebElement ageAtDiagnosisSecondTextBox;
+
+	/* Age at diagnosis third text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#3~3~1~TEXT']")
+	public WebElement ageAtDiagnosisThirdTextBox;
+
+	/* Age at diagnosis fourth text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#3~4~1~TEXT']")
+	public WebElement ageAtDiagnosisFourthTextBox;
+
+	/* Year of diagnosis first text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#4~1~1~TEXT']")
+	public WebElement yearOfDiagnosisFirstTextBox;
+
+	/* Year of diagnosis second text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#4~2~1~TEXT']")
+	public WebElement yearOfDiagnosisSecondTextBox;
+
+	/* Year of diagnosis third text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#4~3~1~TEXT']")
+	public WebElement yearOfDiagnosisThirdTextBox;
+
+	/* Year of diagnosis fourth text box */
+	@FindBy(xpath = "//input[@name='QR~QID86#4~4~1~TEXT']")
+	public WebElement yearOfDiagnosisFourthTextBox;
+
+	/* Currently receiving treatment first radio button */
+	@FindBy(xpath = "//label[@for='QR~QID86#5~1~1']")
+	public WebElement currentlyReceivingTreatmentFirstRadioButton;
+
+	/* Currently receiving treatment second radio button */
+	@FindBy(xpath = "//label[@for='QR~QID86#5~2~1']")
+	public WebElement currentlyReceivingTreatmentSecondRadioButton;
+
+	/* Currently receiving treatment first radio button */
+	@FindBy(xpath = "//label[@for='QR~QID86#5~3~1']")
+	public WebElement currentlyReceivingTreatmentThirdRadioButton;
+
+	/* Currently receiving treatment first radio button */
+	@FindBy(xpath = "//label[@for='QR~QID86#5~4~1']")
+	public WebElement currentlyReceivingTreatmentFourthRadioButton;
+
+	/**
+	 * --------------- END OF Please complete the box below by selecting which
+	 * primary cancers were diagnosed and at what age and year they occurred. If
+	 * cancer spread from one place to another, please only indicate the original
+	 * cancers and not the number of sites where cancer spread. PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Has "Name" been diagnosed with a RASopathy such
+	 * as Noonan syndrome, Noonan syndrome with multiple lentigines, Costello
+	 * syndrome, cardiofaciocutaneous syndrome, Legius syndrome, capillary
+	 * arteriovenous malformation syndrome, hereditary gingival fibromatosis or
+	 * SYNGAP1 syndrome? PAGE ---------------
+	 */
+
+	/* Yes option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID27~1']")
+	public WebElement hasProxyBeenDiagnosedWithaRasopathyYesOption;
+
+	/**
+	 * --------------- END OF Has "Name" been diagnosed with a RASopathy such as
+	 * Noonan syndrome, Noonan syndrome with multiple lentigines, Costello syndrome,
+	 * cardiofaciocutaneous syndrome, Legius syndrome, capillary arteriovenous
+	 * malformation syndrome, hereditary gingival fibromatosis or SYNGAP1 syndrome?
+	 * PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF For which specific RASopathy has Automated Test
+	 * been diagnosed? PAGE ---------------
+	 */
+
+	/* Other option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID28~10']")
+	public WebElement participantSpecificRasopathyOtherOption;
+
+	/* Other text box */
+	@FindBy(xpath = "//input[contains(@title,'Other')]")
+	public WebElement participantSpecificRasopathyOtherTextBox;
+
+	/**
+	 * --------------- END OF For which specific RASopathy has Automated Test been
+	 * diagnosed? PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Age (in years) at diagnosis PAGE ---------------
+	 */
+
+	/* Age (in years) at diagnosis text box */
+	@FindBy(xpath = "//input[@name='QR~QID29~TEXT']")
+	public WebElement participantAgeInYearsAtDiagnosis;
+
+	/**
+	 * --------------- END OF Age (in years) at diagnosis PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Year of diagnosis (yyyy) PAGE ---------------
+	 */
+
+	/* Age (in years) at diagnosis text box */
+	@FindBy(xpath = "//input[@name='QR~QID30~TEXT']")
+	public WebElement participantsYearOfDiagnosis;
+
+	/**
+	 * --------------- END OF Year of diagnosis (yyyy) PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Have any of "name" biological relatives been
+	 * diagnosed with a RASopathy? PAGE ---------------
+	 */
+
+	/* Yes option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID123~1']")
+	public WebElement participantsFamilyMembersBeenDiagnosedYesOption;
+
+	/**
+	 * --------------- END OF Have any of "name" biological relatives been diagnosed
+	 * with a RASopathy? PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Has participant ever had genetic testing? PAGE
+	 * ---------------
+	 */
+
+	/* Yes option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID36~1']")
+	public WebElement participantsEverHadGeneticTestingYesOption;
+
+	/**
+	 * --------------- END OF Has participant ever had genetic testing? PAGE
+	 * ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF Do you have a copy of the genetic test results?
+	 * PAGE ---------------
+	 */
+
+	/* Yes option */
+	@FindBy(xpath = "//span[@class='LabelWrapper']//label[@for='QR~QID102~1']")
+	public WebElement participantCopyOfGeneticTestResultsYesOption;
+
+	/**
+	 * --------------- END OF Do you have a copy of the genetic test results? PAGE
+	 * ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF How would you like to provide the genetic test
+	 * results to us? Please select one. On the next page, you will be provided with
+	 * where to send the information. PAGE ---------------
+	 */
+
+	/* Upload to this questionnaire option */
+	@FindBy(xpath = "//span[contains(@class,'LabelWrapper')]//label[contains(@for,'QR~QID125~2')]")
+	public WebElement participantUploadToThisQuestionnaireOption;
+
+	/**
+	 * --------------- END OF How would you like to provide the genetic test results
+	 * to us? Please select one. On the next page, you will be provided with where
+	 * to send the information. PAGE ---------------
+	 */
+
+	/**
+	 * --------------- BEGINNING OF If you would like to upload the genetic test
+	 * results, please do so here. Note: Only pdf files are accepted. PAGE
+	 * ---------------
+	 */
+
+	/* Drop files or click here to upload */
+	@FindBy(xpath = "//button[normalize-space()='Drop files or click here to upload']")
+	public WebElement dropFilesOrClickHereToUploadButton;
 	
+	/* Word document PDF text */
+	@FindBy(xpath = "//div[contains(@class,'FileUploadName')]")
+	public WebElement wordDocumentPDFtext;
 	
+	/**
+	 * --------------- END OF If you would like to upload the genetic test results,
+	 * please do so here. Note: Only pdf files are accepted. PAGE ---------------
+	 */
 
 	/** --------------- END OF RASopathy Questionnaire PAGE --------------- */
 
