399d27d3b318f5bc127b8c89aef1d466e9b18f52
matakevin <matakevin1777@gmail.com>
2022-01-19 19:16:48 -0500

    Charms - Fixing jenkins wait time issue

--- src/test/java/ServiceNow/CHARMS/Steps/myRASScreenerSubmissions.java
+++ src/test/java/ServiceNow/CHARMS/Steps/myRASScreenerSubmissions.java
@@ -1336,7 +1336,7 @@ public class myRASScreenerSubmissions extends PageInitializer {
 	@When("a ServiceNow user navigates to CHARMS Native view and opens records that are Waiting for Elegibility Review")
 	public void a_ServiceNow_user_navigates_to_CHARMS_Native_view_and_opens_records_that_are_Waiting_for_Elegibility_Review()
 			throws TestingException {
-		MiscUtils.sleep(2500);
+		MiscUtils.sleep(15000);
 		nativeViewLoginImpl.nativeViewLogin();
 		CommonUtils.waitForVisibility(nativeViewHomePage.nativeViewFilterNavigator);
 		nativeViewHomePage.nativeViewFilterNavigator.sendKeys("CHARMS");
