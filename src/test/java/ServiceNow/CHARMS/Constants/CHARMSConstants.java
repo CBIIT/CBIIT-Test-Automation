caa403a8751bd8c2d8255150d0ccec69f754a267
iamdez99 <diegojuarezbusiness@gmail.com>
2023-06-03 03:35:54 -0400

    Updating Ras screener submission

--- src/test/java/ServiceNow/CHARMS/Constants/CHARMSConstants.java
+++ src/test/java/ServiceNow/CHARMS/Constants/CHARMSConstants.java
@@ -1,7 +1,8 @@
 package ServiceNow.CHARMS.Constants;
 
 public class CHARMSConstants {
-	
-	public static final String RAS_SURVEY = System.getProperty("user.dir")+"/src/test/java/ServiceNow/CHARMS/Resources/RasSurveySelfSubmission.xlsx";
-	public static final String RAS_SURVEY_DATA = System.getProperty("user.dir")+"/src/test/java/ServiceNow/CHARMS/Resources/RasSurveyDataVerificationTableData.xlsx";
+
+    public static final String RAS_SURVEY = System.getProperty("user.dir") + "/src/test/java/ServiceNow/CHARMS/Resources/RasSurveySelfSubmission.xlsx";
+    public static final String RAS_SURVEY_DATA = System.getProperty("user.dir") + "/src/test/java/ServiceNow/CHARMS/Resources/RasSurveyDataVerificationTableData.xlsx";
+    public static final String RAS_SELF_SUBMISSION_SCENARIO_1 = System.getProperty("user.dir") + "/src/test/java/ServiceNow/CHARMS/Resources/SelfRasScreenerSubmissionScenario1.xlsx";
 }
