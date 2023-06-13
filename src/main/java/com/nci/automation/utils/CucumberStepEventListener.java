9038991712b6a9ced796e36891ba12c36df6862e
Uddin <uddins2@nih.gov>
2023-06-13 13:33:12 -0400

    minor fixes

--- src/main/java/com/nci/automation/utils/CucumberStepEventListener.java
+++ src/main/java/com/nci/automation/utils/CucumberStepEventListener.java
@@ -1,7 +1,6 @@
 package com.nci.automation.utils;
 
-//import cucumber.api.event.ConcurrentEventListener;
-//import cucumber.api.event.EventPublisher;
+
 import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.EventPublisher;
 
--- src/main/java/com/nci/automation/web/CommonUtils.java
+++ src/main/java/com/nci/automation/web/CommonUtils.java
@@ -18,7 +18,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.Assert;
 import com.nci.automation.utils.MiscUtils;
 
 /**
