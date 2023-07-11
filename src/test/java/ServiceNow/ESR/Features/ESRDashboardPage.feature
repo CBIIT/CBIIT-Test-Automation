1f7460a03d3d833597f3015ad0feb18b3e1e3e3d
Sarwar <sarwara2@nci-02277698-ml.nih.gov>
2023-07-11 19:34:50 -0400

    Put in sidedoor login methods into steps

--- src/test/java/ServiceNow/ESR/Features/ESRDashboardPage.feature
+++ src/test/java/ServiceNow/ESR/Features/ESRDashboardPage.feature
@@ -2,7 +2,7 @@ Feature: ESR Dashboard Page
 
     Description: This feature file contains scenarios relating to the ESR Dashboard page
 
-    @Progression @sarwarahmed1
+    @ESR-540 @ESR-541 @Progression @sarwarahmed1
     Scenario: Test an authenticated user access the ESR dashboard page
         Given a user is on the NCI at your service homepage
         When a user logs in with valid credentials
--- src/test/java/ServiceNow/ESR/Steps/ESRDashboardPageSteps.java
+++ src/test/java/ServiceNow/ESR/Steps/ESRDashboardPageSteps.java
@@ -1,5 +1,36 @@
 package ServiceNow.ESR.Steps;
 
-public class ESRDashboardPageSteps {
-    
+import appsCommon.PageInitializer;
+import com.nci.automation.web.EnvUtils;
+import com.nci.automation.web.WebDriverUtils;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ESRDashboardPageSteps extends PageInitializer {
+    @Given("a user is on the NCI at your service homepage")
+    public void a_user_is_on_the_nci_at_your_service_homepage() {
+        WebDriverUtils.webDriver.get(EnvUtils.getApplicationUrl("nativeViewSideDoor"));
+    }
+    @When("a user logs in with valid credentials")
+    public void a_user_logs_in_with_valid_credentials() {
+        loginImpl.loginToNativeViewSideDoor();
+    }
+    @When("user clicks on Native View")
+    public void user_clicks_on_native_view() {
+
+    }
+    @When("user navigates to Engineering Service Request")
+    public void user_navigates_to_engineering_service_request() {
+
+    }
+    @When("user clicks on Dashboard")
+    public void user_clicks_on_dashboard() {
+
+    }
+    @Then("user is directed to the ESR Dashboard page")
+    public void user_is_directed_to_the_esr_dashboard_page() {
+
+    }
+
 }
