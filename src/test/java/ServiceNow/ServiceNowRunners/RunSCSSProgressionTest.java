a51893e6ca633a0689882daadc241a0001ccd714
Nekrashevich <nekrashevicha2@nih.gov>
2023-07-27 09:22:36 -0400

    formatted runner

--- src/test/java/ServiceNow/ServiceNowRunners/RunSCSSProgressionTest.java
+++ src/test/java/ServiceNow/ServiceNowRunners/RunSCSSProgressionTest.java
@@ -6,16 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report"
-		, "json:target/cucumber.json"
-		, "junit:target/cucumber.xml"
-		, "rerun:target/failed.txt","pretty"}
-		, features="src/test/java/ServiceNow/SCSS/Features"
-	    , glue="ServiceNow.SCSS.Steps"
-		, tags="@Progression"
-    	, dryRun =false
-		, monochrome=false
-		, strict =true
-		)
+        , "json:target/cucumber.json"
+        , "junit:target/cucumber.xml"
+        , "rerun:target/failed.txt","pretty"}
+        , features="src/test/java/ServiceNow/SCSS/Features"
+        , glue="ServiceNow.SCSS.Steps"
+        , tags="@Progression"
+        , dryRun =false
+        , monochrome=false
+        , strict =true
+)
 
 public class RunSCSSProgressionTest {
 }
