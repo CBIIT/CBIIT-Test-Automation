2bf3ecf448204eb76510c3ec4c28a6350205fa1f
mariachaudhry <maria.chaudhry2@nih.gov>
2023-08-03 15:17:16 -0400

    ..

--- src/test/java/ServiceNow/SEER/StepsImplementation/SEERResearchPlusLoginStepImplementation.java
+++ src/test/java/ServiceNow/SEER/StepsImplementation/SEERResearchPlusLoginStepImplementation.java
@@ -17,6 +17,5 @@ public class SEERResearchPlusLoginStepImplementation extends PageInitializer {
         CommonUtils.waitForVisibility(seerLandingPage.seerResearchPlusSignInNIHLoginTitle);
         CommonUtils.assertEqualsWithMessage(SEERDataAccessRequest_Constants.SEER_RESEARCH_PLUS_LOGIN_SIGN_IN_NIH_LOGIN_PAGE_TITLE, seerLandingPage.seerResearchPlusSignInNIHLoginTitle.getText(), "Verifying the User is on Sign In page");
         JavascriptUtils.drawBlueBorder(seerLandingPage.seerResearchPlusSignInNIHLoginTitle);
-        MiscUtils.sleep(5000);
     }
 }
