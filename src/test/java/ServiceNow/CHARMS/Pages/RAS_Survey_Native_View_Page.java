5f2c32f2379aa029761c1ca729e3d222b772febd
Nekrashevich <nekrashevicha2@nih.gov>
2023-08-16 13:54:31 -0400

    ras survey

--- src/test/java/ServiceNow/CHARMS/Pages/RAS_Survey_Native_View_Page.java
+++ src/test/java/ServiceNow/CHARMS/Pages/RAS_Survey_Native_View_Page.java
@@ -3,6 +3,7 @@ package ServiceNow.CHARMS.Pages;
 import com.nci.automation.web.WebDriverUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RAS_Survey_Native_View_Page {
@@ -10,6 +11,57 @@ public class RAS_Survey_Native_View_Page {
     /**
      * STORE RAS SURVEY NATIVE VIEW ELEMENTS HERE
      */
+    /** Diagnosed with a RASopathy by a physician through testing dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.diagnosed_rasopathy']")
+    public WebElement dropdownDiagnosedWithRasopathyByPhysician;
+
+    /** Diagnosed with hypoglycemia during the newborn period dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.hypoglycemia']")
+    public WebElement dropdownDiagnosedWithHypoglycemiaDuringNewbornPeriod;
+
+    /** Were medications taken while biological mother was pregnant with the participant dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.medications_while_pregnant']")
+    public WebElement dropdownWereMedicationsTakenWhileMotherWasPregnant;
+
+    /** Were vaping products use by participants bio mom during pregnancy dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.were_vaping_products_used']")
+    public WebElement dropdownWereVapingProductsUsedDuringPregnancy;
+
+    /** Did participants bio mother use recreational drugs during pregnancy dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.drugs_used_during_pregnancy']")
+    public WebElement dropdownDidBioMotherUsedRecreationalDrugsDuringPregnancy;
+
+    /** Biological mothers occupation while pregnant with the participant field **/
+    @FindBy(xpath = "//div//textarea[@id='x_naci_family_coho_rasopathy_iiq.occuption_while_pregnant']")
+    public WebElement fieldBiologicalMothersOccupationWhilePregnant;
+
+    /** Biological mothers occupation unknown checkbox **/
+    @FindBy(xpath = "//input[@id='x_naci_family_coho_rasopathy_iiq.bio_mom_occupation_unknown']")
+    public WebElement checkboxBiologicalMothersOccupationUnknown;
+
+    /** Biological father occupation unknown checkbox **/
+    @FindBy(xpath = "//input[@id='x_naci_family_coho_rasopathy_iiq.bio_dad_occupation_unknown']")
+    public WebElement checkboxBiologicalFathersOccupationUnknown;
+
+    /** Did biological mother receive prenatal tests for birth defects/medical problems dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.prenatal_testing']")
+    public WebElement dropdownDidBioMotherReceivePrenatalTests;
+
+    /** Did participants biological mother use tobacco products while pregnant dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.tobacco_used_while_pregnant_mom']")
+    public WebElement dropdownDidBioMotherUseTobacco;
+
+    /** Did participants biological mother drink alcohol while pregnant with participant dropdown **/
+    @FindBy(xpath = "//select[@id='x_naci_family_coho_rasopathy_iiq.alcohol_while_pregnant']")
+    public WebElement dropdownDidBioMotherUseAlcohol;
+
+    /** Did the participants mother ever live/work in a place where others use tobacco dropdown **/
+    @FindBy(xpath = "//select[@name='x_naci_family_coho_rasopathy_iiq.live_work_near_tobacco_usage']")
+    public WebElement dropdownDidBioMotherEverLiveWorkWhereOtherUseTobacco;
+
+    /** Biological fathers occupation while mother was pregnant with the participant field **/
+    @FindBy(xpath = "//div//textarea[@id='x_naci_family_coho_rasopathy_iiq.fathers_occupation_while_pregnant']")
+    public WebElement fieldBioFatherOccupationWhileMotherWasPregnant;
 
     /***
      * USE THIS METHOD TO DYNAMICALLY LOCATE TABS BY SPECIFYING EXACT TEXT
--- src/test/java/ServiceNow/CHARMS/Steps/RAS_Survey_Data_Verification_Steps.java
+++ src/test/java/ServiceNow/CHARMS/Steps/RAS_Survey_Data_Verification_Steps.java
@@ -55,7 +55,8 @@ public class RAS_Survey_Data_Verification_Steps extends PageInitializer {
         /**
          *  ----- BEGINNING RASopathy History SECTION VALIDATION ----- ALENA
          */
-
+        CommonUtils.clickOnElement(ras_survey_native_view.dynamicTabLocator("RASopathy History"));
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDiagnosedWithRasopathyByPhysician,"No","No value is displayed for Diagnosed with a RASopathy by a physician through testing");
         /**
          *  ----- END RASopathy History SECTION VALIDATION ----- ALENA
          */
@@ -63,9 +64,20 @@ public class RAS_Survey_Data_Verification_Steps extends PageInitializer {
         /**
          *  ----- BEGINNING Birth and Gestation SECTION VALIDATION ----- ALENA
          */
-
-        /**
-         *  ----- END Birth and Gestation SECTION VALIDATION ----- ALENA
+        CommonUtils.clickOnElement(ras_survey_native_view.dynamicTabLocator("Birth & Gestation"));
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDiagnosedWithHypoglycemiaDuringNewbornPeriod,"No","No value is displayed for Diagnosed with hypoglycemia during the newborn period dropdown");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownWereMedicationsTakenWhileMotherWasPregnant,"No"," No value is displayed for Were medications taken while biological mother was pregnant with the participant dropdown");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownWereVapingProductsUsedDuringPregnancy,"No","No value is displayed for Were vaping products use by participants bio mom during pregnancy dropdown");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDidBioMotherUsedRecreationalDrugsDuringPregnancy,"No","No value is displayed for Did participants bio mother use recreational drugs during pregnancy dropdown");
+        CommonUtils.assertEqualsWithMessage(ras_survey_native_view.fieldBiologicalMothersOccupationWhilePregnant.getText(), "Nurse", "Nurse value is displayed for Biological mothers occupation while pregnant with the participant field");
+        CommonUtils.assertEqualsWithMessage(ras_survey_native_view.checkboxBiologicalMothersOccupationUnknown.getAttribute("value"), "false", "Biological mothers occupation unknown checkbox is unchecked");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDidBioMotherReceivePrenatalTests,"Yes","Yes value is displayed for Did biological mother receive prenatal tests for birth defects/medical problems dropdown");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDidBioMotherUseTobacco,"Yes","Yes value is displayed for Did participants biological mother use tobacco products while pregnant dropdown");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDidBioMotherUseAlcohol,"Yes","Yes value is displayed for Did participants biological mother drink alcohol while pregnant with participant dropdown");
+        CommonUtils.verifyingDropDownValueIsSelected(ras_survey_native_view.dropdownDidBioMotherEverLiveWorkWhereOtherUseTobacco,"Yes","Yes value is displayed for Did the participants mother ever live/work in a place where others use tobacco dropdown");
+        CommonUtils.assertEqualsWithMessage(ras_survey_native_view.fieldBioFatherOccupationWhileMotherWasPregnant.getText(), "", "No value is displayed for Biological mothers occupation while pregnant with the participant field");
+        CommonUtils.assertEqualsWithMessage(ras_survey_native_view.checkboxBiologicalFathersOccupationUnknown.getAttribute("value"), "false", "Biological fathers occupation unknown checkbox is unchecked");
+        /*  ----- END Birth and Gestation SECTION VALIDATION ----- ALENA
          */
 
         /**
