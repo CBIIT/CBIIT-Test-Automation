7a8d43b7ce5e0741ed7ec10df371cbf9345975da
bucurgb <george_buc@yahoo.com>
2023-08-23 12:56:19 -0400

    ..

--- src/test/java/ServiceNow/CHARMS/Steps/RAS_Survey_Submission_Steps.java
+++ src/test/java/ServiceNow/CHARMS/Steps/RAS_Survey_Submission_Steps.java
@@ -41,35 +41,4 @@ public class RAS_Survey_Submission_Steps extends PageInitializer {
 	public void the_participant_submits_a_ras_survey_from_excel_sheet(String rasSurveySheet) {
 		rasScenario1StepsImpl.rasSurveyScenarioSelector(rasSurveySheet);
 	}
-
-	static String rasSurveyPin;
-
-	@Given("enters Okta email address {string} and One Time Pin code to proceed with the RAS Survey")
-	public void enters_Okta_email_address_and_One_Time_Pin_code_to_proceed_with_the_RAS_Survey(
-			String oktaEmailAddress) {
-
-		/**
-		 * USING SAME PIN TEXT AND GO BUTTON LOCATORS AS CGB IIQ SINCE THEY ARE THE SAME
-		 */
-		CommonUtils.waitForVisibility(myRASHomePage.rasoptathyIndividualInformationQuestionnairePinText);
-
-		rasSurveyPin = myRASHomePage.rasoptathyIndividualInformationQuestionnairePinText.getText();
-
-		CommonUtils.waitForVisibility(myRASHomePage.rasoptathyIndividualInformationQuestionnaireOneTimePinGoButton);
-		myRASHomePage.rasoptathyIndividualInformationQuestionnaireOneTimePinGoButton.click();
-
-		Set<String> allWindowHandles1 = WebDriverUtils.webDriver.getWindowHandles();
-		for (String currentWindow1 : allWindowHandles1) {
-			WebDriverUtils.webDriver.switchTo().window(currentWindow1);
-		}
-
-		MiscUtils.sleep(1000);
-		CommonUtils.waitForVisibility(rAS_Survey_Page.cHARMSEmailLogIn);
-		rAS_Survey_Page.cHARMSEmailLogIn.sendKeys("charmsras5@yahoo.com");
-		rAS_Survey_Page.cHARMSpasswordToLogIn.sendKeys(rasSurveyPin);
-		CucumberLogUtils.logScreenshot();
-		rAS_Survey_Page.surveyNextButton.click();
-	}
-
-
 }
