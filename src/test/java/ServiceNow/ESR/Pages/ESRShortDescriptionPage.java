70b77752ed54ceb66aa8f14da5c2296912d91aa2
Sarwar <sarwara2@nci-02277698-ml.nih.gov>
2023-09-20 15:00:13 -0400

    Spacing fixes

--- src/test/java/ServiceNow/ESR/Pages/ESRShortDescriptionPage.java
+++ src/test/java/ServiceNow/ESR/Pages/ESRShortDescriptionPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ESRShortDescriptionPage {
+
     public ESRShortDescriptionPage() {
         PageFactory.initElements(WebDriverUtils.webDriver, this);
     }
