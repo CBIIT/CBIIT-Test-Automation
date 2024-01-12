adb5d6be68aced62705df9d535447a1ac15153c8
Nesar <nesarh2@nih.gov>
2024-01-12 13:25:41 -0500

    Egrants automation - removing extra files

--- src/test/java/CustomBusiness/EIDP/Steps/LoginSteps.java
+++ src/test/java/CustomBusiness/EIDP/Steps/LoginSteps.java
@@ -9,22 +9,28 @@ import io.cucumber.java.en.When;
 public class LoginSteps extends PageInitializer {
 
 	@Given("User logs in to EIDP {string} as {string} and {string}")
-	public void user_logs_in_to_EIDP_as_and(String url, String username, String password) {
+	public void user_logs_in_to_EIDP_as_and(String url, String username, String password) throws TestingException {
 		eidpLoginStepImpl.ApplicationLogin(username, password, url);
 	}
 
 	@When("User will login to the application as \"([^\"]*)\" user")
-	public void loginByUsername(String username) {
+	public void loginByUsername(String username) throws TestingException {
+		if (username.equalsIgnoreCase("gugulothus2")) {
+			eidpLoginStepImpl.eidpApplicationLogin("sgugulothuUsername", "sgugulothuPassword");
+		} else if (username.equalsIgnoreCase("nekrashevicha2")) {
+			eidpLoginStepImpl.eidpApplicationLogin("nekrashevicha2Username", "nekrashevicha2Password");
+		} else {
 			eidpLoginStepImpl.eidpApplicationLogin("Username", "Password");
+		}
 	}
 
 	@When("Logged in user changes the user to \"([^\"]*)\"")
-	public void changeUser(String username) {
+	public void changeUser(String username) throws TestingException {
 		eidpLoginStepImpl.changeUser(username);
 	}
 
 	@When("Logged in user changes the user to trainee")
-	public void changeUser() {
+	public void changeUser() throws Exception {
 		String updatedUserName = SharedData.traineeName;
 		eidpLoginStepImpl.changeUser(eidpLoginStepImpl.lastNameFirstName(updatedUserName));
 	}
@@ -40,4 +46,5 @@ public class LoginSteps extends PageInitializer {
 		String updatedUserName = SharedData.coPmName;
 		eidpLoginStepImpl.changeUser(eidpLoginStepImpl.lastNameFirstName(updatedUserName));
 	}
+
 }
