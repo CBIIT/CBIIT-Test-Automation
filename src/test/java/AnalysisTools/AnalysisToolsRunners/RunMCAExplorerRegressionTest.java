2013cc4b70ede83e14b1de40c5ee5b75bc5640d9
gugulothus2 <gugulothus2@nci-02295927-ml>
2024-01-25 14:39:10 -0500

    create new runner and packages for MCA Explorer Test Cases

---
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunMCAExplorerRegressionTest.java
@@ -0,0 +1,19 @@
+package AnalysisTools.AnalysisToolsRunners;
+
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
+		"junit:target/cucumber.xml", "rerun:target/failed.txt", "pretty" }, features = {
+				"src/test/java/AnalysisTools/PLCO/Features" }, glue = "AnalysisTools.PLCO.Steps", tags = "@Smoke", dryRun = false, monochrome = true, strict = true
+
+)
+
+public class RunMCAExplorerRegressionTest {
+
+}
---
+++ src/test/java/AnalysisTools/MCAExplorer/Features/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package AnalysisTools.MCAExplorer.Features;
