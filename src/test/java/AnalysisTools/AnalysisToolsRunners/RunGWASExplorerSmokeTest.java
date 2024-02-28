dd9390e2e5f15890411ebee304d936dea2cde2bd
matakevin <matakevin1777@gmail.com>
2024-02-28 18:32:58 -0500

    modified runners to work on the new PlayWright framework

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunGWASExplorerSmokeTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunGWASExplorerSmokeTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = { "html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
 		"junit:target/cucumber.xml", "rerun:target/failed.txt",
 		"pretty",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, features = "src/test/java/AnalysisTools/GWASExplorer/Features", glue = "AnalysisTools.GWASExplorer.Steps", tags = "@Smoke", dryRun = false, monochrome = true, strict = true
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, features = "src/test/java/AnalysisTools/GWASExplorer/Features", glue = "AnalysisTools.GWASExplorer.Steps", tags = "@Smoke", dryRun = false
 
 )
 /**
