8ad2beecae44242ae306ac99986a57bc5831c393
matakevin <matakevin1777@gmail.com>
2024-02-28 18:10:07 -0500

    modified runners to work on the new PlayWright framework

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunJPSurvSmokeTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunJPSurvSmokeTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(plugin = { "html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
 		"junit:target/cucumber.xml", "rerun:target/failed.txt",
 		"pretty",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, features = "src/test/java/AnalysisTools/JPSurv/Features", glue = "AnalysisTools.JPSurv.Steps", tags = "@Smoke", dryRun = false, monochrome = true, strict = true
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, features = "src/test/java/AnalysisTools/JPSurv/Features", glue = "AnalysisTools.JPSurv.Steps", tags = "@Smoke", dryRun = false
 
 )
 
