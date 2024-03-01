7597f2b93b25d996e90092dbe5d24c6c9ca354df
gugulothus2 <gugulothus2@ns02262005md.ninds.nih.gov>
2024-03-01 12:30:00 -0500

    Added TestNg Runner

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunMCAExplorerSmokeTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunMCAExplorerSmokeTest.java
@@ -1,22 +1,12 @@
 package AnalysisTools.AnalysisToolsRunners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = { 
-		"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
-		"junit:target/cucumber.xml", "rerun:target/failed.txt", "pretty" }
-        , features = {"src/test/java/AnalysisTools/MCAExplorer/Features" }
-        , glue = "AnalysisTools.MCAExplorer.Steps"
-        , tags = "@MCAExplorer"
-        , dryRun = false
-        , monochrome = true
-        , strict = true
+@CucumberOptions(plugin = { "html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
+		"junit:target/cucumber.xml", "rerun:target/failed.txt", "pretty" }, features = {
+				"src/test/java/AnalysisTools/MCAExplorer/Features" }, glue = "AnalysisTools.MCAExplorer.Steps", tags = "@Smoke", dryRun = false)
 
-)
-
-public class RunMCAExplorerSmokeTest {
+public class RunMCAExplorerSmokeTest extends AbstractTestNGCucumberTests {
 
 }
