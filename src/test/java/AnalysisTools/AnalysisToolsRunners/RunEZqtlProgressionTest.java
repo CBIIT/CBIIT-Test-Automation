610f1ada45ff65d66dd98bdb1c218aaa554e9f73
matakevin <matakevin1777@gmail.com>
2024-03-05 11:18:05 -0500

    tagged ezQTL scenario with progression for Jenknins testing

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunEZqtlProgressionTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunEZqtlProgressionTest.java
@@ -1,20 +1,15 @@
 package AnalysisTools.AnalysisToolsRunners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
         "junit:target/cucumber.xml", "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/AnalysisTools/ezQTL/Features",
         glue = "AnalysisTools.ezQTL.Steps",
         tags = "@Progression",
-        dryRun = false,
-        monochrome = true,
-        strict = true
-
+        dryRun = false
 )
 
 /**
@@ -23,6 +18,6 @@ import org.junit.runner.RunWith;
  *
  * @author sohilz2
  */
-public class RunEZqtlProgressionTest {
+public class RunEZqtlProgressionTest extends AbstractTestNGCucumberTests{
 
 }
