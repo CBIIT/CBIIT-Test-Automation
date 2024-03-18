e42915680bbf8bc9819070f41bbd367c05705b4b
nekrashevicha2 <nekrashevicha2@nih.gov>
2024-03-18 14:15:10 -0400

    upd

--- src/test/java/ServiceNow/ServiceNowRunners/RunSCSSProgressionTest.java
+++ src/test/java/ServiceNow/ServiceNowRunners/RunSCSSProgressionTest.java
@@ -1,10 +1,8 @@
 package ServiceNow.ServiceNowRunners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report"
         , "json:target/cucumber.json"
         , "junit:target/cucumber.xml"
@@ -12,11 +10,9 @@ import org.junit.runner.RunWith;
         , "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         , features="src/test/java/ServiceNow/SCSS/Features"
         , glue="ServiceNow.SCSS.Steps"
-        , tags="@APPTRACK-67"
+        , tags="@APPTRACK-91"
         , dryRun=false
-        , monochrome=true
-        , strict=true
 )
 
-public class RunSCSSProgressionTest {
+public class RunSCSSProgressionTest extends AbstractTestNGCucumberTests {
 }
