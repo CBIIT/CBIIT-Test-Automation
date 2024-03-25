7d44b4b513e799486db2a5183d5af617bf350095
iamdez99 <diegojuarezbusiness@gmail.com>
2024-03-25 13:34:12 -0400

    SSJ Automation

--- src/test/java/appsCommon/Pages/Playwright_NativeView_Side_Door_Login_Page.java
+++ src/test/java/appsCommon/Pages/Playwright_NativeView_Side_Door_Login_Page.java
@@ -2,7 +2,18 @@ package appsCommon.Pages;
 
 public class Playwright_NativeView_Side_Door_Login_Page {
 
+    /**
+     * USERNAME TEXT BOX
+     */
     public static String usernameTextBox = "//input[@type='text']";
+
+    /**
+     * PASSWORD TEXT BOX
+     */
     public static String passwordTextBox = "//input[@type='password']";
+
+    /**
+     * LOGIN BUTTON
+     */
     public static String loginButton = "Log in";
 }
