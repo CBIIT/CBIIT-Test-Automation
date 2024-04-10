a5c98a1530fbe781155f70aaf085bcd7a3018cc0
matakevin <matakevin1777@gmail.com>
2024-04-10 13:21:18 -0400

    added JPSurv on dev env

--- src/test/java/AnalysisTools/AnalysisToolsRunners/RunJPSurvSmokeTest.java
+++ src/test/java/AnalysisTools/AnalysisToolsRunners/RunJPSurvSmokeTest.java
@@ -1,15 +1,10 @@
 package AnalysisTools.AnalysisToolsRunners;
 
-import java.io.File;
-import org.testng.annotations.BeforeClass;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import com.nci.automation.utils.LocalConfUtils;
-import com.nci.automation.web.ConfUtils;
 
-@CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report"
+@CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report.html"
 		, "json:target/cucumber.json"
-		, "junit:target/cucumber.xml"
 		, "rerun:target/failed.txt","pretty"}
 		, features="src/test/java/AnalysisTools/JPSurv/Features"
 		, glue="AnalysisTools.JPSurv.Steps"
