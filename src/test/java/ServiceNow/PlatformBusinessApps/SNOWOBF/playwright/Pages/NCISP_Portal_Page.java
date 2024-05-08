4e1f0bb537410777b06ecf7e1c615a27e16f7e16
mariachaudhry <maria.chaudhry2@nih.gov>
2024-05-08 18:26:13 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/SNOWOBF/playwright/Pages/NCISP_Portal_Page.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SNOWOBF/playwright/Pages/NCISP_Portal_Page.java
@@ -3,11 +3,6 @@ package ServiceNow.PlatformBusinessApps.SNOWOBF.playwright.Pages;
 public class NCISP_Portal_Page {
 
     /***
-     * NATIVE VIEW LINK
-     */
-    public static String nativeViewLink = "Native View";
-
-    /***
      * SERVICES LINK
      */
     public static String servicesLink = "Services";
@@ -23,24 +18,29 @@ public class NCISP_Portal_Page {
     public static String cbiit_BusinessServicesLink = "Business Services";
 
     /***
-     * ADMINISTRATIVE BUSINESS SERVICES LINK LOCATOR
-     */
-    public static String administrative_BusinessServicesLinkLocator = "md-content";
-
-    /***
      * ADMINISTRATIVE BUSINESS SERVICES LINK
      */
     public static String administrative_BusinessServicesLink = "Administrative Business Systems";
 
     /***
-     * ADMINISTRATIVE BUSINESS PAGE LINK
-     */
-    public static String administrative_BusinessPageLink = "Administrative Business";
-
-    /***
      * OBF SYSTEMS SUPPORT LINK
      */
     public static String obf_SystemsSupportLink = "OBF Systems Support";
+
+    /***
+     * NATIVE VIEW LINK
+     */
+    public static String nativeViewLink = "Native View";
+
+    /***
+     * ADMINISTRATIVE BUSINESS SERVICES LINK LOCATOR
+     */
+    public static String administrative_BusinessServicesLinkLocator = "md-content";
+
+    /***
+     * ADMINISTRATIVE BUSINESS PAGE LINK
+     */
+    public static String administrative_BusinessPageLink = "Administrative Business";
 
     /***
      * OBF SYSTEMS SUPPORT PAGE LINK
