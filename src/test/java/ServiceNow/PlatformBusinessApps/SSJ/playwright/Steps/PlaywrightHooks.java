9ad4c1bd745242b0fb75cd52f4b0ad4c861abd85
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-16 12:26:37 -0400

    SSJ - Verification of Basic Vacancy Information section while creating an NCI Vacancy

--- src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Steps/PlaywrightHooks.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Steps/PlaywrightHooks.java
@@ -9,17 +9,29 @@ import org.testng.asserts.SoftAssert;
 
 public class PlaywrightHooks extends PlaywrightUtils {
 
+    private AssertionError assertionError;  // to hold any assertion error during the test
     SoftAssert softAssert;
+
     @Before
     public void start(Scenario scenario){
         CucumberLogUtils.scenario = scenario;
         setUp();
         softAssert = new SoftAssert();
+        assertionError = null;  // reset the assertion error at the start of each test
     }
 
     @After
-    public void end(){
-        softAssert.assertAll();
-        tearDown();
+    public void end() {
+        try {
+            softAssert.assertAll();
+        } catch (AssertionError error) {
+            assertionError = error;
+        } finally {
+            tearDown();  // tearDown() will be always executed
+        }
+
+        if (assertionError != null) {
+            throw assertionError;  // re-throw the assertion error after tearDown()
+        }
     }
 }
