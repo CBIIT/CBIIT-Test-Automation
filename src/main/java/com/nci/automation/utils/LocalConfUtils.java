9d71f9503de6c1c34fe93ef894be287df1eab5e7
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-22 19:11:45 -0400

    Testing github actions

--- src/main/java/com/nci/automation/utils/LocalConfUtils.java
+++ src/main/java/com/nci/automation/utils/LocalConfUtils.java
@@ -2,10 +2,12 @@ package com.nci.automation.utils;
 
 import java.io.File;
 import java.util.Enumeration;
-import java.util.Map;
 import java.util.Properties;
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * @author sohilz2
+ */
 public class LocalConfUtils {
 
 	private static Properties localConf = null;
@@ -22,36 +24,73 @@ public class LocalConfUtils {
 					"Report Config Path is not specified");
 	}
 
+//	public static Properties loadLocalConf() {
+//
+//		/*
+//		 * Check for command line parameter
+//		 */
+//		String localConfResourcesPath = System.getProperty("isCloud");
+//
+//		if (StringUtils.isBlank(localConfResourcesPath)) {
+//			localConfResourcesPath = "/conf/localEnv.properties";
+//		} else if (localConfResourcesPath.equalsIgnoreCase("true")) {
+//			localConfResourcesPath = "/conf/cloudEnv.properties";
+//		}
+//
+//		localConf = new Properties();
+//
+//		try {
+//			PropertiesFileUtils.loadPropsFromResource(localConf,
+//					localConfResourcesPath);
+//			localConf = loadSystemProperties(localConf);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		return localConf;
+//
+//	}
+
 	public static Properties loadLocalConf() {
-		String isCloud = System.getenv("isCloud");
-		System.out.println("IsCloud: " + isCloud); //in loadLocalConf() method in Java
-		if (isCloud == null) {
-			isCloud = System.getProperty("isCloud");
+
+		String isCloud = System.getProperty("isCloud");
+
+		if (StringUtils.isBlank(isCloud)) {
+			isCloud = "false";
 		}
 
 		localConf = new Properties();
 
-		try {
-			if (isCloud != null && isCloud.equalsIgnoreCase("true")) {
-				// Load properties from environment variables
-				Map<String, String> env = System.getenv();
-				for (String envName : env.keySet()) {
-					localConf.setProperty(envName, env.get(envName));
-				}
-			} else {
-				// Load properties from local configuration file
-				String localConfResourcesPath = "conf/localEnv.properties";
-				PropertiesFileUtils.loadPropsFromResource(localConf, localConfResourcesPath);
-			}
-			localConf = loadSystemProperties(localConf);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		if (isCloud.equalsIgnoreCase("true")) {
+			// cloud environment, load properties from environment variables
+			localConf.setProperty("Username", System.getenv("Username"));
+			localConf.setProperty("Password", System.getenv("Password"));
+			localConf.setProperty("browser", System.getenv("browser"));
+			localConf.setProperty("env", System.getenv("env"));
 
+			// Also attempt to load from the cloudEnv.properties file if it exists
+			String cloudConfResourcesPath = "/conf/cloudEnv.properties";
+			try {
+				PropertiesFileUtils.loadPropsFromResource(localConf, cloudConfResourcesPath);
+				localConf = loadSystemProperties(localConf);
+			} catch (Exception e) {
+				System.out.println("cloudEnv.properties not loaded. Using environment variables.");
+			}
+		} else {
+			// local environment, load properties from localEnv.properties file
+			String localConfResourcesPath = "/conf/localEnv.properties";
+			try {
+				PropertiesFileUtils.loadPropsFromResource(localConf, localConfResourcesPath);
+				localConf = loadSystemProperties(localConf);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return localConf;
 	}
 
 	private static Properties loadSystemProperties(Properties localProps) {
+
 		Properties systemProperties = System.getProperties();
 		Enumeration<?> e = systemProperties.propertyNames();
 		String key, value;
@@ -63,6 +102,7 @@ public class LocalConfUtils {
 		}
 
 		return localProps;
+
 	}
 
 	public static Properties getProperties() {
@@ -74,27 +114,35 @@ public class LocalConfUtils {
 	}
 
 	public static String getProperty(String key) {
+
 		if (localConf == null) {
 			loadLocalConf();
 		}
 
-		String value =  System.getenv(key);
-		if (value == null) {
-			value = localConf.getProperty(key);
-		}
+		return localConf.getProperty(key);
 
-		return value;
 	}
 
 	public static void setProperty(String key, String value) {
+
 		if (localConf == null) {
 			loadLocalConf();
 		}
 
 		localConf.setProperty(key, value);
+
 	}
 
 	public static String getRootDir() {
 		return System.getProperty("user.dir");
 	}
+
+	public static String getResourceDir() {
+		return getRootDir() + File.separator + RESOURCE_PATH;
+	}
+
+	public static String getQcResourcesDir() {
+		return getResourceDir() + File.separator + "libs" + File.separator + "qc";
+	}
+
 }
