c3b1bacdc173f5983a67f5dc5fd09e81cd7f2d9b
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-25 02:31:19 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/SSJ/SSJ_Runners/SSJ_Progression_Runners.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/SSJ_Runners/SSJ_Progression_Runners.java
@@ -3,7 +3,7 @@ package ServiceNow.PlatformBusinessApps.SSJ.SSJ_Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/ssj-reports/ssj-progression-report.html", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/ssj-progression-reports/ssj-progression-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         , features = {"src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Features", "src/test/java/ServiceNow/PlatformBusinessApps/SSJ/selenium/Features"}
