a66e7a83e8086a04a9554a2b928b2047b7cef24e
mariachaudhry <maria.chaudhry2@nih.gov>
2024-05-28 16:32:53 -0400

    latest changes

--- src/test/java/ServiceNow/PlatformBusinessApps/NERD/selenium/NERDRunners/Run_NERD_Regression_Test.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/NERD/selenium/NERDRunners/Run_NERD_Regression_Test.java
@@ -3,7 +3,7 @@ package ServiceNow.PlatformBusinessApps.NERD.selenium.NERDRunners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report.html",
+@CucumberOptions(plugin= {"html:target/NERD-reports/NERD-Regression-report.html",
 		"json:target/cucumber.json", "rerun:target/failed.txt",
 			"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 			, features="src/test/java/ServiceNow/PlatformBusinessApps/NERD/Features"
