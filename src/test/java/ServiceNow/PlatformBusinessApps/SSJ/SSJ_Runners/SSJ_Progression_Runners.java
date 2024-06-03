accaea4ac3aeaffe5a1a1bdb6def14676a3dda08
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-03 13:50:04 -0400

    testing NERD github workflow

--- src/test/java/ServiceNow/PlatformBusinessApps/SSJ/SSJ_Runners/SSJ_Progression_Runners.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/SSJ_Runners/SSJ_Progression_Runners.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"html:target/ssj-progression-reports/ssj-progression-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-        , features = {"src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Features", "src/test/java/ServiceNow/PlatformBusinessApps/SSJ/selenium/Features"}
+        , features = {"src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Features"}
         , glue = {"ServiceNow.PlatformBusinessApps.SSJ.playwright.Steps", "ServiceNow.PlatformBusinessApps.SSJ.selenium.Steps", "Hooks"}
         , tags = "@Progression"
         , dryRun = false
