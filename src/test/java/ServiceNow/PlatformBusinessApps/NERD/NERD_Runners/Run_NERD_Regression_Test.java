6df8c98e6f476177512c47a4cbb62f491fc31b3d
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-04 17:58:08 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/NERD/NERD_Runners/Run_NERD_Regression_Test.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/NERD/NERD_Runners/Run_NERD_Regression_Test.java
@@ -3,11 +3,11 @@ package ServiceNow.PlatformBusinessApps.NERD.NERD_Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report.html",
+@CucumberOptions(plugin= {"html:target/nerd-progression-reports/nerd-progression-report.html",
 		"json:target/cucumber.json", "rerun:target/failed.txt",
 			"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 			, features="src/test/java/ServiceNow/PlatformBusinessApps/NERD/Features"
-			, glue="ServiceNow.PlatformBusinessApps.NERD.Steps"
+			, glue={"ServiceNow.PlatformBusinessApps.NERD.Steps", "Hooks"}
 			, tags="@Regression"
 			, dryRun = false
 	)
