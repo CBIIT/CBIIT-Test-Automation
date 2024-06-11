6f0b179184bb6a63e6f07ec51cd3ccb5ce18d4c0
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-11 12:30:59 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/SEER/SEER_Runners/Run_SEER_Regression_Test.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SEER/SEER_Runners/Run_SEER_Regression_Test.java
@@ -1,24 +1,23 @@
-package ServiceNow.ServiceNowRunners.SEERRunners;
+package ServiceNow.PlatformBusinessApps.SEER.SEER_Runners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
         "junit:target/cucumber.xml", "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/ServiceNow/PlatformBusinessApps/SEER/Features",
         glue = "ServiceNow.PlatformBusinessApps.SEER.Steps",
-        tags = "@Progression",
+        tags = "@Regression",
         dryRun = false
 )
 
 /**
  * This class is annotated with @RunWith(Cucumber.class) and it will run
  * cucumber feature(s).
- *
+ * 
  * @author juarezds
  */
 
-public class Run_SEER_Progression_Test extends AbstractTestNGCucumberTests {
-
+public class Run_SEER_Regression_Test extends AbstractTestNGCucumberTests {
 }
