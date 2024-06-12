8869176bfaf7f753f8a6cc07464ae1a5de33f621
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-12 00:42:17 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/SEER/SEER_Runners/Run_SEER_Smoke_Test.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SEER/SEER_Runners/Run_SEER_Smoke_Test.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = {"html:target/seer-smoke-reports/seer-smoke-report.html", "json:target/cucumber.json",
-		"rerun:target/failed.txt",
+        "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         , features = "src/test/java/ServiceNow/PlatformBusinessApps/SEER/Features"
         , glue = {"ServiceNow.PlatformBusinessApps.SEER.Steps", "Hooks"}
