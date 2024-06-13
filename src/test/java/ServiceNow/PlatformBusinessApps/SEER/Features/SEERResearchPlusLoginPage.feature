81c3c4b8d349e5378cfb2a0f658f1cc81ea29ab4
mariachaudhry <maria.chaudhry2@nih.gov>
2024-06-13 14:47:04 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/SEER/Features/SEERResearchPlusLoginPage.feature
+++ src/test/java/ServiceNow/PlatformBusinessApps/SEER/Features/SEERResearchPlusLoginPage.feature
@@ -1,6 +1,6 @@
 Feature: SEER Research Plus Login Scenarios
 
-  @ODS-349 @chaudhryma @Smoke @Regression
+  @ODS-349 @chaudhryma @Need_Update_Requirment
   Scenario: Verifying that "SEER Research Plus" button land on log in page for sanity check
     Given a user is on the SEER Data Access landing page
     When the user clicks the SEER Research Plus button
--- src/test/java/ServiceNow/PlatformBusinessApps/SEER/SEER_Runners/Run_SEER_Smoke_Test.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SEER/SEER_Runners/Run_SEER_Smoke_Test.java
@@ -1,13 +1,13 @@
-package ServiceNow.ServiceNowRunners.SEERRunners;
+package ServiceNow.PlatformBusinessApps.SEER.SEER_Runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
-		"junit:target/cucumber.xml", "rerun:target/failed.txt",
+@CucumberOptions(plugin= {"html:target/html-reports/cucumber-default-report",
+		"json:target/cucumber.json", "rerun:target/failed.txt",
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 		, features="src/test/java/ServiceNow/PlatformBusinessApps/SEER/Features"
-		, glue="ServiceNow.PlatformBusinessApps.SEER.Steps"
+		, glue={"ServiceNow.PlatformBusinessApps.SEER.Steps", "Hooks"}
 		, tags="@Smoke"
 		, dryRun = false
 )
