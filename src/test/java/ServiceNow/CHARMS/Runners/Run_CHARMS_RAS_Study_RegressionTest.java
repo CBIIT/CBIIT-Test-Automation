76030e0fe3b1d38767a1cce393613f88ea16323b
Sonika Jain <sonika.jain@nih.gov>
2024-06-18 14:12:03 -0400

    Changes made to the runner classes

--- src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Study_RegressionTest.java
+++ src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Study_RegressionTest.java
@@ -4,8 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = { "html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
-		"rerun:target/failed.txt", "pretty" },
-		features = "src/test/java/ServiceNow/CHARMS/Features", glue = "ServiceNow.CHARMS.Steps", tags = "@Regression", dryRun = false
+		"rerun:target/failed.txt",
+		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		features = "src/test/java/ServiceNow/CHARMS/Features",
+		glue = "ServiceNow.CHARMS.Steps",
+		tags = "@Regression",
+		dryRun = false
 )
 /**
  * 
