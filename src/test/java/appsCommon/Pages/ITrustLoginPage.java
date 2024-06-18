fbc298bc4f28d2f8d7e6f34ba454674bd4fd7176
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-18 18:02:37 -0400

    GRANTS - UPDATED EM TEST CASES FO I2E ROLE DROP DOWN FUNCTIONALITIES

--- src/test/java/appsCommon/Pages/ITrustLoginPage.java
+++ src/test/java/appsCommon/Pages/ITrustLoginPage.java
@@ -10,76 +10,91 @@ import com.nci.automation.web.JavascriptUtils;
 import com.nci.automation.web.WebDriverUtils;
 import com.nci.automation.xceptions.TestingException;
 
-public class ITrustLoginPage extends CommonUtils{
+public class ITrustLoginPage extends CommonUtils {
 
-	@FindBy(xpath = "//ul[@class='nav navbar-nav navbar-right hidden-xs hidden-sm ng-scope']//li//a")
-	public WebElement loginLink;
-	
-	/** Username field*/
-	@FindBy(xpath = "//input[@id='USER']")
-	public WebElement userNameField;
+    @FindBy(xpath = "//ul[@class='nav navbar-nav navbar-right hidden-xs hidden-sm ng-scope']//li//a")
+    public WebElement loginLink;
 
-	/** Password field */
-	@FindBy(xpath = "//input[@id='PASSWORD']")
-	public WebElement passwordField;
+    /**
+     * Username field
+     */
+    @FindBy(xpath = "//input[@id='USER']")
+    public WebElement userNameField;
 
-	/** Sign in button */
-	@FindBy(xpath="//button[@type='submit']")
-	public WebElement signInButton;
-	
-	/** Forgot Password link */
-	@FindBy(xpath="//a[@class='forgot-password']")
-	public WebElement forgotPasswordLink;
-	
-	/** Trouble Signing In link */ 
-	@FindBy(xpath="//a[text()='Trouble signing in?']")
-	public WebElement TroubleSigningInLink;
-	
-	/** Clicks on the user's profile drop down*/
-	@FindBy (xpath = "//button[@id='user-info-dropdown']")
-	public WebElement profileDropDown;
-	
-	/** Clicks on the profile drop down and selects "Sign Out" */
-	@FindBy (xpath = "//a[contains(text(),'Sign Out')]")
-	public WebElement covidCodeSignOutButton;
-	
-	
+    /**
+     * Password field
+     */
+    @FindBy(xpath = "//input[@id='PASSWORD']")
+    public WebElement passwordField;
 
-	// initialize all variables
-	public ITrustLoginPage() {
-		PageFactory.initElements(WebDriverUtils.webDriver, this);
-	}
-	
-	/**
-	 * Enters user-name to ITrust
-	 * @throws TestingException
-	 */
-	public void enterUsername(WebElement userNameField, String username) throws TestingException {
-		userNameField.clear();
-		sendKeys(userNameField, ConfUtils.getProperty(username));
-	}
+    /**
+     * Sign in button
+     */
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement signInButton;
 
-	/**
-	 * Enters password to ITrust
-	 * @throws TestingException
-	 */
-	public void enterPassword(String password) throws TestingException {
-		passwordField.clear();
-		String decryptedPass=EncryptionUtils.decrypt(ConfUtils.getProperty(password));
-		sendKeys(passwordField, decryptedPass);
-	}
-	
-	/**
-	 * Clicks ITrust sign-in button
-	 */
-	public void clickSignInButton(){
-		CommonUtils.clickOnElement(signInButton);
-	}
-	
-	/** Clicks on log in button on the COVIDCode home page */
-	public void covidCodeLogInButton() {
-	JavascriptUtils.clickByJS(loginLink);
-	
-	}
-	
+    /**
+     * Forgot Password link
+     */
+    @FindBy(xpath = "//a[@class='forgot-password']")
+    public WebElement forgotPasswordLink;
+
+    /**
+     * Trouble Signing In link
+     */
+    @FindBy(xpath = "//a[text()='Trouble signing in?']")
+    public WebElement TroubleSigningInLink;
+
+    /**
+     * Clicks on the user's profile drop down
+     */
+    @FindBy(xpath = "//button[@id='user-info-dropdown']")
+    public WebElement profileDropDown;
+
+    /**
+     * Clicks on the profile drop down and selects "Sign Out"
+     */
+    @FindBy(xpath = "//a[contains(text(),'Sign Out')]")
+    public WebElement covidCodeSignOutButton;
+
+    public ITrustLoginPage() {
+        PageFactory.initElements(WebDriverUtils.webDriver, this);
+    }
+
+    /**
+     * Enters a username into the specified username field.
+     *
+     * @param userNameField The web element representing the username field.
+     * @param username      The username to be entered.
+     */
+    public void enterUsername(WebElement userNameField, String username) {
+        userNameField.clear();
+        sendKeys(userNameField, ConfUtils.getProperty(username));
+    }
+
+    /**
+     * Enters a password into the password field.
+     *
+     * @param password The password to be entered.
+     */
+    public void enterPassword(String password) {
+        passwordField.clear();
+        String decryptedPass = EncryptionUtils.decrypt(ConfUtils.getProperty(password));
+        sendKeys(passwordField, decryptedPass);
+    }
+
+    /**
+     * Clicks on the Sign In button on the iTrust login page.
+     * The method uses the CommonUtils class to perform the click action.
+     */
+    public void clickSignInButton() {
+        CommonUtils.clickOnElement(signInButton);
+    }
+
+    /**
+     * Clicks on log in button on the COVIDCode home page
+     */
+    public void covidCodeLogInButton() {
+        JavascriptUtils.clickByJS(loginLink);
+    }
 }
