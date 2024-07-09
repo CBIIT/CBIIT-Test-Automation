993893a2ec5a019cb94e7e62b13b2474be9d9ff8
iamdez99 <diegojuarezbusiness@gmail.com>
2024-07-09 19:18:59 -0400

    SSJ Automation

--- src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Steps/ApplicantProfileSteps.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Steps/ApplicantProfileSteps.java
@@ -3,12 +3,20 @@ package ServiceNow.PlatformBusinessApps.SSJ.playwright.Steps;
 import ServiceNow.PlatformBusinessApps.SSJ.playwright.Pages.Profile_Tab_Page;
 import ServiceNow.PlatformBusinessApps.SSJ.playwright.StepsImplementation.ApplicantProfileStepsImpl;
 import ServiceNow.PlatformBusinessApps.SSJ.playwright.StepsImplementation.Reset_Account_StepsImpl;
+import ServiceNow.PlatformBusinessApps.SSJ.utils.SSJ_Constants;
+import appsCommon.Pages.Playwright_Common_Locators;
+import com.microsoft.playwright.ElementHandle;
 import com.nci.automation.utils.CucumberLogUtils;
 import com.nci.automation.utils.MiscUtils;
 import com.nci.automation.web.PlaywrightUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+import java.nio.file.Paths;
+
+import static com.nci.automation.web.PlaywrightUtils.page;
 
 public class ApplicantProfileSteps {
 
@@ -19,37 +27,37 @@ public class ApplicantProfileSteps {
 
     @Given("User is on Profile tab - PW")
     public void user_is_on_profile_tab_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.profileTab).click();
+        page.locator(Profile_Tab_Page.profileTab).click();
     }
 
     @Given("User inputs {string} into first name field - PW")
     public void user_inputs_into_first_name_field_pw(String firstName) {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.firstNameTextBox).fill(firstName);
+        page.locator(Profile_Tab_Page.firstNameTextBox).fill(firstName);
     }
 
     @Given("User inputs {string} into middle name field - PW")
     public void user_inputs_into_middle_name_field_pw(String middleName) {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.middleNameTextBox).fill(middleName);
+        page.locator(Profile_Tab_Page.middleNameTextBox).fill(middleName);
     }
 
     @Given("User inputs {string} into last name field - PW")
     public void user_inputs_into_last_name_field_pw(String lastName) {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.lastNameTextBox).fill(lastName);
+        page.locator(Profile_Tab_Page.lastNameTextBox).fill(lastName);
     }
 
     @Given("User inputs {string} into email field - PW")
     public void user_inputs_into_email_field_pw(String email) {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.emailTextBox).fill(email);
+        page.getByLabel(Profile_Tab_Page.emailTextBox).fill(email);
     }
 
     @Given("User inputs {string} into phone field - PW")
     public void user_inputs_into_phone_field_pw(String phoneNumber) {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.phoneNumberTextBox).fill(phoneNumber);
+        page.locator(Profile_Tab_Page.phoneNumberTextBox).fill(phoneNumber);
     }
 
     @Given("User inputs {string} into business phone field - PW")
     public void user_inputs_into_business_phone_field_pw(String businessPhone) {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.businessPhoneNumberTextBox).fill(businessPhone);
+        page.locator(Profile_Tab_Page.businessPhoneNumberTextBox).fill(businessPhone);
     }
 
     @Given("selects highest education {string}")
@@ -59,43 +67,43 @@ public class ApplicantProfileSteps {
 
     @Given("User confirms being a US Citizen - PW")
     public void user_confirms_being_a_us_citizen_pw() {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.are_You_A_US_Citizen_Yes_Radio_Button).check();
+        page.getByLabel(Profile_Tab_Page.are_You_A_US_Citizen_Yes_Radio_Button).check();
     }
 
     @Given("User inputs {string} into address field - PW")
     public void user_inputs_into_address_field_pw(String address) {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.addressTextBox).fill(address);
+        page.getByLabel(Profile_Tab_Page.addressTextBox).fill(address);
     }
 
     @Given("User inputs {string} into appNumber field - PW")
     public void user_inputs_into_app_number_field_pw(String aptNumber) {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.apartmentOrSuiteNumberTextBox).fill(aptNumber);
+        page.locator(Profile_Tab_Page.apartmentOrSuiteNumberTextBox).fill(aptNumber);
     }
 
     @Given("User inputs {string} into city field - PW")
     public void user_inputs_into_city_field_pw(String city) {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.cityTextBox).fill(city);
+        page.getByLabel(Profile_Tab_Page.cityTextBox).fill(city);
     }
 
     @Given("User inputs {string} into state field - PW")
     public void user_inputs_into_state_field_pw(String state) {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.stateTextBox).fill(state);
+        page.getByLabel(Profile_Tab_Page.stateTextBox).fill(state);
     }
 
     @Given("User inputs {string} into country field - PW")
     public void user_inputs_into_country_field_pw(String country) {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.countryTextBox).fill(country);
-        CucumberLogUtils.playwrightScreenshot(PlaywrightUtils.page);
+        page.getByLabel(Profile_Tab_Page.countryTextBox).fill(country);
+        CucumberLogUtils.playwrightScreenshot(page);
     }
 
     @Given("User inputs {string} into zip field - PW")
     public void user_inputs_into_zip_field_pw(String zipCode) {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.zipCodeTextBox).fill(zipCode);
+        page.getByLabel(Profile_Tab_Page.zipCodeTextBox).fill(zipCode);
     }
 
     @When("User clicks on Save Application button - PW")
     public void user_clicks_on_save_application_button_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.saveButton).click();
+        page.locator(Profile_Tab_Page.saveButton).click();
     }
 
     @Then("user verifies that first name {string}, middle name {string}, and last name {string} saved display as expected")
@@ -155,31 +163,110 @@ public class ApplicantProfileSteps {
 
     @When("User edits sex choice - PW")
     public void user_edits_sex_choice_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.sexMaleOption).click();
+        page.locator(Profile_Tab_Page.sexMaleOption).click();
     }
 
     @When("User edits ethnicity choice - PW")
     public void user_edits_ethnicity_choice_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.ethnicityChoiceHispanicLatino).click();
+        page.locator(Profile_Tab_Page.ethnicityChoiceHispanicLatino).click();
     }
 
     @When("User edits race choice - PW")
     public void user_edits_race_choice_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.raceOptionAmericanIndianAlaska).click();
+        page.locator(Profile_Tab_Page.raceOptionAmericanIndianAlaska).click();
     }
 
     @When("User edits disability choice - PW")
     public void user_edits_disability_choice_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.disabilityChoiceDeafSeriousDifficulty).click();
+        page.locator(Profile_Tab_Page.disabilityChoiceDeafSeriousDifficulty).click();
     }
 
     @When("User chooses not to share demographic details - PW")
     public void user_chooses_not_to_share_demographic_details_pw() {
-        PlaywrightUtils.page.getByLabel(Profile_Tab_Page.shareNotDemographicsRadioButton).check();
+        page.getByLabel(Profile_Tab_Page.shareNotDemographicsRadioButton).check();
     }
 
     @Then("User saves the updated section - PW")
     public void user_saves_the_updated_section_pw() {
-        PlaywrightUtils.page.locator(Profile_Tab_Page.SaveDemographicsSection).click();
+        page.locator(Profile_Tab_Page.SaveDemographicsSection).click();
+    }
+
+    @When("clicks on a Vacancy Title")
+    public void clicks_on_a_vacancy_title() {
+        page.locator("//tbody/tr[2]/td/a").click();
+    }
+
+    @When("clicks {string}")
+    public void clicks(String text) {
+        page.locator(Playwright_Common_Locators.dynamicTextLocator(text)).click();
+    }
+
+    @When("proceeds with completing profile by clicking {string}")
+    public void proceeds_with_completing_profile_by_clicking(String text) {
+        page.locator(Playwright_Common_Locators.dynamicTextLocator(text)).click();
+    }
+
+    @Then("navigates to the {string} tab to be able to apply to a vacancy after creating a profile")
+    public void navigates_to_the_tab_to_be_able_to_apply_to_a_vacancy_after_creating_a_profile(String text) {
+        page.locator(Playwright_Common_Locators.dynamicTextLocator(text)).click();
+    }
+
+    @Then("user is on the {string} page")
+    public void user_is_on_the_page(String expectedApplicationText) {
+        String actualApplicationText = page.locator("(//h3)[1]").innerText();
+        Assert.assertEquals(actualApplicationText, expectedApplicationText);
+    }
+
+    @Then("uploads cover letter")
+    public void uploads_cover_letter() {
+        ElementHandle fileInput = page.querySelector("(//input[@type='file'])[1]");
+        fileInput.setInputFiles(Paths.get(SSJ_Constants.SSJ_COVER_LETTER));
+    }
+
+    @Then("uploads qualification statement")
+    public void uploads_qualification_statement() {
+        ElementHandle fileInput = page.querySelector("(//input[@type='file'])[2]");
+        fileInput.setInputFiles(Paths.get(SSJ_Constants.SSJ_QUALIFICATION_STATEMENT));
+    }
+
+    @Then("user enters Reference One first name {string}")
+    public void user_enters_reference_one_first_name(String firstNameText) {
+        page.getByLabel("First Name").fill(firstNameText);
+    }
+
+    @Then("enters Reference One middle name {string}")
+    public void enters_reference_one_middle_name(String middleNameText) {
+        page.getByLabel("Middle Name").fill(middleNameText);
+    }
+
+    @Then("enters Reference One last name {string}")
+    public void enters_reference_one_last_name(String lastNameText) {
+        page.getByLabel("Last Name").fill(lastNameText);
+    }
+
+    @Then("enters Reference One email address {string}")
+    public void enters_reference_one_email_address(String emailAddress) {
+        page.getByLabel("Email Address").fill(emailAddress);
+    }
+
+    @Then("enters Reference One phone number {string}")
+    public void enters_reference_one_phone_number(String phoneNumber) {
+        page.getByPlaceholder("(123) 456-").fill(phoneNumber);
+    }
+
+    @Then("selects Reference One relationship as {string}")
+    public void selects_reference_one_relationship_as(String relationship) {
+        page.getByLabel("Relationship").click();
+        page.getByTitle(relationship).locator("div").click();
+    }
+
+    @Then("enters Reference One position title {string}")
+    public void enters_reference_one_position_title(String positionTitle) {
+        page.getByLabel("Position Title (If Applicable)").fill(positionTitle);
+    }
+
+    @Then("enters Reference One organization name {string}")
+    public void enters_reference_one_organization_name(String organizationName) {
+        page.getByLabel("Organization").fill(organizationName);
     }
 }
---
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/utils/SSJ_Constants.java
@@ -0,0 +1,7 @@
+package ServiceNow.PlatformBusinessApps.SSJ.utils;
+
+public class SSJ_Constants {
+
+    public static final String SSJ_COVER_LETTER = "src/test/java/ServiceNow/PlatformBusinessApps/SSJ/utils/SSJ_COVER_LETTER_TEST.docx";
+    public static final String SSJ_QUALIFICATION_STATEMENT = "src/test/java/ServiceNow/PlatformBusinessApps/SSJ/utils/SSJ_QUALIFICATION_STATEMENT_TEST.pdf";
+}
