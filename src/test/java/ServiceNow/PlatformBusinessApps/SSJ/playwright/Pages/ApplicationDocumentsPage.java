73d294273900e221af39b802672518b2b2d8d818
iamdez99 <diegojuarezbusiness@gmail.com>
2024-07-12 14:43:09 -0400

    Scenario Outline: Verifying applicant is able to see Your Applications tab and page content after applying to a Vacancy

--- src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Pages/ApplicationDocumentsPage.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/playwright/Pages/ApplicationDocumentsPage.java
@@ -2,6 +2,12 @@ package ServiceNow.PlatformBusinessApps.SSJ.playwright.Pages;
 
 public class ApplicationDocumentsPage {
 
+    /**
+     * Locates an input element based on the given text.
+     *
+     * @param text The text used to locate the input element.
+     * @return The XPath query used to locate the input element.
+     */
     public static String dynamicDocLocator(String text){
         return "//*[text()='"+ text + "']//following-sibling::div/div/div/div/div/div/span/div/span/input";
     }
