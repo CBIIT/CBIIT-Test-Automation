0efa668c7c74efa49f8b0fd17064453605697287
mariachaudhry <maria.chaudhry2@nih.gov>
2024-08-20 13:36:43 -0400

    pull latest changes along with my commiting my test updates

---
+++ src/test/java/ServiceNow/PlatformBusinessApps/NERD/selenium/Steps/NERD_NCI_StaffMembers_Steps.java
@@ -0,0 +1,25 @@
+package ServiceNow.PlatformBusinessApps.NERD.selenium.Steps;
+
+import ServiceNow.PlatformBusinessApps.NERD.selenium.Constants.ReturningSubmissions_Constants;
+import ServiceNow.PlatformBusinessApps.NERD.selenium.Constants.TopAccomplishmentsSubmission_Constants;
+import ServiceNow.PlatformBusinessApps.NERD.selenium.StepsImplementation.NERDApplicationStepsImplementation;
+import ServiceNow.PlatformBusinessApps.NERD.selenium.StepsImplementation.NERD_NCI_StaffMemberStepsImplementation;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class NERD_NCI_StaffMembers_Steps {
+    @Given("an NCI Staff Member not on the NERD table is on the NERD CRS Knowledge Base")
+    public void an_nci_staff_member_not_on_the_nerd_table_is_on_the_nerd_crs_knowledge_base() {
+        NERD_NCI_StaffMemberStepsImplementation.an_nci_staff_member_not_on_the_nerd_table_is_on_the_nerd_crs_knowledge_base();
+    }
+
+    @When("NCI Staff Member submits new Top Accomplishments submission")
+    public void nci_staff_member_submits_new_top_accomplishments_submission() {
+        NERD_NCI_StaffMemberStepsImplementation.nci_staff_member_submits_new_top_accomplishments_submission();
+    }
+
+    @When("DOC Planning Contact returning the Top Accomplishments submission back to NCI Staff Member")
+    public void doc_planning_contact_returning_the_top_accomplishments_submission_back_to_nci_staff_member() {
+        NERD_NCI_StaffMemberStepsImplementation.doc_planning_contact_returning_the_top_accomplishments_submission_back_to_nci_staff_member(TopAccomplishmentsSubmission_Constants.TOP_ACCOMPLISHMENTS_SUBMISSION_NAME);
+    }
+}
