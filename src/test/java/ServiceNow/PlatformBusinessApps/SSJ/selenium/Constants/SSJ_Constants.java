200eabcf17553d608f10c3a8d5ae414802210e45
nekrashevicha2 <nekrashevicha2@nih.gov>
2024-08-21 19:41:56 -0400

    upd

--- src/test/java/ServiceNow/PlatformBusinessApps/SSJ/selenium/Constants/SSJ_Constants.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/SSJ/selenium/Constants/SSJ_Constants.java
@@ -1,7 +1,7 @@
 package ServiceNow.PlatformBusinessApps.SSJ.selenium.Constants;
 
 public class SSJ_Constants {
-    public static final String OWM_VACANCY_MANAGER = "Holly Gemar-Griffith";
+    public static final String OWM_VACANCY_MANAGER = "Susan Greenhouse";
     public static final String STADTMAN_VACANCY_MANAGER = "Roland Owens";
     public static final String OKTA_APPLICANT = "Abhishek Desai";
 }
