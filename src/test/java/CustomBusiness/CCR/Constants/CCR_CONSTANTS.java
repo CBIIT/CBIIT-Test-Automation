3a148c31c45887efe06864c0bc3b0aea756c466c
nekrashevicha2 <nekrashevicha2@nih.gov>
2024-08-22 11:18:36 -0400

    upd

--- src/test/java/CustomBusiness/CCR/Constants/CCR_CONSTANTS.java
+++ src/test/java/CustomBusiness/CCR/Constants/CCR_CONSTANTS.java
@@ -1,11 +1,8 @@
 package CustomBusiness.CCR.Constants;
 
-import com.nci.automation.web.CommonUtils;
-
 public class CCR_CONSTANTS {
     public static final String APPLICANT = "Alena Nekrashevich";
     public static final String ADMIN = "Lori Holliday";
     public static final String CV = "C:\\Users\\nekrashevicha2\\Desktop\\CBIIT-Test-Automation\\src\\test\\java\\CustomBusiness\\CCR\\Resources\\CV.docx";
     public static final String RESEARCH_GOALS ="C:\\Users\\nekrashevicha2\\Desktop\\CBIIT-Test-Automation\\src\\test\\java\\CustomBusiness\\CCR\\Resources\\ResearchGoals.docx";
-
 }
