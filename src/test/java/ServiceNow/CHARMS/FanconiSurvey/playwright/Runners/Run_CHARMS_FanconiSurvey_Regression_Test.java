ec78eb811bee1eb2bcc90c5b5b76b79a28167a55
Sonika Jain <sonika.jain@nih.gov>
2024-09-10 11:19:38 -0400

    Completed the Fanconi Reproductive History, Sexual History, Surgical and Other Medical History modules for Scenario 1

--- src/test/java/ServiceNow/CHARMS/FanconiSurvey/playwright/Runners/Run_CHARMS_FanconiSurvey_Regression_Test.java
+++ src/test/java/ServiceNow/CHARMS/FanconiSurvey/playwright/Runners/Run_CHARMS_FanconiSurvey_Regression_Test.java
@@ -3,13 +3,12 @@ package ServiceNow.CHARMS.FanconiSurvey.Playwright.Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-/* we would have to come back and update this runners class at a later time.*/
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/ServiceNow/CHARMS/FanconiSurvey/Playwright/Features",
         glue = {"ServiceNow.CHARMS.FanconiSurvey.Playwright.Steps","Hooks"},
-        tags = "@Testing",
+        tags = "@In-Progress",
         dryRun = false
 )
 public class Run_CHARMS_FanconiSurvey_Regression_Test extends AbstractTestNGCucumberTests {
---
+++ src/test/java/ServiceNow/CHARMS/FanconiSurvey/playwright/Steps/FanconiSurveySexualHistorySteps.java
@@ -0,0 +1,12 @@
+package ServiceNow.CHARMS.FanconiSurvey.Playwright.Steps;
+
+import ServiceNow.CHARMS.FanconiSurvey.Playwright.StepsImplementation.FanconiSurveyStepsImpl;
+import io.cucumber.java.en.Given;
+
+public class FanconiSurveySexualHistorySteps {
+
+    @Given("fills the Sexual History section for scenario 1")
+    public void fills_the_sexual_history_section_for_scenario_1() {
+        FanconiSurveyStepsImpl.sexualHistorySectionScenario1();
+    }
+}
