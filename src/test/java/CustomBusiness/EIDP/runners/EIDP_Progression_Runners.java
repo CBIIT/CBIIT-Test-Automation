87e386de5e41bdc0fc0aed17843bcf23cfefd68f
iamdez99 <diegojuarezbusiness@gmail.com>
2024-09-12 13:46:50 -0400

    Adding SSJ-Regression to gitignore until server issues are resolved

--- src/test/java/CustomBusiness/EIDP/runners/EIDP_Progression_Runners.java
+++ src/test/java/CustomBusiness/EIDP/runners/EIDP_Progression_Runners.java
@@ -3,7 +3,7 @@ package CustomBusiness.EIDP.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/EIDP-smoke-reports/EID-smoke-report.html", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/EIDP-smoke-reports/EIDP-smoke-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         , features = {"src/test/java/CustomBusiness/EIDP/Features"}
