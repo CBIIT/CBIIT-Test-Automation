9c74712e4c57901dc8265d2c766948e42d685bff
mariachaudhry <maria.chaudhry2@nih.gov>
2024-09-12 14:49:26 -0400

    ..

--- src/test/java/ServiceNow/PlatformBusinessApps/GDC/GDC_Runners/RunGDCProgressionTest.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/GDC/GDC_Runners/RunGDCProgressionTest.java
@@ -3,8 +3,8 @@ package ServiceNow.PlatformBusinessApps.GDC.GDC_Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
-        "junit:target/cucumber.xml", "rerun:target/failed.txt",
+@CucumberOptions(plugin = {"html:target/gdc-progression-reports/gdc-progression-report.html",
+        "json:target/cucumber.json", "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/ServiceNow/PlatformBusinessApps/GDC/Features",
         glue = {"ServiceNow.PlatformBusinessApps.GDC.Steps", "Hooks"},
@@ -12,5 +12,4 @@ import io.cucumber.testng.CucumberOptions;
         dryRun = false
 )
 public class RunGDCProgressionTest extends AbstractTestNGCucumberTests{
-
 }
--- src/test/java/ServiceNow/PlatformBusinessApps/GDC/GDC_Runners/RunGDCRegressionTest.java
+++ src/test/java/ServiceNow/PlatformBusinessApps/GDC/GDC_Runners/RunGDCRegressionTest.java
@@ -3,8 +3,8 @@ package ServiceNow.PlatformBusinessApps.GDC.GDC_Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
-        "junit:target/cucumber.xml", "rerun:target/failed.txt",
+@CucumberOptions(plugin = {"html:target/gdc-regression-reports/gdc-regression-report.html",
+        "json:target/cucumber.json", "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/ServiceNow/PlatformBusinessApps/GDC/Features",
         glue = {"ServiceNow.PlatformBusinessApps.GDC.Steps", "Hooks"},
