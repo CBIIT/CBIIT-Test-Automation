4343bebbcec49b6ee34aed300c81a16e77d96031
iamdez99 <diegojuarezbusiness@gmail.com>
2024-09-17 12:06:41 -0400

    Testing RAS GitHub Actions failure

--- src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Regression_Test.java
+++ src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Regression_Test.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		features = {"src/test/java/ServiceNow/CHARMS/Features/RASAccountReset", "src/test/java/ServiceNow/CHARMS/Features/RasopathyStudy"},
 		glue = {"ServiceNow.CHARMS.Steps", "Hooks"},
-		tags = "@RAS_Regression",
+		tags = "@Testing",
 		dryRun = false
 )
 /**
