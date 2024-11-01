6f2d599eaa3f6fef6c7e0a1e0a2aa9c8e068aa06
Sarwar <sarwara2@nci-02277698-ml.lan>
2024-11-01 10:50:45 -0400

    Completed test cases

--- src/test/java/ServiceNow/ESR/Playwright/Steps/ESRTaskSteps.java
+++ src/test/java/ServiceNow/ESR/Playwright/Steps/ESRTaskSteps.java
@@ -35,4 +35,13 @@ public class ESRTaskSteps {
     public void the_user_confirms_the_esr_board_intake_review_catalog_task_is_completed() {
         ESRTaskStepsImplementation.confirmESRBoardIntakeReviewCatalogTaskIsCompleted();
     }
+
+    @And("the Engineering Project Execution catalog task is completed")
+    public void the_engineering_project_execution_catalog_task_is_completed() {
+        ESRTaskStepsImplementation.completeEngineeringProjectExecutionCatalogTask();
+    }
+    @Then("the user confirms the Engineering Project Execution catalog task is completed")
+    public void the_user_confirms_the_engineering_project_execution_catalog_task_is_completed() {
+        ESRTaskStepsImplementation.confirmEngineeringProjectExecutionCatalogTaskIsCompleted();
+    }
 }
--- src/test/java/ServiceNow/ESR/Playwright/StepsImplementation/ESRTaskStepsImplementation.java
+++ src/test/java/ServiceNow/ESR/Playwright/StepsImplementation/ESRTaskStepsImplementation.java
@@ -131,4 +131,17 @@ public class ESRTaskStepsImplementation {
     public static void confirmESRBoardIntakeReviewCatalogTaskIsCompleted() {
         assertThat(Playwright_Common_Locators.iframeLocator().locator("//select[@aria-label='Approval']")).containsText("Requested");
     }
+
+    public static void completeEngineeringProjectExecutionCatalogTask() {
+        Playwright_Common_Locators.iframeLocator().locator("#tabs2_list").getByText("Catalog Tasks (3)").click();
+        Playwright_Common_Locators.iframeLocator().locator("(//td[@class='vt']/a)[1]").click();
+        Playwright_Common_Locators.iframeLocator().locator("//input[@aria-labelledby='label.sc_task.assigned_to']").click();
+        Playwright_Common_Locators.iframeLocator().locator("//input[@aria-labelledby='label.sc_task.assigned_to']").fill("CBIIT Test Account");
+        Playwright_Common_Locators.iframeLocator().getByLabel("Catalog Task form section").getByLabel("State").selectOption("3");
+        Playwright_Common_Locators.iframeLocator().locator("#sysverb_update").click();
+    }
+
+    public static void confirmEngineeringProjectExecutionCatalogTaskIsCompleted() {
+        assertThat(Playwright_Common_Locators.iframeLocator().locator("//select[@aria-label='Stage']")).containsText("Closeout Preparation");
+    }
 }
