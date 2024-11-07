07fef636705c1c60705935110b980b56877faaab
Muzipova <muzipovay2@nci-02322453-ml.fios-router.home>
2024-11-07 15:40:33 -0500

    Updated Fanconi Regression runner and GitHub Actions Fanconi Regression Workflow

--- src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_Fanconi_Regression_Test.java
+++ src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_Fanconi_Regression_Test.java
@@ -3,7 +3,7 @@ package ServiceNow.CHARMS.Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/fanconi-regression-reports/fanconi-regression-report.html", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/html-charms-fanconi-regression-reports/fanconi-regression-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/ServiceNow/CHARMS/Features/FanconiStudy",
