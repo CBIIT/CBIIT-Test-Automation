43aad8fb60c96c0270c51b6c1b83b0ee16d85839
Muzipova <muzipovay2@nci-02322453-ml.fios-router.home>
2024-11-07 15:05:55 -0500

    ..

--- src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_Fanconi_Regression_Test.java
+++ src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_Fanconi_Regression_Test.java
@@ -3,7 +3,7 @@ package ServiceNow.CHARMS.Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
 		"junit:target/cucumber.xml", "rerun:target/failed.txt",
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 features = "src/test/java/ServiceNow/CHARMS/Features/FanconiStudy",
