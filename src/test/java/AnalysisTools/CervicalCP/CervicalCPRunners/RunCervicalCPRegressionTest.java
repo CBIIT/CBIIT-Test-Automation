0393c387c54ab4170344f3f11d0e80e86a1e923c
iamdez99 <diegojuarezbusiness@gmail.com>
2024-11-07 17:22:48 -0500

    Updating GitHub Actions Worklows

--- src/test/java/AnalysisTools/CervicalCP/CervicalCPRunners/RunCervicalCPRegressionTest.java
+++ src/test/java/AnalysisTools/CervicalCP/CervicalCPRunners/RunCervicalCPRegressionTest.java
@@ -3,7 +3,7 @@ package AnalysisTools.CervicalCP.CervicalCPRunners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/CCP-reports/CCP-Regression-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = {"src/test/java/AnalysisTools/CervicalCP/playwright/Features", "src/test/java/AnalysisTools/CervicalCP/selenium/Features"},
--- src/test/java/GrantsApps/GrantsRunners/EnterpriseMaintenance/RunEMRegressionTest.java
+++ src/test/java/GrantsApps/GrantsRunners/EnterpriseMaintenance/RunEMRegressionTest.java
@@ -3,7 +3,7 @@ package GrantsApps.GrantsRunners.EnterpriseMaintenance;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/EM-reports/EM-Regression-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         , features = {"src/test/java/GrantsApps/EM/selenium/Features", "src/test/java/GrantsApps/EM/playwright/Features"}
