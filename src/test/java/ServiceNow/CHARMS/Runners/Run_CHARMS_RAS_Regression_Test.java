016d008de7305bc3fbe39c4e5db9eb224e76b76a
iamdez99 <diegojuarezbusiness@gmail.com>
2024-11-12 11:34:13 -0500

    Testing actions

--- src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Regression_Test.java
+++ src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Regression_Test.java
@@ -8,12 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		features = {"src/test/java/ServiceNow/CHARMS/Features/RASAccountReset", "src/test/java/ServiceNow/CHARMS/Features/RasopathyStudy"},
 		glue = {"ServiceNow.CHARMS.Steps", "Hooks"},
-		tags = "@RAS_Regression",
+		tags = "@TestingActions",
 		dryRun = false
 )
-/**
- * 
- * @author sohilz2
- */
 public class Run_CHARMS_RAS_Regression_Test extends AbstractTestNGCucumberTests{
 }
