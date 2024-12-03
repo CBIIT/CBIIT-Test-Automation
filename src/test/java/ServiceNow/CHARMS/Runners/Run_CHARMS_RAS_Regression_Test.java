122f07c620404c77ce81a4e7027bb97c8423071e
iamdez99 <diegojuarezbusiness@gmail.com>
2024-12-03 11:53:37 -0500

    Modifying and adding workflows

--- src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Regression_Test.java
+++ src/test/java/ServiceNow/CHARMS/Runners/Run_CHARMS_RAS_Regression_Test.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		features = {"src/test/java/ServiceNow/CHARMS/Features/RASAccountReset", "src/test/java/ServiceNow/CHARMS/Features/RasopathyStudy"},
 		glue = {"ServiceNow.CHARMS.Steps", "Hooks"},
-		tags = "@TestingActions",
+		tags = "@Regression",
 		dryRun = false
 )
 public class Run_CHARMS_RAS_Regression_Test extends AbstractTestNGCucumberTests{
