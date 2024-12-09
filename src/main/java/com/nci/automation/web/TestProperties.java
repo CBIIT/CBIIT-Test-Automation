51c108997a293dd577b55e1ce061bc953a5c5b0d
gugulothus2 <satya.gugulothu@nih.gov>
2024-12-09 15:39:35 -0500

    addressed review comments

--- src/main/java/com/nci/automation/web/TestProperties.java
+++ src/main/java/com/nci/automation/web/TestProperties.java
@@ -282,11 +282,11 @@ public class TestProperties {
     /**
      * JP-SURV URLS
      */
-    public static String JP_SURV_TEST_URL = "https://analysistools-test.cancer.gov/jpsurv/";
+    public static String JP_SURV_TEST_URL = "https://analysistools-qa.cancer.gov/jpsurv/";
     public static String JP_SURV_STAGE_URL = "https://analysistools-stage.cancer.gov/jpsurv/";
     public static String JP_SURV_URL;
 
-    /**
+    /**f
      * GWAS TARGET URLS
      */
     public static String GWAS_TARGET_TEST_URL = "https://analysistools-qa.cancer.gov/gwas-target/home";
