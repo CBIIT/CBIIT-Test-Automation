bdd577090cb7559950df31f47ecb05e74e91bd3e
Muzipova <muzipovay2@nci-02322453-ml.fios-router.home>
2024-12-09 13:41:25 -0500

    ..

--- src/test/java/CHARMS/Runners/Run_CHARMS_Progression_Test.java
+++ src/test/java/CHARMS/Runners/Run_CHARMS_Progression_Test.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features = "src/test/java/ServiceNow/CHARMS/Features",
-        glue = {"ServiceNow.CHARMS.Steps", "Hooks"},
+        features = "src/test/java/CHARMS/Features",
+        glue = {"CHARMS.Steps", "Hooks"},
         tags = "@Progression",
         dryRun = false
 )
