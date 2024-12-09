f24436992e0e3a7ebdc9dbceb7229f20334b7f16
mariachaudhry <maria.chaudhry2@nih.gov>
2024-12-09 13:07:47 -0500

    ..

--- src/test/java/PLATFORM_BUSINESS/NERD/NERD_Runners/Run_NERD_Regression_Test.java
+++ src/test/java/PLATFORM_BUSINESS/NERD/NERD_Runners/Run_NERD_Regression_Test.java
@@ -1,4 +1,4 @@
-package ServiceNow.PlatformBusinessApps.NERD.NERD_Runners;
+package PLATFORM_BUSINESS.NERD.NERD_Runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
