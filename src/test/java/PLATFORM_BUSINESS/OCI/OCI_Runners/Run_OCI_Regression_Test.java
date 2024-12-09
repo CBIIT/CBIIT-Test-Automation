f35065c8f3313bdae7b90b90b3574bc972622bee
mariachaudhry <maria.chaudhry2@nih.gov>
2024-12-09 18:05:13 -0500

    ..

--- src/test/java/PLATFORM_BUSINESS/OCI/OCI_Runners/Run_OCI_Regression_Test.java
+++ src/test/java/PLATFORM_BUSINESS/OCI/OCI_Runners/Run_OCI_Regression_Test.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin= {"html:target/onboard-progression-reports/onboard-progression-report.html",
         "json:target/cucumber.json", "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features= "src/test/java/ServiceNow/PlatformBusinessApps/OCI/Features",
-        glue={"ServiceNow.PlatformBusinessApps.OCI.Steps", "Hooks"},
+        features= "src/test/java/PLATFORM_BUSINESS/OCI/Features",
+        glue={"ServiceNow.PLATFORM_BUSINESS.OCI.Steps", "Hooks"},
         tags="@Regression",
         dryRun = false
 )
