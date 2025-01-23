92ac9b944a70805d0ef7d1df103806e0ad64f9a4
Nesar <nesarh2@nih.gov>
2025-01-23 18:37:06 -0500

    Verify contract details & Export Search results

--- src/test/java/CUSTOM_BUSINESS/OASYS/Utils/OASYS_Constants.java
+++ src/test/java/CUSTOM_BUSINESS/OASYS/Utils/OASYS_Constants.java
@@ -5,4 +5,12 @@ public class OASYS_Constants {
     public static final String PROJECT_TITLE = "TEST CONTRACT IMPORT";
     public static final String CONTRACT_HEADER = "HHSTESTN01500067W";
     public static final String VENDOR = "SWORD & SHIELD ENTERPRISE";
+    public static final String PROJ_TITLE_IT = "Project Title: IT Commodities and Solutions";
+    public static final String REFPIID_PIID = "REFPIID/PIID: HHSN316201500067W";
+    public static final String ASSIGNED_CO = "CO: CO, Test";
+    public static final String ASSIGNED_CS = "CS: CS1, Test";
+    public static final String ASSIGNED_COR = "COR: COR, Test";
+    public static final String OASYS_SIDE_LOGIN = "https://oasys-qa.cancer.gov/#/login";
+    public static final String OASYS_TEST_USERNAME = "COR";
+    public static final String OASYS_TEST_PASSWORD = "a";
 }
