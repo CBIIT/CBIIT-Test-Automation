bcd3602058ab63d8b274900c40e24ea515180890
matakevin <matakevin1777@gmail.com>
2025-02-03 11:12:00 -0500

    changed BioMarker Runners to match new AT package with cap letters

--- src/test/java/ANALYSIS_TOOLS/BioMarker/BioMarkerRunners/RunBioMarkerSmokeTest.java
+++ src/test/java/ANALYSIS_TOOLS/BioMarker/BioMarkerRunners/RunBioMarkerSmokeTest.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features = {"src/test/java/AnalysisTools/BioMarker/playwright/Features", "src/test/java/AnalysisTools/BioMarker/selenium/Features"},
-        glue = {"AnalysisTools.BioMarker.playwright.Steps", "AnalysisTools.BioMarker.selenium.Steps", "Hooks"},
+        features = {"src/test/java/ANALYSIS_TOOLS/BioMarker/playwright/Features", "src/test/java/ANALYSIS_TOOLS/BioMarker/selenium/Features"},
+        glue = {"ANALYSIS_TOOLS.BioMarker.playwright.Steps", "ANALYSIS_TOOLS.BioMarker.selenium.Steps", "Hooks"},
         tags = "@Smoke",
         dryRun = false
 )
