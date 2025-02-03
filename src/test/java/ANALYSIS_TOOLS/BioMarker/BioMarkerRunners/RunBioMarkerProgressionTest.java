c4f5fa2bc003246616211c97b411005e70abe704
matakevin <matakevin1777@gmail.com>
2025-02-03 11:06:16 -0500

    Added BioMarker and missing CCP runners to new ANALYSIS TOOLS package

--- src/test/java/ANALYSIS_TOOLS/BioMarker/BioMarkerRunners/RunBioMarkerProgressionTest.java
+++ src/test/java/ANALYSIS_TOOLS/BioMarker/BioMarkerRunners/RunBioMarkerProgressionTest.java
@@ -1,4 +1,4 @@
-package ANALYSIS_TOOLS.CervicalCP.runners;
+package ANALYSIS_TOOLS.BioMarker.BioMarkerRunners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
@@ -6,11 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features = {"src/test/java/ANALYSIS_TOOLS/CervicalCP/playwright/features"},
-        glue = {"ANALYSIS_TOOLS.CervicalCP.playwright.steps", "Hooks"},
+        features = {"src/test/java/AnalysisTools/BioMarker/playwright/Features", "src/test/java/AnalysisTools/BioMarker/selenium/Features"},
+        glue = {"AnalysisTools.BioMarker.playwright.Steps", "AnalysisTools.BioMarker.selenium.Steps", "Hooks"},
         tags = "@Progression",
         dryRun = false
 )
 
-public class RunCervicalCPProgressionTest extends AbstractTestNGCucumberTests{
+public class RunBioMarkerProgressionTest extends AbstractTestNGCucumberTests{
 }
