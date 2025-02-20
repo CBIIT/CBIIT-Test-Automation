d570cbbca60ec9aa4d50b393b8eee7dbe357b7a0
gugulothus2 <satya.gugulothu@nih.gov>
2025-02-20 11:59:39 -0500

    updating regression runner file path

--- src/test/java/ANALYSIS_TOOLS/ACT24/runners/RunACT24RegressionTest.java
+++ src/test/java/ANALYSIS_TOOLS/ACT24/runners/RunACT24RegressionTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"html:target/html-reports/cucumber-default-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features = "src/test/java/AnalysisTools/ACT24/Features",
+        features = "src/test/java/ANALYSIS_TOOLS/ACT24/Features",
         glue = {"ANALYSIS_TOOLS.ACT24.steps", "Hooks"},
         tags = "@Regression",
         dryRun = false
