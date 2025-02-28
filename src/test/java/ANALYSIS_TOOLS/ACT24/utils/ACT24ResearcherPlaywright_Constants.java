6715d60540fe960a69736645d41022cc6f3f59e6
gugulothus2 <satya.gugulothu@nih.gov>
2025-02-28 18:09:09 -0500

    Added the participant portal Scenarios

--- src/test/java/ANALYSIS_TOOLS/ACT24/utils/ACT24ResearcherPlaywright_Constants.java
+++ src/test/java/ANALYSIS_TOOLS/ACT24/utils/ACT24ResearcherPlaywright_Constants.java
@@ -1,5 +1,8 @@
 package ANALYSIS_TOOLS.ACT24.utils;
 
+import com.microsoft.playwright.Page;
+import com.nci.automation.web.PlaywrightUtils;
+
 public class ACT24ResearcherPlaywright_Constants {
 
     /***
@@ -18,12 +21,37 @@ public class ACT24ResearcherPlaywright_Constants {
     public static String FOIA_PAGE_TITLE = "Freedom of Information Act - NCI";
 
     /***
-     * HHS PAGE TITLE
+     * HHS POLICY PAGE LINK
      */
-    public static String HHS_PAGE_TITLE = "https://www.hhs.gov/vulnerability-disclosure-policy";
+    public static String HHS_POLICY_PAGE_LINK = "https://www.hhs.gov/vulnerability-disclosure-policy/index.html";
 
     /***
      * PRIVACY AND POLICY PAGE TITLE
      */
     public static String PRIVACY_PAGE_TITLE = "Privacy and Security - NCI";
+
+    /***
+     * HHS PAGE LINK
+     */
+    public static String HHS_PAGE_LINK = "https://www.hhs.gov/";
+
+    /***
+     * NIH PAGE TITLE
+     */
+    public static String NIH_PAGE_TITLE = "National Institutes of Health (NIH) | Turning Discovery Into Health";
+
+    /***
+     * NIC PAGE TITLE
+     */
+    public static String NCI_PAGE_TITLE = "Comprehensive Cancer Information - NCI";
+
+    /***
+     * USA GOV PAGE LINK
+     */
+    public static String USA_GOV_PAGE = "https://www.usa.gov/";
+
+    /***
+     * NEW PAGE CONTEXT
+     */
+    public static Page newPage1 = PlaywrightUtils.context.newPage();
 }
