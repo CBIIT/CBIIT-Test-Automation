43f2e7f5ce1d9e70d3be4be28c9e3dba12d129fe
ymuzipova <ymuzipova@gmail.com>
2025-04-11 20:13:12 -0400

    CHARMS - RAS Survey Verification in Native View

--- src/test/java/CHARMS/stepsImplementation/RASStudy/RAS_Screener_Submissions_StepsImpl.java
+++ src/test/java/CHARMS/stepsImplementation/RASStudy/RAS_Screener_Submissions_StepsImpl.java
@@ -786,10 +786,10 @@ public class RAS_Screener_Submissions_StepsImpl extends PageInitializer {
         CommonUtils.clickOnElement(myRASHomePage.rasoptathyRasSurveyGoButton);
         CommonUtils.switchToNextWindow();
         CommonUtils.sleep(3000);
-        CommonUtils.waitForVisibility(rasopathyQuestionnairePage.dynamicTextBoxLocator("Email"));
-        rasopathyQuestionnairePage.dynamicTextBoxLocator("Email").sendKeys(email);
-        rasopathyQuestionnairePage.pinTextBox.sendKeys(pin);
-        CucumberLogUtils.logScreenshot();
+//        CommonUtils.waitForVisibility(rasopathyQuestionnairePage.dynamicTextBoxLocator("Email"));
+//        rasopathyQuestionnairePage.dynamicTextBoxLocator("Email").sendKeys(email);
+//        rasopathyQuestionnairePage.pinTextBox.sendKeys(pin);
+//        CucumberLogUtils.logScreenshot();
         CommonUtils.sleep(3000);
         ras_screenerSubmissions_stepsImpl.clickOnSurveySavAndNextButton();
         CommonUtils.sleep(3000);
