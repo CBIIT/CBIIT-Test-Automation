9d48a2e312321d04cd05c2da4c2cf6e709415e43
ymuzipova <ymuzipova@gmail.com>
2025-04-18 19:16:21 -0400

    ..

--- src/test/java/CHARMS/pages/RAS_Survey_Native_View_Page.java
+++ src/test/java/CHARMS/pages/RAS_Survey_Native_View_Page.java
@@ -6,11 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import static Hooks.Hooks.softAssert;
 
 public class RAS_Survey_Native_View_Page {
