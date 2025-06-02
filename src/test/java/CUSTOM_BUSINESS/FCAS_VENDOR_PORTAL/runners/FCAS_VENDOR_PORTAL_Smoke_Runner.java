2be726c22d7e34f52d9d0c63ed2398ccc2a796a3
nekrashevicha2 <nekrashevicha2@nih.gov>
2025-06-02 13:34:02 -0400

    upd per review

--- src/test/java/CUSTOM_BUSINESS/FCAS_VENDOR_PORTAL/runners/FCAS_VENDOR_PORTAL_Smoke_Runner.java
+++ src/test/java/CUSTOM_BUSINESS/FCAS_VENDOR_PORTAL/runners/FCAS_VENDOR_PORTAL_Smoke_Runner.java
@@ -3,7 +3,7 @@ package CUSTOM_BUSINESS.FCAS_VENDOR_PORTAL.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-    @CucumberOptions(plugin = {"html:target/fcas-vendor-portal-smoke-reports/tfm-progression-report.html",
+    @CucumberOptions(plugin = {"html:target/fcas-vendor-portal-smoke-reports/fcas-vendor-portal-smoke-report.html",
             "rerun:target/failed.txt",
             "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
             , features = "src/test/java/CUSTOM_BUSINESS/FCAS_VENDOR_PORTAL/features"
