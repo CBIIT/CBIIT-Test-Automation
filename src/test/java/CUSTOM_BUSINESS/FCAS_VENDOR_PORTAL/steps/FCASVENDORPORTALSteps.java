9785783d334cd5b035989a3cff021016afffb96a
nekrashevicha2 <nekrashevicha2@nih.gov>
2025-06-20 13:51:13 -0400

    upd

--- src/test/java/CUSTOM_BUSINESS/FCAS_VENDOR_PORTAL/steps/FCASVENDORPORTALSteps.java
+++ src/test/java/CUSTOM_BUSINESS/FCAS_VENDOR_PORTAL/steps/FCASVENDORPORTALSteps.java
@@ -57,9 +57,9 @@ public class FCASVENDORPORTALSteps {
      */
     @Given("User chooses Existing Contracts Submit Invoice option")
     public void user_chooses_existing_contracts_submit_invoice_option() {
-       CommonUtils.clickOnElement(fCASVENDORPORTALPage.existingContractsRecordMenu);
-       CommonUtils.waitForClickability(fCASVENDORPORTALPage.submitInvoiceOption);
-       CommonUtils.clickOnElement(fCASVENDORPORTALPage.submitInvoiceOption);
+        CommonUtils.clickOnElement(fCASVENDORPORTALPage.existingContractsRecordMenu);
+        CommonUtils.waitForClickability(fCASVENDORPORTALPage.submitInvoiceOption);
+        CommonUtils.clickOnElement(fCASVENDORPORTALPage.submitInvoiceOption);
     }
 
     @Then("User can see Submit Invoice page header")
@@ -69,12 +69,12 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User can see Invoice field")
     public void user_can_see_invoice_field() {
-      FCASVENDORPORTALStepsImplementation.verifyInvoiceField();
+        FCASVENDORPORTALStepsImplementation.verifyInvoiceField();
     }
 
     @Then("User can see Invoice date field")
     public void user_can_see_invoice_date_field() {
-       FCASVENDORPORTALStepsImplementation.verifyInvoiceDateField();
+        FCASVENDORPORTALStepsImplementation.verifyInvoiceDateField();
     }
 
     @Then("User can see Start date field")
@@ -84,17 +84,17 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User can see End date field")
     public void user_can_see_end_date_field() {
-     FCASVENDORPORTALStepsImplementation.verifyEndDateField();
+        FCASVENDORPORTALStepsImplementation.verifyEndDateField();
     }
 
     @Then("User can see Amount field")
     public void user_can_see_amount_field() {
-       FCASVENDORPORTALStepsImplementation.verifyAmountField();
+        FCASVENDORPORTALStepsImplementation.verifyAmountField();
     }
 
     @Then("User can see Submission Type dropdown")
     public void user_can_see_submission_type_dropdown() {
-       FCASVENDORPORTALStepsImplementation.verifySubmissionTypeDropdown();
+        FCASVENDORPORTALStepsImplementation.verifySubmissionTypeDropdown();
     }
 
     @Then("User can see Primary Contact dropdown")
@@ -109,7 +109,7 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User can see Comments field")
     public void user_can_see_comments_field() {
-       FCASVENDORPORTALStepsImplementation.verifyCommentsField();
+        FCASVENDORPORTALStepsImplementation.verifyCommentsField();
     }
 
     @Then("User can see Choose File button")
@@ -119,7 +119,7 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User clicks Cancel button")
     public void user_clicks_cancel_button() {
-       CommonUtils.clickOnElement(fCASVENDORPORTALPage.cancelButton);
+        CommonUtils.clickOnElement(fCASVENDORPORTALPage.cancelButton);
     }
 
     /**
@@ -134,7 +134,7 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User can see Submit Deliverable page header")
     public void user_can_see_submit_deliverable_page_header() {
-    FCASVENDORPORTALStepsImplementation.verifySubmitDeliverablesPageHeader();
+        FCASVENDORPORTALStepsImplementation.verifySubmitDeliverablesPageHeader();
     }
 
     @Then("User can see Status Filter dropdown")
@@ -164,7 +164,7 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User can see Submit COA page header")
     public void user_can_see_submit_coa_page_header() {
-     FCASVENDORPORTALStepsImplementation.verifySubmitCOAPageHeader();
+        FCASVENDORPORTALStepsImplementation.verifySubmitCOAPageHeader();
     }
 
     @Then("User can see COA Requirement dropdown")
@@ -174,98 +174,11 @@ public class FCASVENDORPORTALSteps {
 
     @Then("User can see COA field")
     public void user_can_see_coa_field() {
-       FCASVENDORPORTALStepsImplementation.verifyCOAField();
+        FCASVENDORPORTALStepsImplementation.verifyCOAField();
     }
 
     @Then("User can see Title field")
     public void user_can_see_title_field() {
         FCASVENDORPORTALStepsImplementation.verifyTitleField();
-    }
-
-    /**
-     * User chooses Existing Contracts Submit Work Order option
-     */
-    @Given("User chooses Existing Contracts Submit Work Order option")
-    public void user_chooses_existing_contracts_submit_work_order_option() {
-        CommonUtils.clickOnElement(fCASVENDORPORTALPage.existingContractsRecordMenu);
-        CommonUtils.waitForClickability(fCASVENDORPORTALPage.submitWorkOrderOption);
-        CommonUtils.clickOnElement(fCASVENDORPORTALPage.submitWorkOrderOption);
-    }
-
-    @Then("User can see Submit Work Order page header")
-    public void user_can_see_submit_work_order_page_header() {
-       FCASVENDORPORTALStepsImplementation.verifySubmitWorkOrderPageHeader();
-    }
-
-    @Then("User can see Version Work Order field")
-    public void user_can_see_version_work_order_field() {
-        FCASVENDORPORTALStepsImplementation.verifyWorkOrderField();
-    }
-
-    @Then("User can see Work Order field")
-    public void user_can_see_work_order_field() {
-        FCASVENDORPORTALStepsImplementation.verifyWorkOrderField();
-    }
-
-    @Then("User can see Package Type dropdown")
-    public void user_can_see_package_type_dropdown() {
-     FCASVENDORPORTALStepsImplementation.verifyPackageTypeDropdown();
-    }
-
-    @Then("User can see Estimate field")
-    public void user_can_see_estimate_field() {
-       FCASVENDORPORTALStepsImplementation.verifyEstimateField();
-    }
-
-    @Then("User can see Beneficiary Occupancy Date field")
-    public void user_can_see_beneficiary_occupancy_date_field() {
-       FCASVENDORPORTALStepsImplementation.verifyBeneficiaryOccupantDateFieldField();
-    }
-
-    @Then("User can see Project ID field")
-    public void user_can_see_project_id_field() {
-      FCASVENDORPORTALStepsImplementation.verifyProjectIdField();
-    }
-
-    @Then("User can see Directorate Customer field")
-    public void user_can_see_directorate_customer_field() {
-        FCASVENDORPORTALStepsImplementation.verifyDirectorateCustomerIDField();
-    }
-
-    @Then("User can see Directorate field")
-    public void user_can_see_directorate_field() {
-        FCASVENDORPORTALStepsImplementation.verifyDirectorateIDField();
-    }
-
-    @Then("User can see Building field")
-    public void user_can_see_building_field() {
-        FCASVENDORPORTALStepsImplementation.verifyBuildingIDField();
-    }
-
-    @Then("User can see Room field")
-    public void user_can_see_room_field() {
-        FCASVENDORPORTALStepsImplementation.verifyRoomIDField();
-    }
-
-    @Given("User chooses Existing Contracts Submit Notification option")
-    public void user_chooses_existing_contracts_submit_notification_option() {
-        CommonUtils.clickOnElement(fCASVENDORPORTALPage.existingContractsRecordMenu);
-        CommonUtils.waitForClickability(fCASVENDORPORTALPage.submitNotificationOption);
-        CommonUtils.clickOnElement(fCASVENDORPORTALPage.submitNotificationOption);
-    }
-
-    @Then("User can see Submit Notification page header")
-    public void user_can_see_submit_notification_page_header() {
-     FCASVENDORPORTALStepsImplementation.verifySubmitNotificationHeader();
-    }
-
-    @Then("User can see Notification Type dropdown")
-    public void user_can_see_notification_type_dropdown() {
-     FCASVENDORPORTALStepsImplementation.verifyNotificationsDropdown();
-    }
-
-    @Then("User can see Notification field")
-    public void user_can_see_notification_field() {
-       FCASVENDORPORTALStepsImplementation.verifyNotificationField();
     }
 }
